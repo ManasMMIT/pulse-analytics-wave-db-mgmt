6a0face0789fd210f9edf9802494c0e323aceec3
Jon Lin <jon.cp.lin@gmail.com>
2019-03-21 21:42:00 -0400

    Clean up csv parser script further; allow it to use fileMonth/fileYear when sheet doesn't have month/year column

--- historicalImportScripts/parse-csv-file-and-write-to-db.js
+++ historicalImportScripts/parse-csv-file-and-write-to-db.js
@@ -77,26 +77,28 @@ const parseCsvFileAndWriteToDb = ({
       })
     },
     step: (results, parser) => {
-      const isEmptyLine = isEmptyRow(results.data[0])
+      const rowIndex = rowParseCount++ // this returns the number PRE-incrementation
+      if (rowIndex < 2) return // skip processing the row if it's first two rows
 
-      // skip processing the row if it's empty or it's the first two rows
-      if (!isEmptyLine && rowParseCount > 1) {
-        const resultData = sanitizeKeysAndTrimData(results.data[0])
-        resultData.month = parseInt(resultData.month)
-        resultData.year = parseInt(resultData.year)
+      let row = results.data[0]
+      row = sanitizeKeysAndTrimData(row)
 
-        // error out if input month/year doesn't match csv file month/year
-        if (resultData.month !== fileMonth || resultData.year !== fileYear) {
-          wasParserAborted = true
-          console.error('At least this row doesn\'t match file name\'s month and year', resultData)
-          parser.abort()
-          return
-        }
+      // skip processing the row if it's empty after sanitization
+      if (isEmptyRow(row)) return
 
-        data.push(resultData)
+      // if there's no month/year, default to fileMonth and fileYear
+      row.month = Number(row.month) || fileMonth
+      row.year = Number(row.year) || fileYear
+
+      // error out if row has month/year but it doesn't match file month/year
+      if (row.month !== fileMonth || row.year !== fileYear) {
+        wasParserAborted = true
+        console.error('At least this row doesn\'t match file name\'s month and year', row)
+        parser.abort()
+        return
       }
 
-      rowParseCount++
+      data.push(row)
     },
     error: async err => {
       await terminateScript('Error from Papa parse operation:', err)
