0db6643a6b20fdb3b3fc96ee5980b864a0104397
Jon Lin <jon.cp.lin@gmail.com>
2019-04-30 17:43:08 -0400

    Build out process_auth0_data util

---
+++ pulse-core-psql/process_auth0_data.js
@@ -0,0 +1,30 @@
+const _ = require('lodash')
+const AuthorizationExtensionClient = require('./AuthorizationExtensionClient')
+const {
+  auth0_auth_api_clientid,
+  auth0_auth_api_secret,
+  auth0_auth_api_url,
+  auth0_auth_api_issuer,
+} = require('./auth0.config.js')
+
+const clientId = auth0_auth_api_clientid
+const clientSecret = auth0_auth_api_secret
+const url = auth0_auth_api_url
+const tokenIssuerUrl = auth0_auth_api_issuer
+
+const authClient = new AuthorizationExtensionClient(
+  url,
+  { tokenIssuerConfig: { tokenIssuerUrl, clientId, clientSecret } }
+)
+
+module.exports = () => Promise.all([
+  authClient.getUsers(),
+  authClient.getGroups(),
+]).then(([
+  users,
+  groups,
+]) => {
+  const formattedUsers = users.map(({ user_id: id, name: username }) => ({ id, username }))
+  const [roles, manufacturers] = _.partition(groups, groupObj => groupObj.name.includes('-'))
+  return { users: formattedUsers, roles, manufacturers }
+})
