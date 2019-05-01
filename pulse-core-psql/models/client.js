1bbd1faf81d3fb3c48cf75b2195a7003d7c6958d
Jon Lin <jon.cp.lin@gmail.com>
2019-05-01 17:11:18 -0400

    Rename manufacturer table to client

--- pulse-core-psql/models/client.js
+++ pulse-core-psql/models/client.js
@@ -1,7 +1,7 @@
 /* jshint indent: 2 */
 
 module.exports = function (sequelize, DataTypes) {
-  return sequelize.define('manufacturer', {
+  return sequelize.define('client', {
     id: {
       type: DataTypes.STRING,
       allowNull: false,
