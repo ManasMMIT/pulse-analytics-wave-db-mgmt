8ae511c8b848aa19f5791b857fd8a9cdf0ab2355
Jon Lin <jon.cp.lin@gmail.com>
2019-05-01 17:12:39 -0400

    Rename manufacturer to client in sync-auth0 script

--- pulse-core-psql/sync-auth0-psql.js
+++ pulse-core-psql/sync-auth0-psql.js
@@ -2,20 +2,20 @@ const _ = require('lodash')
 const getAuth0Data = require('./process_auth0_data')
 
 const syncAuth0WithDB = async sequelize => {
-  const User = await sequelize.import('user', require('./models/user.js'))
-  const Role = await sequelize.import('role', require('./models/role.js'))
-  const UserRole = await sequelize.import('users_roles', require('./models/users_roles.js'))
-  const Manufacturer = await sequelize.import('manufacturer', require('./models/manufacturer.js'))
+  const User = await sequelize.import('user', require('./models/user'))
+  const Role = await sequelize.import('role', require('./models/role'))
+  const UserRole = await sequelize.import('users_roles', require('./models/users_roles'))
+  const Client = await sequelize.import('client', require('./models/client'))
 
-  User.belongsToMany(Role, {through: UserRole})
-  Role.belongsToMany(User, {through: UserRole})
+  User.belongsToMany(Role, { through: UserRole })
+  Role.belongsToMany(User, { through: UserRole })
 
-  Manufacturer.hasMany(Role, {onDelete: 'cascade'})
-  Role.belongsTo(Manufacturer)
+  Client.hasMany(Role, { onDelete: 'cascade' })
+  Role.belongsTo(Client)
 
-  await sequelize.sync({force: true})
+  await sequelize.sync({ force: true })
 
-  const {users, roles, manufacturers} = await getAuth0Data()
+  const { users, roles, clients } = await getAuth0Data()
 
   await User.bulkCreate(users)
 
@@ -26,7 +26,7 @@ const syncAuth0WithDB = async sequelize => {
       description: role.description,
     })
 
-    const {members} = role
+    const { members } = role
 
     if (!_.isEmpty(members)) {
       for (memberId of members) {
@@ -36,22 +36,24 @@ const syncAuth0WithDB = async sequelize => {
     }
   }
 
-  for (const manufacturer of manufacturers) {
-    const CurrentManufacturer = await Manufacturer.create({
-      id: manufacturer._id,
-      name: manufacturer.name,
-      description: manufacturer.description,
+  for (const client of clients) {
+    const CurrentClient = await Client.create({
+      id: client._id,
+      name: client.name,
+      description: client.description,
     })
 
-    const {nested: roles} = manufacturer
+    const { nested: roles } = client
 
     if (!_.isEmpty(roles)) {
       for (const roleId of roles) {
         const TargetRole = await Role.findByPk(roleId)
-        await CurrentManufacturer.addRole(TargetRole)
+        await CurrentClient.addRole(TargetRole)
       }
     }
   }
+
+  return { User, Role, Client }
 }
 
 module.exports = syncAuth0WithDB
