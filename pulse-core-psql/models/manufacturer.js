5f090974608d48b8af2cc4108b2d95aa18c2c55e
Jon Lin <jon.cp.lin@gmail.com>
2019-05-01 15:03:10 -0400

    Add manufacturer model

---
+++ pulse-core-psql/models/manufacturer.js
@@ -0,0 +1,19 @@
+/* jshint indent: 2 */
+
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define('manufacturer', {
+    id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+  });
+};
