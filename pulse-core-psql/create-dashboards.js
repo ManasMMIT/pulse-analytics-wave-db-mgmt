99303067f574e2474ec8015e4dd879864164911d
Jon Lin <jon.cp.lin@gmail.com>
2019-05-02 09:29:14 -0400

    Comment back in dashboards creations

--- pulse-core-psql/create-dashboards.js
+++ pulse-core-psql/create-dashboards.js
@@ -3,20 +3,20 @@ const createDashboards = async sequelize => {
   Dashboard.belongsTo(Dashboard)
   Dashboard.hasMany(Dashboard, { onDelete: 'cascade' })
 
-  // await Dashboard.sync({ force: true })
+  await Dashboard.sync({ force: true })
 
-  // await Dashboard.bulkCreate([
-  //   { name: 'Provider Targeted Accounts' },
-  //   { name: 'Payer' },
-  // ])
+  await Dashboard.bulkCreate([
+    { name: 'Provider Targeted Accounts' },
+    { name: 'Payer' },
+  ])
 
-  // const parentDashes = await Dashboard.findAll()
-  // for (childDash of ['Management', 'Accounts']) {
-  //   for (parentDash of parentDashes) {
-  //     const createdChildDash = await Dashboard.create({ name: childDash })
-  //     await parentDash.addDashboard(createdChildDash)
-  //   }
-  // }
+  const parentDashes = await Dashboard.findAll()
+  for (childDash of ['Management', 'Accounts']) {
+    for (parentDash of parentDashes) {
+      const createdChildDash = await Dashboard.create({ name: childDash })
+      await parentDash.addDashboard(createdChildDash)
+    }
+  }
 
   return Dashboard
 }
