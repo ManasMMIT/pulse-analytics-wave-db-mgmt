685c97d3f1da9507bdbb7bb216b7793691a20e62
Jon Lin <jon.cp.lin@gmail.com>
2019-05-02 00:54:31 -0400

    Verify self-referential Dashboard tables work, break out initialization in separate file

---
+++ pulse-core-psql/create-dashboards.js
@@ -0,0 +1,24 @@
+const createDashboards = async sequelize => {
+  const Dashboard = await sequelize.import('dashboard', require('./models/dashboard'))
+  Dashboard.belongsTo(Dashboard)
+  Dashboard.hasMany(Dashboard, { onDelete: 'cascade' })
+
+  await sequelize.sync({ force: true })
+
+  await Dashboard.bulkCreate([
+    { name: 'Provider Targeted Accounts' },
+    { name: 'Payer' },
+  ])
+
+  const parentDashes = await Dashboard.findAll()
+  for (childDash of ['Management', 'Accounts']) {
+    for (parentDash of parentDashes) {
+      const createdChildDash = await Dashboard.create({ name: childDash })
+      await parentDash.addDashboard(createdChildDash)
+    }
+  }
+
+  return Dashboard
+}
+
+export default createDashboards
