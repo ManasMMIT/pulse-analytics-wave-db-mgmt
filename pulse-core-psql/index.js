3ad618bc6c1af69b5fcda265c92c3e32523a5631
Jon Lin <jon.cp.lin@gmail.com>
2019-05-02 20:04:06 -0400

    Get first permission creation working

--- pulse-core-psql/index.js
+++ pulse-core-psql/index.js
@@ -44,6 +44,58 @@ const executeDbOperations = async () => {
   const Page = await createPages(sequelize, Dashboard, false)
   const Card = await createCards(sequelize, Page, false)
   const Content = await createContents(sequelize, Card, false)
+
+  const Resource = await sequelize.import('resource', require('./models/resource'))
+  const Permission = await sequelize.import('permission', require('./models/permission'))
+
+  Permission.belongsTo(Role)
+  Permission.belongsTo(Content)
+  Permission.belongsTo(Resource)
+  Role.hasMany(Permission, { onDelete: 'cascade' })
+  Content.hasMany(Permission, { onDelete: 'cascade' })
+  Resource.hasMany(Permission, { onDelete: 'cascade' })
+
+  // await Resource.sync({ force: true })
+  await Permission.sync({ force: true })
+
+  // const regionalBreakdownResource = await Resource.findByPk(1)
+  // const regionalMap1Content = await Content.findByPk(15)
+  // const roleForMap1 = await Role.findByPk('5404d17a-d830-4e68-ba5a-623abf96ab74')
+
+  const firstPermission = await Permission.create({
+    contentOrder: 1,
+    resourceId: 1,
+    contentId: 15,
+    roleId: '5404d17a-d830-4e68-ba5a-623abf96ab74'
+  })
+
+  // debugger
+
+  // await regionalBreakdownResource.addPermission(firstPermission)
+  // debugger
+  // await regionalMap1Content.addPermission(firstPermission)
+  // debugger
+  // await roleForMap1.addPermission(firstPermission)
+  // debugger
+  // await firstPermission.setResource(regionalBreakdownResource)
+  // debugger
+  // await firstPermission.setContent(regionalMap1Content)
+  // debugger
+  // await firstPermission.setRole(roleForMap1)
+  // debugger
+
+  // const testSave = await firstPermission.save()
+  // debugger
+
+  const resourceCheck = await firstPermission.getResource()
+  const contentCheck = await firstPermission.getContent()
+  const roleCheck = await firstPermission.getRole()
+  debugger
+
+  const permission1Check = await resourceCheck.getPermissions()
+  const permission2Check = await contentCheck.getPermissions()
+  const permission3Check = await roleCheck.getPermissions()
+  debugger
 }
 
 executeDbOperations()
