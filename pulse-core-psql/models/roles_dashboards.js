47f46c7df727df35bfd0fd41556d73a1c71db4ce
Jon Lin <jon.cp.lin@gmail.com>
2019-05-06 17:13:01 -0400

    Change roles_dashboards dashboardOrder to order in light of sequelize key shortening

--- pulse-core-psql/models/roles_dashboards.js
+++ pulse-core-psql/models/roles_dashboards.js
@@ -12,7 +12,7 @@ module.exports = function (sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: true,
     },
-    dashboardOrder: {
+    order: {
       type: DataTypes.INTEGER,
       allowNull: false,
     },
