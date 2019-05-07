784356c4e15b56e649d58fa14097b89a842d7392
Jon Lin <jon.cp.lin@gmail.com>
2019-05-06 23:23:56 -0400

    Create roles_cards and roles_pages models

---
+++ pulse-core-psql/models/roles_cards.js
@@ -0,0 +1,22 @@
+/* jshint indent: 2 */
+
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define('roles_cards', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    alias: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    order: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  }, {
+      tableName: 'roles_cards'
+    });
+};
