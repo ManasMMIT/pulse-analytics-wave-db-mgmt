b9397d3ced9b952e48a83b6627c0bf9716a6ec2d
Jon Lin <jon.cp.lin@gmail.com>
2019-05-07 17:11:26 -0400

    Update regional_breakdowns_us_states_regions join table model

--- pulse-core-psql/initializeTables/models/regional_breakdowns_us_states_regions.js
+++ pulse-core-psql/initializeTables/models/regional_breakdowns_us_states_regions.js
@@ -1,6 +1,6 @@
 /* jshint indent: 2 */
 
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   return sequelize.define('regional_breakdowns_us_states_regions', {
     id: {
       type: DataTypes.INTEGER,
@@ -8,7 +8,17 @@ module.exports = function(sequelize, DataTypes) {
       primaryKey: true,
       autoIncrement: true,
     },
+    regional_breakdown_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: 'compositeIndex',
+    },
+    us_state_region_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: 'compositeIndex',
+    },
   }, {
-    tableName: 'regional_breakdowns_us_states_regions'
-  });
+      tableName: 'regional_breakdowns_us_states_regions'
+    });
 };
