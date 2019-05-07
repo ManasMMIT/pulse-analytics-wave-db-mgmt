d5475b2c44a3bf3d55e6d2a8bb624e3f72e0d11f
Jon Lin <jon.cp.lin@gmail.com>
2019-05-07 13:18:33 -0400

    Break out connecting to psql into separate file

---
+++ pulse-core-psql/connect-to-psql.js
@@ -0,0 +1,32 @@
+require('dotenv').load()
+const Sequelize = require('sequelize')
+
+const PSQL_LOCAL_LOADER_URI = require('./db.config.js')
+const PSQL_PROD_LOADER_URI = process.env.PSQL_PROD_LOADER_URI
+
+const sslConfig = PSQL_PROD_LOADER_URI
+  ? {
+    ssl: true,
+    dialectOptions: {
+      ssl: { require: true }
+    }
+  }
+  : {}
+
+const sequelize = new Sequelize(PSQL_PROD_LOADER_URI || PSQL_LOCAL_LOADER_URI, sslConfig)
+
+const connectToPsql = async () => {
+  await sequelize
+    .authenticate()
+    .then(() => {
+      const environment = PSQL_PROD_LOADER_URI ? 'production' : 'local'
+      console.log(`-----Connected to PostgreSQL ${environment}-----`)
+    })
+    .catch(err => {
+      console.error('Unable to connect to the database:', err)
+    })
+
+  return sequelize
+}
+
+module.exports = connectToPsql
