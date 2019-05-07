acdbbf9aed1cbffd308ad7b2121af0e4de37360b
Jon Lin <jon.cp.lin@gmail.com>
2019-05-07 17:14:06 -0400

    Complete regional tables creation script

--- pulse-core-psql/initializeTables/create-regional-tables/index.js
+++ pulse-core-psql/initializeTables/create-regional-tables/index.js
@@ -3,6 +3,7 @@ let args = require('yargs')
 const connectToPsql = require('../../connect-to-psql')
 const csvToJson = require('./csv-to-json')
 const { getScriptTerminator } = require('../../../utils')
+const { STATES_MAP, STATE_CODE_MAP } = require('./states-util')
 
 args = args.usage('Usage: $0 --filepath [string]')
   .demandOption(['filepath'])
@@ -23,8 +24,7 @@ if (filepath) {
   if (fileExists(filepath)) {
     const executeRegionalSeeding = async () => {
       const sequelize = await connectToPsql()
-      debugger
-      await createRegionalTables({ sequelize, shouldSeed: true })
+      await createRegionalTables({ sequelize, shouldSeed: false })
     }
 
     executeRegionalSeeding()
@@ -39,30 +39,80 @@ if (filepath) {
 async function createRegionalTables({ sequelize, shouldSeed }) {
   const UsState = await sequelize.import('us_state', require('../models/us_state'))
   const Region = await sequelize.import('region', require('../models/region'))
+  const RegionalBreakdown = await sequelize.import('regional_breakdown', require('../models/regional_breakdown'))
 
-  UsState.belongsToMany(Region, { through: 'us_state_regions' })
-  Region.belongsToMany(UsState, { through: 'us_state_regions' })
+  UsState.belongsToMany(Region, { through: 'us_states_regions', otherKey: 'stateId' })
+  Region.belongsToMany(UsState, { through: 'us_states_regions' })
 
-  const UsStateRegion = sequelize.define('us_state_regions')
-  debugger
+  const UsStateRegion = await sequelize.import('us_states_regions', require('../models/us_states_regions'))
+
+  UsStateRegion.belongsToMany(
+    RegionalBreakdown,
+    {
+      through: 'regional_breakdowns_us_states_regions',
+      foreignKey: 'us_state_region_id',
+      otherKey: 'regional_breakdown_id',
+    },
+  )
+
+  RegionalBreakdown.belongsToMany(
+    UsStateRegion,
+    {
+      through: 'regional_breakdowns_us_states_regions',
+      foreignKey: 'regional_breakdown_id',
+      otherKey: 'us_state_region_id',
+      as: 'statesRegions',
+    },
+  )
+
+  const BreakdownStateRegion = await sequelize.import(
+    'regional_breakdowns_us_states_regions',
+    require('../models/regional_breakdowns_us_states_regions'),
+  )
 
   if (shouldSeed) {
     const terminateScript = getScriptTerminator(sequelize)
 
-    const Models = [UsState, Region, UsStateRegion]
+    const Models = [UsState, Region, UsStateRegion, RegionalBreakdown, BreakdownStateRegion]
     for (const Model of Models) { await Model.sync({ force: true }) }
-    const json = await csvToJson(filepath).catch(terminateScript)
-    debugger
+    const rawClientRegionsData = await csvToJson(filepath).catch(terminateScript)
 
+    for (const datum of rawClientRegionsData) {
+      const { user, role, content, region, state } = datum
 
-    // for (let i = 1; i < 25; i += 1) {
-    //   const currentCard = await Card.create({ name: PAGE_CARD_MAP[i] })
-    //   const currentPage = await Page.findByPk(i);
-    //   await currentPage.addCard(currentCard)
-    // }
+      const currentUsState = await UsState.findOrCreate({
+        where: {
+          id: STATE_CODE_MAP[state],
+          state,
+          stateLong: STATES_MAP[state] || state
+        },
+      }).then(arr => arr[0])
+
+      const currentRegion = await Region.findOrCreate({
+        where: { name: region },
+      }).then(arr => arr[0])
+
+      const currentUsStateRegion = await UsStateRegion.findOrCreate({
+        where: {
+          stateId: currentUsState.id,
+          regionId: currentRegion.id,
+        },
+      }).then(arr => arr[0])
+
+      const currentRegionalBreakdown = await RegionalBreakdown.findOrCreate({
+        where: { name: user },
+      }).then(arr => arr[0])
+
+      const currentBreakdownStateRegion = await BreakdownStateRegion.findOrCreate({
+        where: {
+          regional_breakdown_id: currentRegionalBreakdown.id,
+          us_state_region_id: currentUsStateRegion.id,
+        },
+      }).then(arr => arr[0])
+    }
   }
 
-  return null
+  return BreakdownStateRegion
 }
 
 module.exports = createRegionalTables
