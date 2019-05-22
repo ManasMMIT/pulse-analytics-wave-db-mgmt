78a01a4aba44e590bf51ebfe9a3e6c73b2e65270
Jon Lin <jon.cp.lin@gmail.com>
2019-05-21 23:39:52 -0400

    Build out create-cards seed func, including adding cards to provider/overview

--- pulse-core-psql/initializeTables/create-contents/create-cards.js
+++ pulse-core-psql/initializeTables/create-contents/create-cards.js
@@ -1,46 +1,60 @@
-const PAGE_CARD_MAP = {
-  1: 'Site Locations',
-  2: 'Internal Dispensing',
-  3: 'Pathways',
-  4: 'Alternative Payment Model Breakdown',
-  5: 'Staffing',
-  6: 'Clinical Designations',
-  7: '3rd Party & Internal Pathways',
-  8: 'Valued Data Sources & Education Resources',
-  9: 'Portfolio Quality of Access Summary',
-  10: 'Quality of Access',
-  11: null,
-  12: null,
-  13: 'Review Timing',
-  14: 'FACT-Accredited Treatment Centers',
-  15: 'Regional Quality of Access',
-  16: 'Regional Quality of Access',
-  17: 'Payer Participation in Value-Based Models',
-  18: 'Strategic Accounts',
-  19: 'PowerPoints & Excel Reports',
-  20: 'Account Summary: Dominant Quality of Access',
-  21: 'Payer Overview',
-  22: 'Implemented Traditional Management',
-  23: 'Review Timing',
-  24: null
-}
+const _ = require('lodash')
+const PAGE_CARD_MAP = require('./page-to-card-map')
 
-const createCards = async ({ sequelize, Page, shouldSeed }) => {
-  const Card = await sequelize.import('card', require('./models/card'))
-  Card.belongsTo(Page)
-  Page.hasMany(Card, { onDelete: 'cascade' })
+const providerOverviewCardsData = [
+  {
+    name: 'Management',
+    type: 'card',
+  },
+  {
+    name: 'Accounts',
+    type: 'card',
+  },
+  {
+    name: 'Tutorial',
+    type: 'card',
+  },
+]
 
-  if (shouldSeed) {
-    await Card.sync({ force: true })
+const createCards = async ({
+  Content,
+  pages,
+  dashboards: { provider_overview },
+}) => {
+  const cards = {}
 
-    for (let i = 1; i < 25; i += 1) {
-      const currentCard = await Card.create({ name: PAGE_CARD_MAP[i] })
-      const currentPage = await Page.findByPk(i);
-      await currentPage.addCard(currentCard)
-    }
-  }
+  const promisesForCardsCreation = _.map(pages, (page, pageKey) => {
+    const cardEntry = PAGE_CARD_MAP[pageKey]
 
-  return Card
+    if (!cardEntry) return null
+
+    const cardsDataToPersist = cardEntry.map(({ name }) => ({
+      name,
+      type: 'card',
+    }))
+
+    return Content.bulkCreate(cardsDataToPersist).then(createdCards => {
+      createdCards.forEach(createdCard => {
+        cards[`${pageKey}_${_.camelCase(createdCard.name)}`] = createdCard
+      })
+
+      return page.addChildren(createdCards)
+    })
+  })
+
+  promisesForCardsCreation.push(
+    Content.bulkCreate(providerOverviewCardsData).then(createdCards => {
+      createdCards.forEach(createdCard => {
+        cards[`provider_overview_${createdCard.name.toLowerCase()}Card`] = createdCard
+      })
+
+      return provider_overview.addChildren(createdCards)
+    })
+  )
+
+  await Promise.all(promisesForCardsCreation)
+
+  return cards
 }
 
 module.exports = createCards
---
+++ pulse-core-psql/initializeTables/create-contents/page-to-card-map.js
@@ -0,0 +1,140 @@
+const PAGE_CARD_MAP = {
+  provider_management_regionalFootprint: [
+    {
+      name: 'Site Locations',
+    },
+    {
+      name: 'Site of Care Mix',
+    },
+  ],
+  provider_management_internalPharmacy: [
+    {
+      name: 'Internal Dispensing',
+    },
+    {
+      name: 'Specialty Pharmacy Accreditation',
+    },
+  ],
+  provider_management_pathways: [
+    {
+      name: 'Pathways',
+    },
+    {
+      name: '3rd Party Pathways',
+    },
+    {
+      name: '3rd Party & Internal Pathways Integration',
+    },
+    {
+      name: '3rd Party & Internal Pathways Policing Mechanisms',
+    },
+  ],
+  provider_management_alternativePaymentModels: [
+    {
+      name: 'Alternative Payment Model Breakdown',
+    },
+  ],
+  provider_accounts_businessModelCapabilities: [
+    {
+      name: 'Staffing',
+    },
+    {
+      name: 'Annual Patient Volume by Indication',
+    },
+    {
+      name: 'Payer Mix',
+    },
+    {
+      name: 'Operational Infrastructure',
+    },
+    {
+      name: 'M&A, Affiliations, Academic Partnerships',
+    },
+  ],
+  provider_accounts_clinicalSophistication: [
+    {
+      name: 'Clinical Designations',
+    },
+    {
+      name: 'Clinical Trials Volume',
+    },
+    {
+      name: 'Precision Medicine',
+    },
+    {
+      name: 'NCCN Panel Members (KOLs)',
+    },
+  ],
+  provider_accounts_valueBasedCare: [
+    {
+      name: '3rd Party & Internal Pathways',
+    },
+    {
+      name: 'Internal Pathways: [Dynamic Account]',
+    },
+    {
+      name: 'Alternative Payment Model / Quality Program',
+    },
+    {
+      name: 'Internal Pathways Influencers',
+    },
+  ],
+  provider_accounts_manufacturerEngagement: [
+    {
+      name: 'Valued Data Sources & Education Resources',
+    },
+    {
+      name: 'Manufacturer Engagement',
+    },
+    {
+      name: 'Key Decision Makers',
+    },
+  ],
+  // payer_management_summary: [
+
+  // ],
+  // payer_management_qualityOfAccess: [
+
+  // ],
+  // payer_management_dupixentRelativeAccess: [
+
+  // ],
+  // payer_management_competitiveAccess: [
+
+  // ],
+  // payer_management_reviewTiming: [
+
+  // ],
+  // payer_management_treatmentCenters: [
+
+  // ],
+  // payer_management_regionalTargeting: [
+
+  // ],
+  // payer_management_valueBasedModels: [
+
+  // ],
+  // payer_management_strategicAccounts: [
+
+  // ],
+  // payer_management_reports: [
+
+  // ],
+  // payer_accounts_summaryEngagement: [
+
+  // ],
+  // payer_accounts_overview: [
+
+  // ],
+  // payer_accounts_managementCapabilities: [
+
+  // ],
+  // payer_accounts_reviewProcess: [
+
+  // ],
+  // payer_accounts_productCoverage: [
+
+  // ],
+}
+
+module.exports = PAGE_CARD_MAP
