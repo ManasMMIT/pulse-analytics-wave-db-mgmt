eb243198a9531095c664c5642b716f1f18bb6a4b
Jon Lin <jon.cp.lin@gmail.com>
2019-05-23 09:52:30 -0400

    WIP manual SQL query on nodes table

--- pulse-core-psql/generate-data-for-mongodb.js
+++ pulse-core-psql/generate-data-for-mongodb.js
@@ -1,11 +1,12 @@
 const _ = require('lodash')
 const initializeTables = require('./initialize-tables')
 
-const processRawUsersNodesResources = require('./process-users-nodes-resources')
-const processRawUsersSitemaps = require('./process-users-sitemaps')
+// const processRawUsersNodesResources = require('./process-users-nodes-resources')
+// const processRawUsersSitemaps = require('./process-users-sitemaps')
 
 const generateDataForMongoDb = async () => {
   const {
+    sequelize,
     User,
     Role,
     Client,
@@ -16,21 +17,108 @@ const generateDataForMongoDb = async () => {
     Permission,
   } = await initializeTables()
 
-  // // regional breakdown can only be seeded by uploading CSV
-  // const RegionalBreakdown = await createRegionalTables({ sequelize })
+  await Role.findOne(
+    {
+      where: { id: 'c04bfb71-9314-4a51-be72-480c3d7c82cf' },
+      include: [
+        {
+          model: Node,
+          duplicating: true,
+          required: true,
+        },
+      ],
+    }
+  )
 
-  // const Resource = await createResources({
-  //   sequelize,
-  //   RegionalBreakdown,
-  //   shouldSeed: false,
-  // })
+  await sequelize.query(`
+    SELECT *, (
+      SELECT * FROM nodes
+      JOIN roles_nodes
+      ON nodes.id = roles_nodes."nodeId"
+      JOIN roles
+      ON roles.id = roles_nodes."roleId"
+      WHERE roles.id = 'c04bfb71-9314-4a51-be72-480c3d7c82cf'
+    ) as targetNodes
+    JOIN n2n
+    ON targetNodes.id = n2n.parentId
+    JOIN targetNodes.id = n2n.childId
+  `)
 
-  // const Permission = await createPermissions({
-  //   sequelize,
-  //   RoleNode,
-  //   Resource,
-  //   shouldSeed: false,
-  // })
+  const queryToGetAllAccessibleNodes = `
+    SELECT
+      nodes.id, nodes.name, nodes.type, roles_nodes.order
+    FROM
+      nodes
+    JOIN
+      roles_nodes
+    ON
+      nodes.id = roles_nodes."nodeId"
+    JOIN
+      roles
+    ON
+      roles.id = roles_nodes."roleId"
+    WHERE
+      roles.id = 'c04bfb71-9314-4a51-be72-480c3d7c82cf'
+    `
+
+  await sequelize.query(queryToGetAllAccessibleNodes)
+
+  await sequelize.query(`
+    SELECT
+      nodes.id, nodes.name, nodes.type, nodes.order, n2n."childId", n2n."parentId"
+    FROM (${queryToGetAllAccessibleNodes}) AS nodes
+    JOIN
+      n2n
+    ON
+      n2n."parentId" = nodes.id
+  `)
+
+  return {
+    // usersNodesResources,
+    // usersSitemaps
+  }
+}
+
+module.exports = generateDataForMongoDb
+
+  // // get all the users who have sitemaps
+  // await User.findAll(
+  //   {
+  //     duplicating: true,
+  //     required: true,
+  //     include: [
+  //       {
+  //         model: Role,
+  //         duplicating: true,
+  //         required: true,
+  //         include: [
+  //           {
+  //             model: Node,
+  //             duplicating: true,
+  //             required: true,
+  //             where: { type: 'sitemap' }
+  //           }
+  //         ]
+  //       }
+  //     ]
+  //   }
+  // )
+
+  // // get all the roles that currently have sitemaps
+  // await Role.findAll(
+  //   {
+  //     duplicating: true,
+  //     required: true,
+  //     include: [
+  //       {
+  //         model: Node,
+  //         duplicating: true,
+  //         required: true,
+  //         where: { type: 'sitemap' }
+  //       }
+  //     ]
+  //   }
+  // )
 
   // // get users.nodes.resources
   // const rawUsersNodesResources = await User.findAll(
@@ -90,26 +178,6 @@ const generateDataForMongoDb = async () => {
   // })
 
   // // get users.sitemaps
-  // const roleTopDashboardWhereCond = sequelize.where(
-  //   sequelize.col('roles.id'),
-  //   sequelize.col('roles->nodes->card->page->dashboard->dashboard->roles_dashboards.roleId'),
-  // )
-
-  // const roleLowerDashboardWhereCond = sequelize.where(
-  //   sequelize.col('roles.id'),
-  //   sequelize.col('roles->nodes->card->page->dashboard->roles_dashboards.roleId'),
-  // )
-
-  // const rolePageWhereCond = sequelize.where(
-  //   sequelize.col('roles.id'),
-  //   sequelize.col('roles->nodes->card->page->roles_pages.roleId'),
-  // )
-
-  // const roleCardWhereCond = sequelize.where(
-  //   sequelize.col('roles.id'),
-  //   sequelize.col('roles->nodes->card->roles_cards.roleId'),
-  // )
-
   // const rawUsersSitemaps = await User.findAll(
   //   {
   //     duplicating: true,
@@ -209,11 +277,3 @@ const generateDataForMongoDb = async () => {
   // )
 
   // const usersSitemaps = processRawUsersSitemaps(rawUsersSitemaps)
-
-  return {
-    // usersNodesResources,
-    // usersSitemaps
-  }
-}
-
-module.exports = generateDataForMongoDb
