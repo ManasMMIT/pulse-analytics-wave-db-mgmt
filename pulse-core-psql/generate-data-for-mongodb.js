2d568f27046960e18ccab0cd3daf19f2e55bce78
Jon Lin <jon.cp.lin@gmail.com>
2019-05-23 18:49:09 -0400

    Reach pause point with first half of bottom up query

--- pulse-core-psql/generate-data-for-mongodb.js
+++ pulse-core-psql/generate-data-for-mongodb.js
@@ -100,8 +100,6 @@ recursionQueryTopDown = `
 `
 await sequelize.query(recursionQueryTopDown)
 
-
-// PAUSING ON THE BELOW BECAUSE IT'S GETTING CONFUSING
 recursionQueryBottomUp = `
   WITH RECURSIVE nodes_from_parents AS (${recursionQueryTopDown}),
   nodes_from_children AS (
@@ -113,10 +111,11 @@ recursionQueryBottomUp = `
         || jsonb_build_object('parentId', pcj."parentId")
       )::jsonb AS js
     FROM nodes_from_parents AS tree
-    LEFT JOIN (SELECT "parentId", "childId" FROM n2n) as pcj
+    JOIN (SELECT "parentId", "childId" FROM n2n) as pcj
     ON pcj."childId" = tree.id
-    LEFT JOIN nodes as c
-    ON c.id = pcj."parentId"
+    JOIN nodes as c
+    USING(id)
+    WHERE level > 0 AND NOT id = any(parents)
     GROUP BY pcj."parentId"
   )
   SELECT jsonb_agg(js)
@@ -124,265 +123,290 @@ recursionQueryBottomUp = `
 `
 await sequelize.query(recursionQueryBottomUp)
 
-`
 
-    UNION ALL
+// PAUSING ON THE BELOW BECAUSE IT'S GETTING CONFUSING
+// recursionQueryBottomUp = `
+//   WITH RECURSIVE nodes_from_parents AS (${recursionQueryTopDown}),
+//   nodes_from_children AS (
+//     SELECT
+//       pcj."parentId",
+//       json_agg(
+//         jsonb_build_object('name', c.name)
+//         || jsonb_build_object('id', c.id)
+//         || jsonb_build_object('parentId', pcj."parentId")
+//       )::jsonb AS js
+//     FROM nodes_from_parents AS tree
+//     LEFT JOIN (SELECT "parentId", "childId" FROM n2n) as pcj
+//     ON pcj."childId" = tree.id
+//     LEFT JOIN nodes as c
+//     ON c.id = pcj."parentId"
+//     WHERE level > 0 AND NOT id = any(parents)
+//     GROUP BY pcj."parentId"
+//   )
+//   SELECT jsonb_agg(js)
+//   FROM nodes_from_children
+// `
+// await sequelize.query(recursionQueryBottomUp)
 
-    SELECT
-      pcj2."parentId",
-      jsonb_build_object('name', c2.name)
-        || jsonb_build_object('id', c2.id)
-        || jsonb_build_object('parentId', pcj2."parentId")
-        || jsonb_build_object('kids', js) AS js
-    FROM nodes_from_children AS tree2
-    JOIN n2n AS pcj2
-    ON pcj2."childId" = tree2."parentId"
-    JOIN nodes AS c2
-    ON c2.id = pcj2."childId"
-`
+// `
 
-recursionQueryBottomUp = `
-  WITH RECURSIVE nodes_from_parents AS (${recursionQueryTopDown}),
-  nodes_from_children AS (
-    SELECT n2n."parentId", json_agg(jsonb_build_object('name', c.name))::jsonb as js
-    FROM nodes_from_parents AS tree
-    JOIN (SELECT "parentId", "childId" from n2n) as n2n
-    ON n2n."childId" = tree.id
-    JOIN nodes AS c
-    USING(id)
-    WHERE level > 0 AND NOT id = any(parents)
-    GROUP BY n2n."parentId"
+//     UNION ALL
 
-    UNION ALL
+//     SELECT
+//       pcj2."parentId",
+//       jsonb_build_object('name', c2.name)
+//         || jsonb_build_object('id', c2.id)
+//         || jsonb_build_object('parentId', pcj2."parentId")
+//         || jsonb_build_object('kids', js) AS js
+//     FROM nodes_from_children AS tree2
+//     JOIN n2n AS pcj2
+//     ON pcj2."childId" = tree2."parentId"
+//     JOIN nodes AS c2
+//     ON c2.id = pcj2."childId"
+// `
 
-    SELECT
-      asdf."parentId",
-      jsonb_build_object('name', c.name) || jsonb_build_object('kids', js) as js
-    FROM nodes_from_children AS tree
-    JOIN n2n as asdf
-    ON asdf."childId" = tree."parentId"
-    JOIN nodes AS c
-    ON c.id = asdf."parentId"
-  )
-  SELECT jsonb_agg(js)
-  FROM nodes_from_children
-`
+// recursionQueryBottomUp = `
+//   WITH RECURSIVE nodes_from_parents AS (${recursionQueryTopDown}),
+//   nodes_from_children AS (
+//     SELECT n2n."parentId", json_agg(jsonb_build_object('name', c.name))::jsonb as js
+//     FROM nodes_from_parents AS tree
+//     JOIN (SELECT "parentId", "childId" from n2n) as n2n
+//     ON n2n."childId" = tree.id
+//     JOIN nodes AS c
+//     USING(id)
+//     WHERE level > 0 AND NOT id = any(parents)
+//     GROUP BY n2n."parentId"
 
-`
-  SELECT n2n."parentId", json_agg(jsonb_build_object('name', n2n.name))::jsonb as js
-  FROM nodes_from_parents AS tree
-  JOIN n2n
-  ON n2n."childId" = tree.id
-  JOIN nodes as c
-  ON n2n."parentId" = c.id using(id)
-  WHERE level > 0 AND NOT id = any(parents)
-  GROUP BY n2n."parentId"
+//     UNION ALL
 
-  UNION ALL
+//     SELECT
+//       asdf."parentId",
+//       jsonb_build_object('name', c.name) || jsonb_build_object('kids', js) as js
+//     FROM nodes_from_children AS tree
+//     JOIN n2n as asdf
+//     ON asdf."childId" = tree."parentId"
+//     JOIN nodes AS c
+//     ON c.id = asdf."parentId"
+//   )
+//   SELECT jsonb_agg(js)
+//   FROM nodes_from_children
+// `
 
-  SELECT
-  n2n."parentId",
-    jsonb_build_object('name', n2n.name) || jsonb_build_object('Sub Classes', js) as js
-  FROM nodes_from_children AS tree
-  JOIN
-`
+// `
+//   SELECT n2n."parentId", json_agg(jsonb_build_object('name', n2n.name))::jsonb as js
+//   FROM nodes_from_parents AS tree
+//   JOIN n2n
+//   ON n2n."childId" = tree.id
+//   JOIN nodes as c
+//   ON n2n."parentId" = c.id using(id)
+//   WHERE level > 0 AND NOT id = any(parents)
+//   GROUP BY n2n."parentId"
+
+//   UNION ALL
+
+//   SELECT
+//   n2n."parentId",
+//     jsonb_build_object('name', n2n.name) || jsonb_build_object('Sub Classes', js) as js
+//   FROM nodes_from_children AS tree
+//   JOIN
+// `
 
 
-module.exports = generateDataForMongoDb
+// module.exports = generateDataForMongoDb
 
-  // // get all the users who have sitemaps
-  // await User.findAll(
-  //   {
-  //     duplicating: true,
-  //     required: true,
-  //     include: [
-  //       {
-  //         model: Role,
-  //         duplicating: true,
-  //         required: true,
-  //         include: [
-  //           {
-  //             model: Node,
-  //             duplicating: true,
-  //             required: true,
-  //             where: { type: 'sitemap' }
-  //           }
-  //         ]
-  //       }
-  //     ]
-  //   }
-  // )
+//   // // get all the users who have sitemaps
+//   // await User.findAll(
+//   //   {
+//   //     duplicating: true,
+//   //     required: true,
+//   //     include: [
+//   //       {
+//   //         model: Role,
+//   //         duplicating: true,
+//   //         required: true,
+//   //         include: [
+//   //           {
+//   //             model: Node,
+//   //             duplicating: true,
+//   //             required: true,
+//   //             where: { type: 'sitemap' }
+//   //           }
+//   //         ]
+//   //       }
+//   //     ]
+//   //   }
+//   // )
 
-  // // get all the roles that currently have sitemaps
-  // await Role.findAll(
-  //   {
-  //     duplicating: true,
-  //     required: true,
-  //     include: [
-  //       {
-  //         model: Node,
-  //         duplicating: true,
-  //         required: true,
-  //         where: { type: 'sitemap' }
-  //       }
-  //     ]
-  //   }
-  // )
+//   // // get all the roles that currently have sitemaps
+//   // await Role.findAll(
+//   //   {
+//   //     duplicating: true,
+//   //     required: true,
+//   //     include: [
+//   //       {
+//   //         model: Node,
+//   //         duplicating: true,
+//   //         required: true,
+//   //         where: { type: 'sitemap' }
+//   //       }
+//   //     ]
+//   //   }
+//   // )
 
-  // // get users.nodes.resources
-  // const rawUsersNodesResources = await User.findAll(
-  //   {
-  //     duplicating: true,
-  //     required: true,
-  //     include: [
-  //       {
-  //         model: Role,
-  //         through: { attributes: [] },
-  //         duplicating: true,
-  //         required: true,
-  //         include: [
-  //           {
-  //             model: RoleNode,
-  //             duplicating: true,
-  //             required: true,
-  //             include: [
-  //               {
-  //                 model: Resource,
-  //                 duplicating: true,
-  //                 required: true,
-  //                 include: [
-  //                   {
-  //                     model: RegionalBreakdown,
-  //                     as: 'regionalBreakdown',
-  //                     duplicating: true,
-  //                     required: true,
-  //                     include: [
-  //                       {
-  //                         model: sequelize.models.us_states_regions,
-  //                         duplicating: true,
-  //                         required: true,
-  //                         as: 'bsr',
-  //                       }
-  //                     ]
-  //                   }
-  //                 ]
-  //               }
-  //             ]
-  //           }
-  //         ],
-  //       },
-  //     ],
-  //   },
-  // )
+//   // // get users.nodes.resources
+//   // const rawUsersNodesResources = await User.findAll(
+//   //   {
+//   //     duplicating: true,
+//   //     required: true,
+//   //     include: [
+//   //       {
+//   //         model: Role,
+//   //         through: { attributes: [] },
+//   //         duplicating: true,
+//   //         required: true,
+//   //         include: [
+//   //           {
+//   //             model: RoleNode,
+//   //             duplicating: true,
+//   //             required: true,
+//   //             include: [
+//   //               {
+//   //                 model: Resource,
+//   //                 duplicating: true,
+//   //                 required: true,
+//   //                 include: [
+//   //                   {
+//   //                     model: RegionalBreakdown,
+//   //                     as: 'regionalBreakdown',
+//   //                     duplicating: true,
+//   //                     required: true,
+//   //                     include: [
+//   //                       {
+//   //                         model: sequelize.models.us_states_regions,
+//   //                         duplicating: true,
+//   //                         required: true,
+//   //                         as: 'bsr',
+//   //                       }
+//   //                     ]
+//   //                   }
+//   //                 ]
+//   //               }
+//   //             ]
+//   //           }
+//   //         ],
+//   //       },
+//   //     ],
+//   //   },
+//   // )
 
-  // const allStates = await sequelize.models.us_state.findAll()
-  // const allRegions = await sequelize.models.region.findAll()
-  // const statesByKey = _.keyBy(allStates, 'id')
-  // const regionsByKey = _.keyBy(allRegions, 'id')
+//   // const allStates = await sequelize.models.us_state.findAll()
+//   // const allRegions = await sequelize.models.region.findAll()
+//   // const statesByKey = _.keyBy(allStates, 'id')
+//   // const regionsByKey = _.keyBy(allRegions, 'id')
 
-  // const usersNodesResources = processRawUsersNodesResources({
-  //   rawUsersNodesResources,
-  //   statesByKey,
-  //   regionsByKey,
-  // })
+//   // const usersNodesResources = processRawUsersNodesResources({
+//   //   rawUsersNodesResources,
+//   //   statesByKey,
+//   //   regionsByKey,
+//   // })
 
-  // // get users.sitemaps
-  // const rawUsersSitemaps = await User.findAll(
-  //   {
-  //     duplicating: true,
-  //     required: true,
-  //     order: [
-  //       [
-  //         sequelize.col('roles->nodes->card->page->dashboard->dashboard->roles_dashboards.order'),
-  //         'ASC',
-  //       ],
-  //       [
-  //         sequelize.col('roles->nodes->card->page->dashboard->roles_dashboards.order'),
-  //         'ASC',
-  //       ],
-  //       [
-  //         sequelize.col('roles->nodes->card->page->roles_pages.order'),
-  //         'ASC',
-  //       ],
-  //       [
-  //         sequelize.col('roles->nodes->card->roles_cards.order'),
-  //         'ASC',
-  //       ],
-  //       [
-  //         sequelize.col('roles->nodes->roles_nodes.order'),
-  //         'ASC',
-  //       ],
-  //     ],
-  //     include: [
-  //       {
-  //         model: Role,
-  //         duplicating: true,
-  //         required: true,
-  //         through: { attributes: [] },
-  //         include: [
-  //           {
-  //             model: Node,
-  //             duplicating: true,
-  //             required: true,
-  //             include: [
-  //               {
-  //                 model: Card,
-  //                 duplicating: true,
-  //                 required: true,
-  //                 include: [
-  //                   {
-  //                     model: Page,
-  //                     duplicating: true,
-  //                     required: true,
-  //                     include: [
-  //                       {
-  //                         model: RolePage,
-  //                         duplicating: true,
-  //                         required: true,
-  //                         where: rolePageWhereCond,
-  //                       },
-  //                       {
-  //                         model: Dashboard,
-  //                         duplicating: true,
-  //                         required: true,
-  //                         include: [
-  //                           {
-  //                             model: Dashboard,
-  //                             duplicating: true,
-  //                             required: true,
-  //                             include: [
-  //                               {
-  //                                 model: RoleDashboard,
-  //                                 duplicating: true,
-  //                                 required: true,
-  //                                 where: roleTopDashboardWhereCond,
-  //                               }
-  //                             ]
-  //                           },
-  //                           {
-  //                             model: RoleDashboard,
-  //                             duplicating: true,
-  //                             required: true,
-  //                             where: roleLowerDashboardWhereCond,
-  //                           },
-  //                         ]
-  //                       },
-  //                     ]
-  //                   },
-  //                   {
-  //                     model: RoleCard,
-  //                     duplicating: true,
-  //                     required: true,
-  //                     where: roleCardWhereCond,
-  //                   },
-  //                 ]
-  //               },
-  //             ],
-  //           },
-  //         ],
-  //       },
-  //     ],
-  //   },
-  // )
+//   // // get users.sitemaps
+//   // const rawUsersSitemaps = await User.findAll(
+//   //   {
+//   //     duplicating: true,
+//   //     required: true,
+//   //     order: [
+//   //       [
+//   //         sequelize.col('roles->nodes->card->page->dashboard->dashboard->roles_dashboards.order'),
+//   //         'ASC',
+//   //       ],
+//   //       [
+//   //         sequelize.col('roles->nodes->card->page->dashboard->roles_dashboards.order'),
+//   //         'ASC',
+//   //       ],
+//   //       [
+//   //         sequelize.col('roles->nodes->card->page->roles_pages.order'),
+//   //         'ASC',
+//   //       ],
+//   //       [
+//   //         sequelize.col('roles->nodes->card->roles_cards.order'),
+//   //         'ASC',
+//   //       ],
+//   //       [
+//   //         sequelize.col('roles->nodes->roles_nodes.order'),
+//   //         'ASC',
+//   //       ],
+//   //     ],
+//   //     include: [
+//   //       {
+//   //         model: Role,
+//   //         duplicating: true,
+//   //         required: true,
+//   //         through: { attributes: [] },
+//   //         include: [
+//   //           {
+//   //             model: Node,
+//   //             duplicating: true,
+//   //             required: true,
+//   //             include: [
+//   //               {
+//   //                 model: Card,
+//   //                 duplicating: true,
+//   //                 required: true,
+//   //                 include: [
+//   //                   {
+//   //                     model: Page,
+//   //                     duplicating: true,
+//   //                     required: true,
+//   //                     include: [
+//   //                       {
+//   //                         model: RolePage,
+//   //                         duplicating: true,
+//   //                         required: true,
+//   //                         where: rolePageWhereCond,
+//   //                       },
+//   //                       {
+//   //                         model: Dashboard,
+//   //                         duplicating: true,
+//   //                         required: true,
+//   //                         include: [
+//   //                           {
+//   //                             model: Dashboard,
+//   //                             duplicating: true,
+//   //                             required: true,
+//   //                             include: [
+//   //                               {
+//   //                                 model: RoleDashboard,
+//   //                                 duplicating: true,
+//   //                                 required: true,
+//   //                                 where: roleTopDashboardWhereCond,
+//   //                               }
+//   //                             ]
+//   //                           },
+//   //                           {
+//   //                             model: RoleDashboard,
+//   //                             duplicating: true,
+//   //                             required: true,
+//   //                             where: roleLowerDashboardWhereCond,
+//   //                           },
+//   //                         ]
+//   //                       },
+//   //                     ]
+//   //                   },
+//   //                   {
+//   //                     model: RoleCard,
+//   //                     duplicating: true,
+//   //                     required: true,
+//   //                     where: roleCardWhereCond,
+//   //                   },
+//   //                 ]
+//   //               },
+//   //             ],
+//   //           },
+//   //         ],
+//   //       },
+//   //     ],
+//   //   },
+//   // )
 
-  // const usersSitemaps = processRawUsersSitemaps(rawUsersSitemaps)
+//   // const usersSitemaps = processRawUsersSitemaps(rawUsersSitemaps)
