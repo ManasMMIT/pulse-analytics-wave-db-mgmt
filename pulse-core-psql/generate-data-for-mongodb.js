9aa9599aadf92bc59da4a1223818d3a87d9e5f4e
Jon Lin <jon.cp.lin@gmail.com>
2019-05-29 02:15:20 -0400

    Update generate-data-for-mongodb to include usersNodesResources

--- pulse-core-psql/generate-data-for-mongodb.js
+++ pulse-core-psql/generate-data-for-mongodb.js
@@ -1,7 +1,7 @@
 const _ = require('lodash')
 const initializeTables = require('./initialize-tables')
 
-// const processRawUsersNodesResources = require('./process-users-nodes-resources')
+const processUsersNodesResources = require('./process-users-nodes-resources')
 const processUsersSitemaps = require('./process-users-sitemaps')
 
 const generateDataForMongoDb = async () => {
@@ -10,12 +10,10 @@ const generateDataForMongoDb = async () => {
     models: {
       User,
       Role,
-      Client,
       Node,
       RoleNode,
       RegionalBreakdown,
       Resource,
-      Permission,
     }
   } = await initializeTables()
 
@@ -26,67 +24,66 @@ const generateDataForMongoDb = async () => {
     Node,
   })
 
-  debugger
+  // get users.nodes.resources
+  const rawUsersNodesResources = await User.findAll(
+    {
+      duplicating: true,
+      required: true,
+      include: [
+        {
+          model: Role,
+          through: { attributes: [] },
+          duplicating: true,
+          required: true,
+          include: [
+            {
+              model: RoleNode,
+              duplicating: true,
+              required: true,
+              include: [
+                {
+                  model: Resource,
+                  duplicating: true,
+                  required: true,
+                  include: [
+                    {
+                      model: RegionalBreakdown,
+                      as: 'regionalBreakdown',
+                      duplicating: true,
+                      required: true,
+                      include: [
+                        {
+                          model: sequelize.models.us_states_regions,
+                          duplicating: true,
+                          required: true,
+                          as: 'bsr',
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            }
+          ],
+        },
+      ],
+    },
+  )
 
-  // // get users.nodes.resources
-  // const rawUsersNodesResources = await User.findAll(
-  //   {
-  //     duplicating: true,
-  //     required: true,
-  //     include: [
-  //       {
-  //         model: Role,
-  //         through: { attributes: [] },
-  //         duplicating: true,
-  //         required: true,
-  //         include: [
-  //           {
-  //             model: RoleNode,
-  //             duplicating: true,
-  //             required: true,
-  //             include: [
-  //               {
-  //                 model: Resource,
-  //                 duplicating: true,
-  //                 required: true,
-  //                 include: [
-  //                   {
-  //                     model: RegionalBreakdown,
-  //                     as: 'regionalBreakdown',
-  //                     duplicating: true,
-  //                     required: true,
-  //                     include: [
-  //                       {
-  //                         model: sequelize.models.us_states_regions,
-  //                         duplicating: true,
-  //                         required: true,
-  //                         as: 'bsr',
-  //                       }
-  //                     ]
-  //                   }
-  //                 ]
-  //               }
-  //             ]
-  //           }
-  //         ],
-  //       },
-  //     ],
-  //   },
-  // )
+  const allStates = await sequelize.models.us_state.findAll()
+  const allRegions = await sequelize.models.region.findAll()
+  const statesByKey = _.keyBy(allStates, 'id')
+  const regionsByKey = _.keyBy(allRegions, 'id')
 
-  // const allStates = await sequelize.models.us_state.findAll()
-  // const allRegions = await sequelize.models.region.findAll()
-  // const statesByKey = _.keyBy(allStates, 'id')
-  // const regionsByKey = _.keyBy(allRegions, 'id')
-
-  // const usersNodesResources = processRawUsersNodesResources({
-  //   rawUsersNodesResources,
-  //   statesByKey,
-  //   regionsByKey,
-  // })
+  const usersNodesResources = processUsersNodesResources({
+    rawUsersNodesResources,
+    statesByKey,
+    regionsByKey,
+  })
 
   return {
     usersSitemaps,
+    usersNodesResources,
   }
 }
 
