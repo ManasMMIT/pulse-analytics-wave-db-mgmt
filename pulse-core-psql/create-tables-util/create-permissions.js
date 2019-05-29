2a31f68bfc55196ff4c299d38ae2b75c52240744
Jon Lin <jon.cp.lin@gmail.com>
2019-05-29 02:13:52 -0400

    Update create-permissions util

--- pulse-core-psql/create-tables-util/create-permissions.js
+++ pulse-core-psql/create-tables-util/create-permissions.js
@@ -1,24 +1,24 @@
 const createPermissions = async ({
   sequelize,
-  RoleContent,
+  RoleNode,
   Resource,
   shouldSeed,
 }) => {
   const Permission = await sequelize.import('permission', require('./models/permission'))
 
-  RoleContent.belongsToMany(
+  RoleNode.belongsToMany(
     Resource,
     {
       through: Permission,
-      foreignKey: 'role_content_id',
+      foreignKey: 'role_node_id',
     },
   )
 
   Resource.belongsToMany(
-    RoleContent,
+    RoleNode,
     {
       through: Permission,
-      otherKey: 'role_content_id'
+      otherKey: 'role_node_id'
     },
   )
 
@@ -27,15 +27,15 @@ const createPermissions = async ({
 
     const adminPermissions = [
       {
-        resourceId: 2,
-        role_content_id: 15,
+        resourceId: '6e1aa22e-0b09-4e99-9878-3501a8b395d7',
+        role_node_id: '1693d51d-4101-4b9f-892c-e959f1142650',
       },
     ]
 
     const regeneronPermissions = [
       {
-        resourceId: 1,
-        role_content_id: 30,
+        resourceId: '7841e51c-c6ea-495d-af1b-3663a99d4647',
+        role_node_id: 'c9495c16-1999-4552-b5e7-c517b07d0586',
       },
     ]
 
