f4b7199d75037e08f35923ec30f5c2eb33e22b1f
Jon Lin <jon.cp.lin@gmail.com>
2019-05-30 14:47:22 -0400

    Add d3 and csvToJson to REPL global vars

--- pulse-core-psql/sequelizeConsole.js
+++ pulse-core-psql/sequelizeConsole.js
@@ -1,4 +1,6 @@
 const repl = require('repl')
+const d3 = require('d3-collection')
+const csvToJson = require('./create-tables-util/create-regional-tables/csv-to-json')
 const initializeTables = require('./initialize-tables')
 const chain = require('./query-tables-util')
 const Sequelize = require('sequelize')
@@ -14,6 +16,8 @@ initializeTables().then(({ models, sequelize }) => {
     prompt: 'sequelize > '
   })
 
+  replServer.context.d3 = d3
+  replServer.context.csvToJson = csvToJson
   replServer.context.chain = chain
   replServer.context.sequelize = sequelize
   replServer.context.Sequelize = Sequelize
