1dfa522e7ebbb86f84089de5c2a8800a33196fab
Jon Lin <jon.cp.lin@gmail.com>
2019-06-07 22:58:11 -0400

    Refactor consolidate-payer-data for readability, verify payer bucketing is correct

--- importHistoricalData/consolidate-payer-data.js
+++ importHistoricalData/consolidate-payer-data.js
@@ -1,22 +1,9 @@
 const _ = require('lodash')
 const d3 = require('d3-collection')
 const connectToMongoDb = require('../connect-to-mongodb')
-// const getLatestMonthYearProjectPipeline = require('./importProjectBasedData/latest-month-year-project')
 const {
   getScriptTerminator,
-  // latestMonthYearPipeline,
 } = require('../utils')
-
-const getLivesKey = (book, coverage) => {
-  if (book.includes('Medicare')) {
-    return _.camelCase(`medicare ${coverage}`)
-  }
-  return _.camelCase(`${book} ${coverage}`)
-}
-
-const getTreatmentPlanKey = d => (
-  `${d.indication}|${d.population}|${d.line}|${d.regimen}|${getLivesKey(d.book, d.coverage)}`
-)
 
 const consolidatePayerData = async (...args) => {
   let pulseDevDb
@@ -33,23 +20,48 @@ const consolidatePayerData = async (...args) => {
   }
 
   try {
-    // Entry point for joining data is latest month/year/project qoa data
-    let combinedPayerData = await pulseDevDb.collection('payerHistoricalQualityAccess')
-      .find().toArray()
+    /*
+      * * PLAN OF ATTACK * *
 
-    // Join access objects from master access list in pulse-core
-    let qualityAccessScores = await pulseCoreDb.collection('qualityAccessScores')
-      .find().toArray()
+      STEP 1:
+        Join all the latest month/year/project payer data together.
+        That means: 1. qoa 2. access scores 3. policy links 4. additional criteria.
+        That'll get us `combinedPayerData`.
 
-    qualityAccessScores = _.keyBy(qualityAccessScores, 'access')
+      STEP 2:
+        For every `treatmentPlan` in `combinedPayerData`
+        (`treatmentPlan` is combination of indication, population, line,
+        regimen, book, coverage), work in the DRG and MMIT state lives data so
+        we can get a result that's the breakdown of payers and their lives by access
+        category for every state for every `treatmentPlan`.
+    */
 
-    // warning: order of destructuring matters here! otherwise obj's _id is overwritten, resulting in dup keys
+    // fetch all the data we'll need to work with simultaneously upfront
+    const [
+      payerHistoricalQualityAccess,
+      qualityAccessScores,
+      payerHistoricalPolicyLinks,
+      payerHistoricalAdditionalCriteria,
+      payerHistoricalDrgStateLives,
+      payerHistoricalMmitStateLives,
+    ] = await Promise.all([
+      pulseDevDb.collection('payerHistoricalQualityAccess').find().toArray(),
+      pulseCoreDb.collection('qualityAccessScores').find().toArray(), // this master collection is in CORE
+      pulseDevDb.collection('payerHistoricalPolicyLinks').find().toArray(),
+      pulseDevDb.collection('payerHistoricalAdditionalCriteria').find().toArray(),
+      pulseDevDb.collection('payerHistoricalDrgStateLives').find().toArray(),
+      pulseDevDb.collection('payerHistoricalMmitStateLives').find().toArray(),
+    ])
+
+    // Starting point is payerHistoricalQualityAccess
+    let combinedPayerData = _.merge([], payerHistoricalQualityAccess)
+
+    // First join the access properties such as score to combinedPayerData
+    const accessScoresByAccess = _.keyBy(qualityAccessScores, 'access')
+    // WARNING: order of destructuring matters here! otherwise obj's _id is overwritten, resulting in dup keys
     combinedPayerData = combinedPayerData.map(obj => ({ ...qualityAccessScores[obj.access], ...obj }))
 
-    // Join latest month/year/project historical policy links
-    const payerHistoricalPolicyLinks = await pulseDevDb.collection('payerHistoricalPolicyLinks')
-      .find().toArray()
-
+    // Next join the policy links
     const policyLinksHash = _.keyBy(
       payerHistoricalPolicyLinks,
       d => `${d.slug} ${d.regimen} ${d.book} ${d.coverage}`
@@ -63,10 +75,7 @@ const consolidatePayerData = async (...args) => {
         }
     })
 
-    // Join latest month/year/project historical additional criteria
-    const payerHistoricalAdditionalCriteria = await pulseDevDb.collection('payerHistoricalAdditionalCriteria')
-      .find().toArray()
-
+    // Join the additional criteria
     const addlCriteriaHash = _.groupBy(
       payerHistoricalAdditionalCriteria,
       d => `${d.slug} ${d.indication} ${d.population} ${d.line} ${d.coverage} ${d.book} ${d.regimen}`
@@ -89,42 +98,107 @@ const consolidatePayerData = async (...args) => {
       }
     })
 
-    // // just to make it easier to see the collection output; not yet used directly by APP or API
-    // await pulseCoreDb.collection('payerHistoricalCombinedData').deleteMany()
-    // await pulseCoreDb.collection('payerHistoricalCombinedData').insertMany(combinedPayerData)
+    // * Write the combinedPayerData to MongoDB for future use
+    // await pulseDevDb.collection('payerHistoricalCombinedData').deleteMany()
+    // await pulseDevDb.collection('payerHistoricalCombinedData').insertMany(combinedPayerData)
     // console.log(`pulse-core collection 'payerHistoricalCombinedData' updated`)
 
-    // a 'treatmentPlan' refers to the combo of indication, regimen, population, etc.
+    // * We now move onto Step 2, the state lives side of things
+
     const payerDataGroupedByTreatmentPlan = d3.nest()
       .key(getTreatmentPlanKey)
       .rollup(arr => _.keyBy(arr, 'slug'))
       .object(combinedPayerData)
 
-    // get latest month/year data for DRG and MMIT state lives
-    const payerHistoricalDrgStateLives = await pulseDevDb.collection('payerHistoricalDrgStateLives')
-      .find().toArray()
+    function getTreatmentPlanKey(d) {
+      return `${d.indication}|${d.population}|${d.line}|${d.regimen}|${getLivesKey(d.book, d.coverage)}`
+    }
 
-    const payerHistoricalMmitStateLives = await pulseDevDb.collection('payerHistoricalMmitStateLives')
-      .find().toArray()
+    function getLivesKey(book, coverage) {
+      if (book.includes('Medicare')) {
+        return _.camelCase(`medicare ${coverage}`)
+      }
+      return _.camelCase(`${book} ${coverage}`)
+    }
 
-    const drgGroupedBySlug = d3.nest()
-      .key(d => d.state)
-      .key(d => d.slug)
-      .rollup(arr => arr[0])
-      .object(payerHistoricalDrgStateLives)
+    // group the payers and their lives data by state
+    // TODO: add validation to make sure same payer doesn't appear more than once for a given state
+    const LIVES_DATA_drgPayersByState = _.groupBy(payerHistoricalDrgStateLives, 'state')
+    const LIVES_DATA_mmitPayersByState = _.groupBy(payerHistoricalMmitStateLives, 'state')
 
-    const mmitGroupedBySlug = d3.nest()
-      .key(d => d.state)
-      .key(d => d.slug)
-      .rollup(arr => arr[0])
-      .object(payerHistoricalMmitStateLives)
+    /*
+      Add a full set of states and lives data (both MMIT and DRG) to each treatmentPlan,
+      while bucketing the payers for each state by access category using the
+      data in the `combinedPayerDataBySlug`.
+    */
+    const payerDataWithLives = _.map(payerDataGroupedByTreatmentPlan, generateStateLivesData)
 
-    const bucketizeLivesData = (livesGroupedBySlug, payerAccessHash) => {
-      const result = _.map(livesGroupedBySlug, (payers, state) => {
-        const accessBuckets = _.groupBy(payers, ({ slug }) => {
-          const access = payerAccessHash[slug] ? payerAccessHash[slug].access : 'Not Audited'
-          return access
-        })
+    function generateStateLivesData(combinedPayerDataBySlug, treatmentPlan) {
+      const livesType = _.last(treatmentPlan.split('|'))
+
+      const DRG_statesData = bucketizeLivesData({
+        livesData: LIVES_DATA_drgPayersByState,
+        combinedPayerDataBySlug,
+        livesType,
+      })
+
+      const MMIT_statesData = bucketizeLivesData({
+        livesData: LIVES_DATA_mmitPayersByState,
+        combinedPayerDataBySlug,
+        livesType,
+      })
+
+      return {
+        treatmentPlan,
+        DRG_statesData,
+        MMIT_statesData,
+      }
+    }
+
+    function bucketizeLivesData({
+      livesData,
+      combinedPayerDataBySlug,
+      livesType,
+    }) {
+      const result = _.map(livesData, (payers, state) => {
+        /*
+          Note: Some commented out code has been left in below in case
+          we need to add in payers who aren't profiled in QOA data. But there's
+          a lot of them. Not sure if meaningful to user.
+        */
+
+        // let notAuditedPayers = []
+        let payersWithAccessAdded = _.reduce(payers, (acc, payerObj) => {
+          const { slug } = payerObj
+
+          // skip payers who aren't profiled in qoa data for the given treatment plan
+          if (!combinedPayerDataBySlug[slug]) {
+            // notAuditedPayers.push(payerObj)
+            return acc
+          }
+
+          // also skip the payer if the lives for the livesType is 0 or absent
+          if (!Number(payerObj[livesType])) return acc
+
+          const combinedPayerData = combinedPayerDataBySlug[slug]
+          acc.push({ ...payerObj, ...combinedPayerData })
+
+          return acc
+        }, [])
+
+        /*
+          notAuditedPayers = notAuditedPayers.map(payerObj => ({
+            ...payerObj,
+            access: 'Not Audited',
+            color: 'gray',
+            // other properties needed for not audited
+          }))
+
+          payersWithAccessAdded = payersWithAccessAdded.concat(notAuditedPayers)
+        */
+
+        // group the payers by access
+        const accessBuckets = _.groupBy(payersWithAccessAdded, 'access')
 
         return {
           state,
@@ -135,28 +209,10 @@ const consolidatePayerData = async (...args) => {
       return result
     }
 
-    // const allPayersAcrossDrgMmit = _.merge(drgGroupedBySlug, mmitGroupedBySlug)
-
-    // For every treatmentPlan, each payer has a given access category, provided in the payerAccessHash.
-    // Add a full set of states and lives data (both MMIT and DRG) to each treatmentPlan,
-    // while bucketizing the payers associated with each state by access category.
-    const payerDataWithLives = _.map(payerDataGroupedByTreatmentPlan, (payerAccessHash, treatmentPlan) => {
-      const DRG_statesData = bucketizeLivesData(drgGroupedBySlug, payerAccessHash)
-      const MMIT_statesData = bucketizeLivesData(mmitGroupedBySlug, payerAccessHash)
-
-      return {
-        treatmentPlan,
-        DRG_statesData,
-        MMIT_statesData,
-      }
-    })
-
-    debugger
-
   } catch (e) {
     console.error(e)
   } finally {
-    console.log('Script finished executing')
+    console.log('Script terminating...')
     await terminateScript()
   }
 }
@@ -164,19 +220,17 @@ const consolidatePayerData = async (...args) => {
 // module.exports = consolidatePayerData
 consolidatePayerData()
 
+/*
+Saving snippet below for potential future use:
+Join whether account has been profiled
 
-// Saving snippet below for potential future use:
-// Join whether account has been profiled
+const profiledPayerHash = _.keyBy(payerOrganizationOverview, 'slug')
 
-// const payerOrganizationOverview = await pulseDevDb.collection('payerOrganizationOverview')
-//   .find().toArray()
-
-// const profiledPayerHash = _.keyBy(payerOrganizationOverview, 'slug')
-
-// combinedPayerData.forEach(obj => {
-//   if (profiledPayerHash[obj.slug]) {
-//     obj.isProfiled = true
-//   } else {
-//     obj.isProfiled = false
-//   }
-// })
+combinedPayerData.forEach(obj => {
+  if (profiledPayerHash[obj.slug]) {
+    obj.isProfiled = true
+  } else {
+    obj.isProfiled = false
+  }
+})
+*/
