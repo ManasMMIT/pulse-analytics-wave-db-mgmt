8b6bb5320a13e227b609019750472d83d6311b41
Jon Lin <jon.cp.lin@gmail.com>
2019-06-10 11:05:42 -0400

    Get combine lives to calc restrictive lives, bucket and sort payers by access for each state

--- importHistoricalData/consolidate-payer-data/combine-lives.js
+++ importHistoricalData/consolidate-payer-data/combine-lives.js
@@ -2,8 +2,11 @@ const _ = require('lodash')
 const d3 = require('d3-collection')
 const connectionWrapper = require('./connection-wrapper')
 
+const RESTRICTIVE_SCORE_THRESHOLD = 11
+
 let combineLives = async ({
   pulseDevDb,
+  pulseCoreDb,
   terminateScript,
   payerHistoricalCombinedData,
 }) => {
@@ -12,10 +15,14 @@ let combineLives = async ({
     const [
       payerHistoricalDrgStateLives,
       payerHistoricalMmitStateLives,
+      payerDrgStateLivesTotals,
+      payerMmitStateLivesTotals,
       combinedPayerData,
     ] = await Promise.all([
       pulseDevDb.collection('payerHistoricalDrgStateLives').find().toArray(),
       pulseDevDb.collection('payerHistoricalMmitStateLives').find().toArray(),
+      pulseCoreDb.collection('payerDrgStateLivesTotals').find().toArray(),
+      pulseCoreDb.collection('payerMmitStateLivesTotals').find().toArray(),
       payerHistoricalCombinedData || pulseDevDb.collection('payerHistoricalCombinedData').find().toArray()
     ])
 
@@ -46,7 +53,13 @@ let combineLives = async ({
       Add a full set of states and lives data (both MMIT and DRG) to each treatmentPlan,
       while bucketing the payers for each state by access category using the
       data in the `combinedPayerDataBySlug`.
+
+      In the process of bucketing the payers, calculate lives percentages for payers as
+      well as restrictive lives percentages.
     */
+    const drgStatesLivesTotalsByState = _.keyBy(payerDrgStateLivesTotals, 'state')
+    const mmitStatesLivesTotalsByState = _.keyBy(payerMmitStateLivesTotals, 'state')
+
     const payerDataWithStateLives = _.map(payerDataGroupedByTreatmentPlan, generateStateLivesData)
 
     function generateStateLivesData(combinedPayerDataBySlug, treatmentPlan) {
@@ -56,12 +69,14 @@ let combineLives = async ({
         livesData: LIVES_DATA_drgPayersByState,
         combinedPayerDataBySlug,
         livesType,
+        livesTotalsByState: drgStatesLivesTotalsByState,
       })
 
       const MMIT_statesData = bucketizeLivesData({
         livesData: LIVES_DATA_mmitPayersByState,
         combinedPayerDataBySlug,
         livesType,
+        livesTotalsByState: mmitStatesLivesTotalsByState,
       })
 
       return {
@@ -75,6 +90,7 @@ let combineLives = async ({
       livesData,
       combinedPayerDataBySlug,
       livesType,
+      livesTotalsByState,
     }) {
       const result = _.map(livesData, (payers, state) => {
         /*
@@ -83,41 +99,84 @@ let combineLives = async ({
           a lot of them. Not sure if meaningful to user.
         */
 
-        // let notAuditedPayers = []
-        let payersWithAccessAdded = _.reduce(payers, (acc, payerObj) => {
-          const { slug } = payerObj
+        let notAuditedPayers = []
+        let auditedLives = 0 // total audited lives for the current state
+        const totalLivesForStateAndLivesType = livesTotalsByState[state][livesType]
+        let restrictiveLivesPercent = 0
 
+        let payersWithAccessAdded = _.reduce(payers, (acc, payerObj) => {
+          const lives = Number(payerObj[livesType])
+          // skip the payer if the lives for the livesType is 0 or absent
+          if (!lives) return acc
+
+          const { slug } = payerObj
           // skip payers who aren't profiled in qoa data for the given treatment plan
-          if (!combinedPayerDataBySlug[slug]) {
-            // notAuditedPayers.push(payerObj)
+          const qoaDataForSlug = combinedPayerDataBySlug[slug]
+          if (!qoaDataForSlug) {
+            notAuditedPayers.push(payerObj)
             return acc
           }
 
-          // also skip the payer if the lives for the livesType is 0 or absent
-          if (!Number(payerObj[livesType])) return acc
+          let livesPercent = 0
+          if (totalLivesForStateAndLivesType) { // to prevent division by 0
+            livesPercent = lives / totalLivesForStateAndLivesType
+          }
+
+          if (Number(qoaDataForSlug.score) >= RESTRICTIVE_SCORE_THRESHOLD) {
+            restrictiveLivesPercent += livesPercent
+          }
+
+          auditedLives += lives
 
           const combinedPayerData = combinedPayerDataBySlug[slug]
-          acc.push({ ...payerObj, ...combinedPayerData })
+          acc.push({ ...combinedPayerData, livesPercent, livesRaw: lives })
 
           return acc
         }, [])
 
         /*
-          notAuditedPayers = notAuditedPayers.map(payerObj => ({
-            ...payerObj,
-            access: 'Not Audited',
-            color: 'gray',
-            // other properties needed for not audited
-          }))
+        notAuditedPayers = notAuditedPayers.map(payerObj => ({
+          ...payerObj,
+          access: 'Not Audited',
+          // other properties needed for not audited
+          // color: 'gray',
+        }))
 
-          payersWithAccessAdded = payersWithAccessAdded.concat(notAuditedPayers)
+        payersWithAccessAdded = payersWithAccessAdded.concat(notAuditedPayers)
         */
 
-        // group the payers by access
-        const accessBuckets = _.groupBy(payersWithAccessAdded, 'access')
+        // group the payers by access, restructure as needed
+        let accessBuckets = d3.nest()
+          .key(d => d.access)
+          .rollup(arr => {
+            const livesRaw = arr.reduce((acc, payerObj) => acc + payerObj.livesRaw, 0)
+            const livesPercent = livesRaw / totalLivesForStateAndLivesType
+
+            return {
+              access: arr[0].access,
+              score: Number(arr[0].score),
+              color: arr[0].color,
+              livesRaw,
+              livesPercent,
+              payers: arr
+            }
+          })
+          .object(payersWithAccessAdded)
+
+        accessBuckets = Object.values(accessBuckets)
+        accessBuckets = _.orderBy(accessBuckets, ['score'], ['desc'])
+
+        let auditedLivesPercent = 0
+        if (totalLivesForStateAndLivesType) { // to prevent division by 0
+          auditedLivesPercent = auditedLives / totalLivesForStateAndLivesType
+        }
 
         return {
           state,
+          auditedLives,
+          totalLives: totalLivesForStateAndLivesType,
+          auditedLivesPercent,
+          restrictiveLivesPercent,
           accessBuckets,
         }
       })
@@ -134,3 +193,16 @@ let combineLives = async ({
 
 combineLives = connectionWrapper(combineLives)
 module.exports = combineLives
+combineLives()
+
+
+  // "indication": "NSCLC",
+  // "population": "No Subtype Specified",
+  // "line": "1L+",
+  // "regimen": "Keytruda",
+  // "book": "Commercial",
+  // "coverage": "Medical",
+  // "treatmentPlan": "NSCLC|No Subtype Specified|1L+|Keytruda|commercialMedical",
+  // "auditedLives": 160078898,
+  // "totalLives": 169395659,
+  // "auditedLivesPercent": 0.945,
