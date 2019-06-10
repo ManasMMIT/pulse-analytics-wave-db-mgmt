162342abeeb944dad5c6d91e3a3ba173838e80cc
Jon Lin <jon.cp.lin@gmail.com>
2019-06-10 09:07:23 -0400

    Add step to calc state lives totals on import, save to separate collections

--- importHistoricalData/importNonProjectBasedData/pushToDev.js
+++ importHistoricalData/importNonProjectBasedData/pushToDev.js
@@ -1,4 +1,5 @@
 const { latestMonthYearPipeline } = require('../../utils')
+const { getStateLivesTotal } = require('./get-payer-lives')
 
 const pushToDev = async ({
   collectionName,
@@ -17,6 +18,20 @@ const pushToDev = async ({
     await pulseDevDb.collection(collectionName).insertMany(latestMonthYearData)
 
     console.log(`pulse-dev collection '${collectionName}' updated to only contain month: ${month}, year: ${year}`)
+
+    let stateLivesTotals
+    if (collectionName === 'payerHistoricalDrgStateLives') {
+      statesLivesTotals = getStateLivesTotal(latestMonthYearData)
+      await pulseCoreDb.collection('payerDrgStateLivesTotals').deleteMany()
+      await pulseCoreDb.collection('payerDrgStateLivesTotals').insertMany(statesLivesTotals)
+      console.log(`pulse-core collection 'payerDrgStateLivesTotals' updated`)
+    } else if (collectionName === 'payerHistoricalMmitStateLives') {
+      // TODO: if it's MMIT lives data, you also have to resync the MMIT lives with the DRG lives
+      statesLivesTotals = getStateLivesTotal(latestMonthYearData)
+      await pulseCoreDb.collection('payerMmitStateLivesTotals').deleteMany()
+      await pulseCoreDb.collection('payerMmitStateLivesTotals').insertMany(statesLivesTotals)
+      console.log(`pulse-core collection 'payerMmitStateLivesTotals' updated`)
+    }
   } catch(e) {
     console.error(e)
   } finally {
