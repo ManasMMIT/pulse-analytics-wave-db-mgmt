f14026aa618351b1aa24362738ec1db87fabf52d
Matt Van Dyke <matthewlvandyke@gmail.com>
2019-07-15 14:06:53 -0400

    Postgresql Basic CRUD (#22)
    * Setup find client(s) postgresql crud PULS-3846
    
    * Setup PostgreSQL CREATE Client - PULS-3847
    
    * Create sequelize.j to load models synchronously
    
    * Replace endpoint model implementation to use directly imported models
    
    * Remove sequelize middleware from server
    
    * Delete psql folder
    
    * Setup Delete Client PULS-3849
    
    * Setup Edit Client - PULS-3848
    
    * Add bodyparser middleware
    
    * Switch from query to body req vars
    
    * Fill in all basic PostgreSQL routes
    
    * Copy all models into backend/models
    
    * Scrub console logs
    
    * Add example of PostgreSQL transactional coverage

--- admin-hub/phoenix/backend/index.js
+++ admin-hub/phoenix/backend/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
-const _ = require('lodash')
+const bodyParser = require('body-parser')
+
 const routes = require('./routes')
 
 const app = express()
 const port = 3000
 
+app.use(bodyParser.urlencoded({ extended: false }))
+
 app.use('/api', routes)
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`PHOENIX ONLINE. PORT ${port}!`))
--- admin-hub/phoenix/backend/routes.js
+++ admin-hub/phoenix/backend/routes.js
@@ -1,87 +1,251 @@
 const express = require('express')
+
+// ? temporary until we change the user model via sequelize migration
+const uuid = require('uuid/v4')
+
 const auth0 = require('./auth0')
-const psql = require('./psql')
+const {
+  sequelize,
+  models: {
+    Client,
+    User,
+    Role,
+  }
+} = require('./sequelize')
 
 const subApp = express()
 
-subApp.get('/test', (req, res) => {
-  res.send('yo')
-})
-
 subApp.get('/clients', async (req, res) => {
-  auth0.clients.find()
-  psql.clients.find()
-})
-
-subApp.get('/clients/:id', async (req, res) => {
-  const { id } = req.params
+  const clients = await Client.findAll()
   
-  auth0.clients.find(id)
-  psql.clients.find(id)
+  res.json(clients)
 })
 
-subApp.post('/clients', async (req, res) => {
-  psql.clients.create()
-    .then(client => {
-      auth0.clients.create(client)
+subApp.get('/clients/:id', async ({ params }, res) => { 
+  // auth0.clients.find(id)
+  const client = await Client.findByPk(params.id)
+
+  res.json(client)
+})
+
+subApp.post('/clients', async ({
+  body: { name, description },
+}, res) => {
+  if (!Boolean(name)) {
+    res.send('A name value is required when creating a client')
+    return
+  }
+  const client = await Client.create({ name, description })
+    // .then(client => {
+      // auth0.clients.create(client)
+    // })
+  res.json(client)
+})
+
+subApp.patch('/clients/:id', async ({
+  params: { id },
+  body: {
+    name,
+    description,
+  },
+}, res) => {
+  // auth0.clients.update(id)
+  if (!Boolean(name)) {
+    res.send('name field invalid')
+    return
+  } else if (!Boolean(description)) {
+    res.send('description field invalid')
+    return
+  }
+
+  const client = await Client.findByPk(id)
+
+  if (!Boolean(client)) {
+    res.send(`client with id ${ id } doesn't exist`)
+    return
+  }
+
+  await client.update({
+    name,
+    description,
+  })
+
+  res.json(client)
+})
+
+subApp.delete('/clients/:id', async ({
+  params: { id },
+}, res) => {
+  // auth0.clients.delete(id)
+  const client = await Client.findByPk(id)
+  await client.destroy()
+
+  res.send(`${ client.name } roasted by the Phoenix`)
+})
+
+subApp.get('/clients/:clientId/roles', async ({
+  params: { clientId },
+}, res) => {
+  const client = await Client.findByPk(clientId)
+  const clientRoles = await client.getRoles()
+
+  res.json(clientRoles)
+})
+
+subApp.get('/roles/:roleId/clients', async ({
+  params: { roleId },
+}, res) => {
+  const role = await Role.findByPk(roleId)
+  const roleClients = await role.getClients()
+
+  res.json(roleClients)
+})
+
+subApp.get('/roles', async (req, res) => {
+  // auth0.roles.find()
+  const roles = await Role.findAll()
+
+  res.json(roles)
+})
+
+subApp.get('/roles/:id', async ({
+  params: { id },
+}, res) => {
+  // auth0.roles.find()
+  const role = await Role.findByPk(id)
+
+  res.json(role)
+})
+
+// TODO: Wrap in transaction
+subApp.post('/roles', async ({
+  body: { name, description, clientId },
+}, res) => {
+  if (!Boolean(name)) {
+    res.send('name field invalid')
+    return
+  }
+  // auth0.roles.find()
+  const clientIdArr = clientId
+    ? [clientId]
+    : null
+
+  const role = await sequelize.transaction(t => {
+    return Role.create({
+      name,
+      description,
+    }, {transaction: t})
+    .then(role => {
+      role.setClients(
+        [clientId],
+        { transaction: t }
+      )
+      return role
     })
+  })
+  .then(role => role)
+  .catch(err => res.send(err))
+  // const role = await Role.create({ name, description })
+
+  // await role.setClients([clientId])
+  res.json(role)
 })
 
-subApp.patch('/clients/:id', async (req, res) => {
-  const { id } = req.params
+subApp.patch('/roles/:id', async ({
+  params: { id },
+  body: { name, description },
+}, res) => {
+  // auth0.roles.find()
+  
+  const role = await Role.findByPk(id)
 
-  auth0.clients.update(id)
-  psql.clients.update(id)
+  await role.update({ name, description })
+
+  res.json(role)
 })
 
-subApp.delete('/clients/:id', async (req, res) => {
-  const { id } = req.params
+subApp.delete('/roles/:id', async ({
+  params: { id },
+}, res) => {
+  // auth0.roles.find()
+  const role = await Role.findByPk(id)
 
-  auth0.clients.delete(id)
-  psql.clients.delete(id)
+  await role.destroy(id)
+
+  res.send(`${ role.name } role torched by the Phoenix`)
 })
 
-// ! Next Steps:
-// subApp.get('/roles', (req, res) => {
-//   auth0.roles.find()
-//   psql.roles.find()
-// })
+subApp.get('/roles/:roleId/users', async ({
+  params: { roleId },
+}, res) => {
+  // auth0.users.find()
+  const role = await Role.findByPk(roleId)
+  const roleUsers = await role.getUsers()
 
-// subApp.post('/roles', (req, res) => {
-//   auth0.roles.find()
-//   psql.roles.find()
-// })
+  res.json(roleUsers)
+})
 
-// subApp.patch('/roles', (req, res) => {
-//   auth0.roles.find()
-//   psql.roles.find()
-// })
+subApp.get('/users/:userId/roles', async ({
+  params: { userId },
+}, res) => {
+  const user = await User.findByPk(userId)
+  const userRoles = await user.getRoles()
 
-// subApp.delete('/roles', (req, res) => {
-//   auth0.roles.find()
-//   psql.roles.find()
-// })
+  res.json(userRoles)
+})
 
-// subApp.get('/users', (req, res) => {
-//   auth0.users.find()
-//   psql.users.find()
-// })
+subApp.get('/users', async (req, res) => {
+  // auth0.users.find()
+  const users = await User.findAll()
 
-// subApp.post('/users', (req, res) => {
-//   // create own uuid for user and save in psql and auth0
+  res.json(users)
+})
 
-//   auth0.users.find()
-//   psql.users.find()
-// })
+subApp.get('/users/:id', async ({
+  params: { id },
+}, res) => {
+  // auth0.users.find()
+  const user = await User.findByPk(id)
 
-// subApp.patch('/users', (req, res) => {
-//   auth0.users.find()
-//   psql.users.find()
-// })
+  res.json(user)
+})
 
-// subApp.delete('/users', (req, res) => {
-//   auth0.users.find()
-//   psql.users.find()
-// })
+subApp.post('/users', async ({
+  body: { username },
+}, res) => {
+  // create own uuid for user and save in psql and auth0
+  // auth0.users.find()
+  if (!Boolean(username)) {
+    res.send('username field invalid')
+    return
+  }
+  const id = uuid()
+  const user = await User.create({ id, username })
+
+  res.json(user)
+})
+
+subApp.patch('/users/:id', async ({
+  params: { id },
+  body: { username },
+}, res) => {
+  // auth0.users.find()
+  const user = await User.findByPk(id)
+
+  await user.update({ username })
+
+  res.json(user)
+})
+
+subApp.delete('/users/:id', async ({
+  params: { id },
+}, res) => {
+  // auth0.users.find()
+  const user = await User.findByPk(id)
+
+  await user.destroy()
+
+  res.send(`${ user.username } fell prey to the Phoenix`)
+})
 
 module.exports = subApp
