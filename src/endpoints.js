85cf8c786af85cded05b29760523c00983b37d45
Matt Van Dyke <matthewlvandyke@gmail.com>
2019-07-18 21:15:42 -0400

    Add editClient frontend endpoint

--- src/endpoints.js
+++ src/endpoints.js
@@ -9,3 +9,9 @@ export const getAllTeamUsers = selectedTeam => fetch(`api/roles/${selectedTeam}/
 
 export const getAllClientRoles = clientId => fetch(`api/clients/${clientId}/roles`)
   .then(response => response.json());
+
+export const editClient = (id, name) => fetch(`api/clients/${ id }`, {
+  method: 'PATCH',
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ name })
+}).then(response => response.json())
