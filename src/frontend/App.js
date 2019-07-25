3248038b0187ba97e0a8de3d7f905dee389af156
Jon Lin <jon.cp.lin@gmail.com>
2019-07-25 17:13:41 -0400

    Load up App with ApolloProvider and pass graphql query data to Phoenix

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,10 +1,54 @@
 import React from 'react'
+
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { RestLink } from 'apollo-link-rest'
+import { Query, ApolloProvider } from 'react-apollo'
+import gql from 'graphql-tag'
+
+import resolvers from './resolvers'
+import typeDefs from './typeDefs'
 import Phoenix from './Phoenix'
 
-const App = () => (
-  <div>
-    <Phoenix />
-  </div>
-)
+const cache = new InMemoryCache()
+
+const restLink = new RestLink({ uri: 'api' })
+
+const client = new ApolloClient({
+  cache,
+  link: restLink,
+  resolvers,
+  typeDefs,
+})
+
+const GET_INITIAL_STATE = gql`
+  query initialState {
+    clients @rest(type: "Client", path: "/clients") {
+      id
+      name
+      description
+    }
+    selectedClient @client {
+      id
+      name
+      description
+    }
+    # teams @client {
+    #   id
+    #   name
+    #   description
+    # }
+  }
+`
+
+const App = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Query query={GET_INITIAL_STATE}>
+        {({ loading, data }) => loading || <Phoenix data={data} />}
+      </Query>
+    </ApolloProvider>
+  )
+}
 
 export default App
