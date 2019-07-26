473d680bfa0af057d4ee47c4a06110ceba8565c9
Jon Lin <jon.cp.lin@gmail.com>
2019-07-26 15:15:06 -0400

    Get selectedTeam resolver working

--- src/frontend/api/resolvers.js
+++ src/frontend/api/resolvers.js
@@ -1,6 +1,8 @@
 import {
   GET_CLIENTS,
-  GET_SELECTED_CLIENT
+  GET_SELECTED_CLIENT,
+  GET_CLIENT_TEAMS,
+  GET_SELECTED_TEAM,
 } from './queries'
 
 const resolvers = {
@@ -13,7 +15,6 @@ const resolvers = {
   Mutation: {
     selectedClient: (_, { id }, { cache }) => {
       const { clients } = cache.readQuery({ query: GET_CLIENTS })
-      debugger
 
       let selectedClient = clients[0]
 
@@ -23,6 +24,33 @@ const resolvers = {
 
       cache.writeQuery({ query: GET_SELECTED_CLIENT, data: { selectedClient } })
       return selectedClient
+    },
+    selectedTeam: async (_, { id }, { cache, client }) => {
+      const { selectedClient: { id: clientId } } = cache.readQuery({ query: GET_SELECTED_CLIENT })
+
+      const queryObjForClientTeams = {
+        query: GET_CLIENT_TEAMS,
+        variables: { clientId },
+      }
+
+      // TODO: make the following try...catch into a util
+      let teams
+      try {
+        const data = cache.readQuery(queryObjForClientTeams)
+        teams = data.teams
+      } catch(e) {
+        const response = await client.query(queryObjForClientTeams)
+        teams = response.data.teams
+      }
+
+      let selectedTeam = teams[0]
+
+      if (id) {
+        selectedTeam = teams.find(({ id: teamId }) => id === teamId)
+      }
+
+      cache.writeQuery({ query: GET_SELECTED_TEAM, data: { selectedTeam } })
+      return selectedTeam
     },
   },
   // Mutation: {
