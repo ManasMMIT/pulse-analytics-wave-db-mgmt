62e13e9a0cd404a13df8d3789f79a8803baa0582
Jon Lin <jon.cp.lin@gmail.com>
2019-07-26 13:41:02 -0400

    Write out basic mutations functions

---
+++ src/frontend/api/mutations.js
@@ -0,0 +1,31 @@
+import gql from 'graphql-tag'
+
+export const SELECT_CLIENT = gql`
+  mutation SelectClient($id: String) {
+    selectedClient(id: $id) @client {
+      id
+      name
+      description
+    }
+  }
+`
+
+export const SELECT_TEAM = gql`
+  mutation SelectTeam($id: String) {
+    selectedTeam(id: $id) @client {
+      id
+      name
+      description
+    }
+  }
+`
+
+export const SELECT_USER = gql`
+  mutation SelectUser($id: String) {
+    selectedUser(id: $id) @client {
+      id
+      username
+      email
+    }
+  }
+`
