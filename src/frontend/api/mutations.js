f142a97561a67e10320cf32ec2990915e808e09a
Jon Lin <jon.cp.lin@gmail.com>
2019-07-29 21:40:48 -0400

    Add CREATE_CLIENT mutation

--- src/frontend/api/mutations.js
+++ src/frontend/api/mutations.js
@@ -10,6 +10,16 @@ export const SELECT_CLIENT = gql`
   }
 `
 
+export const CREATE_CLIENT = gql`
+  mutation CreateClient($description: String) {
+    createdClient(description: $description) @client {
+      id
+      name
+      description
+    }
+  }
+`
+
 export const SELECT_TEAM = gql`
   mutation SelectTeam($id: String) {
     selectedTeam(id: $id) @client {
