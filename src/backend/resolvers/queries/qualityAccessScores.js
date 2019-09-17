9c31073e13ac5844853e069d9ab90c07fd37176b
Matt Van Dyke <matthewlvandyke@gmail.com>
2019-09-16 21:45:24 -0400

    Sort QoA scores by score then sortOrder

--- src/backend/resolvers/queries/qualityAccessScores.js
+++ src/backend/resolvers/queries/qualityAccessScores.js
@@ -3,7 +3,14 @@ const _ = require('lodash')
 const qualityAccessScores = async (parent, args, { pulseCoreDb }, info) => {
   const qualityAccessScores = await pulseCoreDb.collection('qualityAccessScores').find().toArray()
 
-  const sortedQualityAccessScores = _.sortBy(qualityAccessScores, ({ access }) => parseInt(access))
+  const sortedQualityAccessScores = _.orderBy(
+    qualityAccessScores,
+    [
+      ({ score }) => parseInt(score),
+      ({ sortOrder }) => parseInt(sortOrder)
+    ],
+    ['desc', 'asc']
+  )
 
   return sortedQualityAccessScores
 }
