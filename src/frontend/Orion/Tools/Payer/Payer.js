3598e5487e02cb4c769252205df07626f56b35da
Matt Van Dyke <matthewlvandyke@gmail.com>
2019-09-18 11:26:47 -0400

    WIP Scaffold PayerAccounts Panel

--- src/frontend/Orion/Tools/Payer/Payer.js
+++ src/frontend/Orion/Tools/Payer/Payer.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 
 import QualityAccessScorePanel from './QualityAccessScorePanel'
+import Accounts from './Accounts'
 
 const Payer = () => (
   <Switch>
+    <Route path={'/orion/tools/payer/accounts'} component={Accounts} />
     <Route path={'/orion/tools/payer/scores'} component={QualityAccessScorePanel} />
-    <Redirect to={'/orion/tools/payer/scores'} />
+    <Redirect to={'/orion/tools/payer/accounts'} />
   </Switch>
 )
 
