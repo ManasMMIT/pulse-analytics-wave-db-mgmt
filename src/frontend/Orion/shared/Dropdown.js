c50402b97c2c70033f13906efb208bc397f9cc18
Matt Van Dyke <matthewlvandyke@gmail.com>
2019-09-18 10:43:56 -0400

    Adjust basic Dropdown styles for arrow icon

--- src/frontend/Orion/shared/Dropdown.js
+++ src/frontend/Orion/shared/Dropdown.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons"
 
 const Dropdown = ({ style, label, children }) => {
   const [show, toggleShow] = useState(false)
@@ -6,8 +8,11 @@ const Dropdown = ({ style, label, children }) => {
 
   return (
     <>
-      <div style={style} onClick={handleClick}>
-        {label}
+      <div style={{ display: 'flex', justifyContent: 'space-between', ...style }} onClick={handleClick}>
+        <span>{label}</span>
+        <span>
+          <FontAwesomeIcon size="lg" icon={faCaretDown} />
+        </span>
       </div>
       {
         show && (
