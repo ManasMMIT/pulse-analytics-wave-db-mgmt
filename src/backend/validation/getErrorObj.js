576312e172655b8141a15a103702b3cbc092497e
Matt Van Dyke <matthewlvandyke@gmail.com>
2019-09-21 14:13:48 -0400

    Refactor getErrorObj to return flat array

--- src/backend/validation/getErrorObj.js
+++ src/backend/validation/getErrorObj.js
@@ -3,23 +3,12 @@ const _ = require('lodash')
 const getProblemRowsByType = require('./getProblemRowsByType')
 
 const getErrorObj = async (rawJson, core) => {
-  const problemRows = await getProblemRowsByType(
-    rawJson,
-    core
-  )
+  const problemRows = await getProblemRowsByType(rawJson, core)
 
-  const errorObj = Object.keys(problemRows).reduce((acc, field) => {
-    const problemRowsForField = problemRows[field]
-
-    acc[field] = ({
-      message: `${ _.capitalize(field) } validation failed`,
-      problemRows: problemRowsForField,
-    })
-
-    return acc
-  }, {})
-
-  return errorObj
+  return Object.keys(problemRows).map(field => ({
+    message: `${ _.capitalize(field) } validation failed`,
+    problemRows: problemRows[field],
+  }))
 }
 
 module.exports = getErrorObj
