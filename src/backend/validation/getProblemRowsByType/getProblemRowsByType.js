1e9ba88405702f607b01b3a3e784f350caa9d61e
Matt Van Dyke <matthewlvandyke@gmail.com>
2019-09-23 15:28:40 -0400

    Remove empty problem row slices

--- src/backend/validation/getProblemRowsByType/getProblemRowsByType.js
+++ src/backend/validation/getProblemRowsByType/getProblemRowsByType.js
@@ -1,10 +1,19 @@
+const _ = require('lodash')
+
 const getValidFieldsByType = require('./getValidFieldsByType')
 const formatProblemRowsByType = require('./formatProblemRowsByType')
 
 const getProblemRowsByType = async (data, core) => {
   const validFieldsByType = await getValidFieldsByType(core)
 
-  return formatProblemRowsByType(validFieldsByType, data)
+  const formattedProblemRowsByType = formatProblemRowsByType(validFieldsByType, data)
+  for (const type in formattedProblemRowsByType) {
+    if (_.isEmpty(formattedProblemRowsByType[type])) {
+      delete formattedProblemRowsByType[type]
+    }
+  }
+
+  return formattedProblemRowsByType
 }
 
 module.exports = getProblemRowsByType
