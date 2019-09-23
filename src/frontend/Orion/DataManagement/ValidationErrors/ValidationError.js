1ef55206cb2476e4d0819f7dc6d751930e903f72
Matt Van Dyke <matthewlvandyke@gmail.com>
2019-09-23 15:26:45 -0400

    Style Validation error output

--- src/frontend/Orion/DataManagement/ValidationErrors/ValidationError.js
+++ src/frontend/Orion/DataManagement/ValidationErrors/ValidationError.js
@@ -1,11 +1,79 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from "@emotion/styled"
+
+const Row = styled.div({
+  display: 'flex',
+}, ({ backgroundColor }) => ({
+  backgroundColor: backgroundColor,
+}))
+
+const Cell = styled.div({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: 200,
+  boxSizing: 'border-box',
+  padding: 12,
+  border: '1px solid black',
+  flexDirection: 'column',
+})
+
+const MultiCellWrapper = styled.div({
+  flex: 1,
+  display: 'flex',
+})
+
+const MultiCell = styled.div({
+  padding: 12,
+  boxSizing: 'border-box',
+  width: '100%',
+  textAlign: 'center',
+  borderBottom: '1px solid black',
+  flex: 1,
+  minHeight: 50,
+})
 
 const ValidationError = ({
-  error,
-}) => (
-  <div>{error.message}</div>
-)
+  backgroundColor,
+  error: {
+    type,
+    problemRows,
+  },
+}) => {
+  if (!problemRows.length) return null
+
+  const {
+    sheetRows,
+    values,
+    suggestions,
+  } = problemRows.reduce((acc, { sheetRow, value, suggestion }) => {
+    if (!acc.sheetRows.includes(sheetRow)) acc.sheetRows.push(sheetRow)
+    if (!acc.values.includes(value)) acc.values.push(value)
+    if (!acc.suggestions.includes(suggestion)) acc.suggestions.push(suggestion)
+
+    return acc
+  }, { sheetRows: [], values: [], suggestions: []})
+
+  return (
+    <Row backgroundColor={backgroundColor}>
+      <Cell>
+        {type}
+      </Cell>
+      <Cell>
+        {sheetRows.join(', ')}
+      </Cell>
+      <MultiCellWrapper>
+        <Cell style={{ padding: 0 }}>
+          {values.map(value => <MultiCell>{value}</MultiCell>)}
+        </Cell>
+        <Cell style={{ padding: 0 }}>
+          {suggestions.map(suggestion => <MultiCell>{suggestion}</MultiCell>)}
+        </Cell>
+      </MultiCellWrapper>
+    </Row>
+  )
+}
 
 ValidationError.propTypes = {
   error: PropTypes.object,
--- src/frontend/Orion/DataManagement/ValidationErrors/ValidationErrors.js
+++ src/frontend/Orion/DataManagement/ValidationErrors/ValidationErrors.js
@@ -1,15 +1,55 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from "@emotion/styled"
 
 import ValidationError from './ValidationError'
 
+const COLUMNS = ['type', 'rows', 'values', 'suggestions']
+
+const TableHeader = styled.div({
+  display: 'flex',
+  fontWeight: 700,
+})
+
+const Column = styled.div({
+  padding: 12,
+  border: '1px solid black',
+  width: 200,
+  display: 'flex',
+  justifyContent: 'center',
+  boxSizing: 'border-box',
+})
+
 const ValidationErrors = ({ errors }) => (
   <>
-    <div>ERRORS</div>
+    <div style={{ padding: '24px 0', color: 'red' }}>
+      <div style={{ fontWeight: 700, paddingBottom: '12' }}>Sheet Errors</div>
+      <div>
+        <ul>
+          {
+            errors.map(({ message }) => (
+              <li key={message}>
+                {message}
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    </div>
+    <TableHeader>
+      {
+        COLUMNS.map(label => (
+          <Column key={`column-${label}`}>
+            {label}
+          </Column>
+        ))
+      }
+    </TableHeader>
     {
-      errors.map(error => (
+      errors.map((error, idx) => (
         <ValidationError
-          key={error.message}
+          key={error.type}
+          backgroundColor={idx % 2 === 0 ? "#8080804d" : "white"}
           error={error}
         />
       ))
