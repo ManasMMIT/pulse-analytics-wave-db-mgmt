793bca0a19000869f8f0b3ee4d8343ec7f8e4b2d
Matt Van Dyke <matthewlvandyke@gmail.com>
2019-09-24 14:11:10 -0400

    Refactor Import into own dir with components

---
+++ src/frontend/Orion/DataManagement/Import/Import.js
@@ -0,0 +1,137 @@
+import React from "react"
+import XLSX from 'xlsx'
+
+import SheetSelector from './SheetSelector'
+import CollectionSelector from './CollectionSelector'
+import SubmitButton from './SubmitButton'
+import Spinner from '../../../Phoenix/shared/Spinner'
+
+class Import extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.fileInputRef = React.createRef()
+
+    this.workbook = null
+    this.jsonForImport = []
+
+    this.state = {
+      sheetNames: [],
+      isLoading: false,
+      selectedSheet: null,
+      selectedCollection: null,
+      greatSuccess: false,
+    }
+  }
+
+  handleSheetSelection = selectedSheet => {
+    this.setState({ selectedSheet })
+  }
+
+  handleCollectionSelection = selectedCollection => {
+    this.setState({ selectedCollection })
+  }
+
+  onFilesAdded = () => {
+    this.setState({ isLoading: true }, () => {
+      const file = this.fileInputRef.current.files[0]
+
+      const reader = new FileReader()
+
+      reader.onload = e => {
+        const data = new Uint8Array(e.target.result)
+        const workbook = XLSX.read(data, { type: 'array' })
+        this.workbook = workbook
+
+        const sheetNames = workbook.SheetNames
+        const selectedSheet = { value: sheetNames[0], label: sheetNames[0] }
+
+        this.setState({
+          sheetNames,
+          isLoading: false,
+          selectedSheet,
+        })
+      }
+
+      reader.readAsArrayBuffer(file)
+    })
+  }
+
+  handleSuccess = () => {
+    this.fileInputRef.current.value = ''
+
+    this.setState({ greatSuccess: true }, () => {
+      setTimeout(() => {
+        this.setState({
+          sheetNames: [],
+          isLoading: false,
+          selectedSheet: null,
+          selectedCollection: null,
+          greatSuccess: false,
+        })
+      }, 1500)
+    })
+  }
+
+  render() {
+    const {
+      sheetNames,
+      selectedSheet,
+      isLoading,
+      selectedCollection,
+      greatSuccess,
+    } = this.state
+
+    return (
+      <div style={{ padding: 24 }}>
+        {/* TODO: Figure out ref logic to pull this into FileSelector.js */}
+        <div>
+          <p>Pick an Excel file:</p>
+          <label style={{
+            display: 'block',
+            border: '1px solid black',
+            borderRadius: 4,
+            padding: '100px 200px',
+            textAlign: 'center',
+          }}>
+            Import File
+            <input
+              style={{ display: 'none' }}
+              ref={this.fileInputRef}
+              type="file"
+              multiple
+              onChange={this.onFilesAdded}
+            />
+          </label>
+        </div>
+        {
+          greatSuccess
+            ? <div>IMPORT SUCCESSFUL</div>
+            : (
+              <>
+                <SheetSelector
+                  sheetNames={sheetNames}
+                  selectedSheet={selectedSheet}
+                  handleSheetSelection={this.handleSheetSelection}
+                />
+                <CollectionSelector
+                  selectedCollection={selectedCollection}
+                  selectedSheet={selectedSheet}
+                  handleCollectionSelection={this.handleCollectionSelection}
+                />
+                <SubmitButton
+                  workbook={this.workbook}
+                  selectedCollection={selectedCollection}
+                  handleSuccess={this.handleSuccess}
+                  selectedSheet={selectedSheet}
+                />
+              </>
+            )
+        }
+        {isLoading && <Spinner />}
+      </div>
+    )
+
+  }
+}
+export default Import
