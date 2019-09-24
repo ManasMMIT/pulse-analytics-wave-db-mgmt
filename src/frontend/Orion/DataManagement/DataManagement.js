cb49d0d613839ddfa29a1712a2344913d8a38a4b
Matt Van Dyke <matthewlvandyke@gmail.com>
2019-09-23 23:40:38 -0400

    Componentize SelectionFields in DataMGMT

--- src/frontend/Orion/DataManagement/DataManagement.js
+++ src/frontend/Orion/DataManagement/DataManagement.js
@@ -1,22 +1,9 @@
 import React, { Component } from "react"
-import { Query, Mutation } from 'react-apollo'
-import _ from 'lodash'
 import XLSX from 'xlsx'
-import Select from 'react-select'
-import CreatableSelect from 'react-select/creatable'
 
 import Spinner from '../../Phoenix/shared/Spinner'
-import sheetToJson from './sheetToJson'
 
-import ValidationErrors from './ValidationErrors'
-
-import {
-  GET_RAW_COLLECTION_NAMES,
-} from './../../api/queries'
-
-import {
-  UPLOAD_COLLECTION,
-} from './../../api/mutations'
+import SelectionFields from './SelectionFields'
 
 class DataManagement extends Component {
   constructor(props) {
@@ -32,6 +19,7 @@ class DataManagement extends Component {
       isLoading: false,
       selectedSheet: null,
       selectedCollection: null,
+      greatSuccess: false,
     }
   }
 
@@ -68,100 +56,67 @@ class DataManagement extends Component {
     })
   }
 
+  handleSuccess = () => {
+    this.fileInputRef.current.value = ''
+
+    this.setState({ greatSuccess: true }, () => {
+      setTimeout(() => {
+        this.setState({
+          sheetNames: [],
+          isLoading: false,
+          selectedSheet: null,
+          selectedCollection: null,
+          greatSuccess: false,
+        })
+      }, 1500)
+    })
+  }
+
   render() {
     const {
       sheetNames,
       selectedSheet,
       isLoading,
       selectedCollection,
+      greatSuccess,
     } = this.state
 
     return (
       <div style={{ padding: 24 }}>
         <div>
           <p>Pick an Excel file:</p>
-          <input
-            ref={this.fileInputRef}
-            type="file"
-            multiple
-            onChange={this.onFilesAdded}
-          />
+          <label style={{
+            display: 'block',
+            border: '1px solid black',
+            borderRadius: 4,
+            padding: '100px 200px',
+            textAlign: 'center',
+          }}>
+            Import File
+            <input
+              style={{ display: 'none' }}
+              ref={this.fileInputRef}
+              type="file"
+              multiple
+              onChange={this.onFilesAdded}
+            />
+          </label>
         </div>
 
         {
-          _.isEmpty(sheetNames) || (
-            <div style={{ marginTop: 24, width: 500 }}>
-              <p>Sheets to Upload:</p>
-              {
-                <Select
-                  value={selectedSheet}
-                  onChange={this.handleSheetSelection}
-                  options={this.state.sheetNames.map(n => ({ value: n, label: n }))}
-                />
-              }
-            </div>
-          )
-        }
-
-        {
-          selectedSheet && (
-            <div style={{ marginTop: 24 }}>
-              <p>Which collection would you like to upload to?</p>
-              <p>Create a blank collection or pick an existing collection to overwrite.</p>
-              <Query query={GET_RAW_COLLECTION_NAMES}>
-                {({ data: { collections }, loading, error }) => {
-                  if (error) return <div style={{ color: 'red' }}>Error processing request</div>
-                  if (loading) return <Spinner />
-
-                  return (
-                    <CreatableSelect
-                      onChange={this.handleCollectionSelection}
-                      options={collections.map(n => ({ value: n, label: n }))}
-                      value={selectedCollection}
-                    />
-                  )
-                }}
-              </Query>
-            </div>
-          )
-        }
-
-        {
-          selectedCollection && (
-            <Mutation mutation={UPLOAD_COLLECTION}>
-              {(handleUpload, { loading, error }) => {
-                if (loading) return <Spinner />
-
-                const { selectedSheet, selectedCollection } = this.state
-
-                // TODO: Make error handling less wonky
-                const errors = error && error.graphQLErrors[0].extensions.exception.error
-
-                const selectedSheetObj = this.workbook.Sheets[selectedSheet.value]
-                const { json } = sheetToJson(selectedSheetObj)
-
-                const handleSubmit = () => {
-                  handleUpload({
-                    variables: {
-                      input: {
-                        data: json,
-                        collectionName: selectedCollection.value,
-                      }
-                    }
-                  })
-                }
-
-                return (
-                  <>
-                    <div style={{ marginTop: 24 }}>
-                      <button onClick={handleSubmit}>Upload</button>
-                    </div>
-                    {!!error && <ValidationErrors errors={errors} />}
-                  </>
-                )
-              }}
-            </Mutation>
-          )
+          !greatSuccess
+            ? (
+              <SelectionFields
+                workbook={this.workbook}
+                sheetNames={sheetNames}
+                selectedSheet={selectedSheet}
+                selectedCollection={selectedCollection}
+                handleSuccess={this.handleSuccess}
+                handleSheetSelection={this.handleSheetSelection}
+                handleCollectionSelection={this.handleCollectionSelection}
+              />
+            )
+            : <div>IMPORT SUCCESSFUL</div>
         }
         {isLoading && <Spinner />}
       </div>
