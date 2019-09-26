6984d4a18556adc7f604fb654096e6c28e1d97ce
Matt Van Dyke <matthewlvandyke@gmail.com>
2019-09-26 00:57:47 -0400

    Refactor ValidationError(s) components

--- src/frontend/Orion/DataManagement/Import/ValidationErrors/ValidationError.js
+++ src/frontend/Orion/DataManagement/Import/ValidationErrors/ValidationError.js
@@ -2,63 +2,97 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from "@emotion/styled"
 
-const Row = styled.div({
-  display: 'flex',
-}, ({ backgroundColor }) => ({
-  backgroundColor: backgroundColor,
-}))
-
-const Cell = styled.div({
-  display: 'flex',
-  alignItems: 'center',
-  width: 200,
-  boxSizing: 'border-box',
-  padding: 12,
-  border: '1px solid black',
-  flexDirection: 'column',
+const Title = styled.div({
+  fontSize: 20,
+  fontWeight: 500,
 })
 
-const ValidationError = ({
-  backgroundColor,
-  error: {
-    type,
-    problemRows,
-  },
-}) => {
-  if (!problemRows.length) return null
+const Subtitle = styled.div({
+  color: '#808080bd',
+  lineHeight: 1.5,
+})
 
-  return (
-    <Row backgroundColor={backgroundColor}>
-      <Cell>
-        {type}
-      </Cell>
+const RowWrapper = styled.div({
+  justifyContent: 'space-between',
+})
+
+const HeaderRowWrapper = styled.div({
+  display: 'flex',
+  padding: 8,
+  margin: 6,
+  paddingLeft: 24,
+  marginLeft: 0,
+})
+
+const ProblemRowWrapper = styled.div({
+  display: 'flex',
+  padding: 8,
+  margin: 6,
+  paddingLeft: 24,
+  marginLeft: 0,
+  background: '#ff000029',
+  borderRadius: 2,
+}, ({ background }) => ({ background }))
+
+const ColumnItems = styled.span({
+  width: '33%',
+  padding: 12,
+  overflowWrap: 'break-word',
+  maxHeight: 40,
+  overflowY: 'scroll',
+}, ({ color }) => ({ color }))
+
+const ValidationError = ({
+  type,
+  problemRows,
+  errorColor,
+  rowColors,
+  style,
+}) => (
+  <div key={type} style={style}>
+    <div>
+      <Title>
+        {`${type.toUpperCase()} ERRORS`}
+      </Title>
+      <Subtitle>
+        {`Fix the following ${type} errors in the sheet and re-upload`}
+      </Subtitle>
+    </div>
+    <RowWrapper>
       <div>
-        {
-          problemRows.map(row => (
-            <div key={row.value} style={{ display: 'flex' }}>
-              <Cell>
-                {row.sheetRows.join(', ')}
-              </Cell>
-              <Cell>
-                {row.value}
-              </Cell>
-              <Cell>
-                {row.suggestion}
-              </Cell>
-            </div>
-          ))
-        }
+        <HeaderRowWrapper>
+          <ColumnItems>Value</ColumnItems>
+          <ColumnItems>Rows</ColumnItems>
+          <ColumnItems>Suggestions</ColumnItems>
+        </HeaderRowWrapper>
+        <div>
+          {
+            problemRows.map(({ value, sheetRows, suggestion }) => (
+              <ProblemRowWrapper background={errorColor} key={value}>
+                <ColumnItems color={rowColors[0]}>
+                  {value}
+                </ColumnItems>
+                <ColumnItems color={rowColors[1]}>
+                  {sheetRows.join(', ')}
+                </ColumnItems>
+                <ColumnItems color={rowColors[2]}>
+                  {suggestion}
+                </ColumnItems>
+              </ProblemRowWrapper>
+            ))
+          }
+        </div>
       </div>
-    </Row>
-  )
-}
+    </RowWrapper>
+  </div>
+)
 
 ValidationError.propTypes = {
-  error: PropTypes.object,
-}
-
-ValidationError.defaultProps = {
-  error: {},
+  style: PropTypes.object,
+  type: PropTypes.string,
+  problemRows: PropTypes.array,
+  errorColor: PropTypes.string,
+  rowColors: PropTypes.array,
 }
 
 export default ValidationError
--- src/frontend/Orion/DataManagement/Import/ValidationErrors/ValidationErrors.js
+++ src/frontend/Orion/DataManagement/Import/ValidationErrors/ValidationErrors.js
@@ -1,46 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from "@emotion/styled"
 
-const Title = styled.div({
-  fontSize: 20,
-  fontWeight: 500,
-})
-
-const Subtitle = styled.div({
-  color: '#808080bd',
-  lineHeight: 1.5,
-})
-
-const RowWrapper = styled.div({
-  justifyContent: 'space-between',
-})
-
-const HeaderRowWrapper = styled.div({
-  display: 'flex',
-  padding: 8,
-  margin: 6,
-  paddingLeft: 24,
-  marginLeft: 0,
-})
-
-const ProblemRowWrapper = styled.div({
-  display: 'flex',
-  padding: 8,
-  margin: 6,
-  paddingLeft: 24,
-  marginLeft: 0,
-  background: '#ff000029',
-  borderRadius: 2,
-})
-
-const ColumnItems = styled.span({
-  width: '33%',
-  padding: 12,
-  overflowWrap: 'break-word',
-  maxHeight: 40,
-  overflowY: 'scroll',
-}, ({ color }) => ({ color }))
+import ValidationError from './ValidationError'
 
 const ValidationErrors = ({ errors }) => {
   if (!errors) return null
@@ -48,37 +9,13 @@ const ValidationErrors = ({ errors }) => {
   return (
     <div>
       {
-        errors.map(({ type, problemRows }, idx, errors) => (
-          <div key={type} style={idx !== errors.length ? { marginBottom: 24 } : {}}>
-            <div>
-              <Title>
-                {`${ type.toUpperCase() } ERRORS`}
-              </Title>
-              <Subtitle>
-                {`Fix the following ${ type } errors in the sheet and re-upload`}
-              </Subtitle>
-            </div>
-            <RowWrapper>
-              <div>
-                <HeaderRowWrapper>
-                  <ColumnItems>Value</ColumnItems>
-                  <ColumnItems>Rows</ColumnItems>
-                  <ColumnItems>Suggestions</ColumnItems>
-                </HeaderRowWrapper>
-                <div>
-                  {
-                    problemRows.map(({ value, sheetRows, suggestion }) => (
-                      <ProblemRowWrapper key={value}>
-                        <ColumnItems color={'red'}>{ value }</ColumnItems>
-                        <ColumnItems>{ sheetRows.join(', ') }</ColumnItems>
-                        <ColumnItems color={'blue'}>{ suggestion }</ColumnItems>
-                      </ProblemRowWrapper>
-                    ))
-                  }
-                </div>
-              </div>
-            </RowWrapper>
-          </div>
+        errors.map(({ type, problemRows }) => (
+          <ValidationError
+            style={{ marginBottom: 24 }}
+            type={type}
+            problemRows={problemRows}
+            rowColors={['red', 'black', 'blue']}
+          />
         ))
       }
     </div>
