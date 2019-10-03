cd2638bb619f1e732e55706baaba6d4aae1eb9b6
Jon Lin <jon.cp.lin@gmail.com>
2019-10-03 15:48:18 -0400

    Delete apollo cruft from App.js and uninstall associated apollo packages

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,10 +1,5 @@
 import React from 'react'
 import { transparentize } from 'polished'
-// import { ApolloClient } from 'apollo-client'
-// import { ApolloLink } from 'apollo-link'
-// import { InMemoryCache } from 'apollo-cache-inmemory'
-// import { RestLink } from 'apollo-link-rest'
-// import { HttpLink } from 'apollo-link-http'
 import { ApolloProvider } from 'react-apollo'
 import ApolloClient from 'apollo-boost'
 
@@ -21,23 +16,6 @@ import typeDefs from './api/typeDefs'
 import Phoenix from './Phoenix'
 import Orion from './Orion'
 import Delphi from './Delphi'
-
-// const cache = new InMemoryCache()
-
-// const restLink = new RestLink({ uri: '/api' })
-// const httpLink = new HttpLink({ uri: '/api/graphql' })
-
-// const link = ApolloLink.from([
-//   restLink,
-//   httpLink,
-// ]);
-
-// const client = new ApolloClient({
-//   cache,
-//   link,
-//   resolvers,
-//   typeDefs,
-// })
 
 const client = new ApolloClient({
   uri: '/api/graphql',
