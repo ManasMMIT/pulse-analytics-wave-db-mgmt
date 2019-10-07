5626a4895fec626aabccd74d38ae780f2d7cd331
Matt Van Dyke <matthewlvandyke@gmail.com>
2019-10-07 14:26:39 -0400

    Comment back in TeamsPanel in Home

--- src/frontend/Phoenix/Home/Home.js
+++ src/frontend/Phoenix/Home/Home.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import ClientsPanel from './ClientsPanel'
-// import TeamsPanel from './TeamsPanel'
+import TeamsPanel from './TeamsPanel'
 // import UsersPanel from './UsersPanel'
 // import StatusPanel from './StatusPanel'
 
 const Home = () => (
   <div style={{ display: "flex" }}>
     <ClientsPanel />
-    {/* <TeamsPanel />
-    <UsersPanel />
+    <TeamsPanel />
+    {/* <UsersPanel />
     <StatusPanel /> */}
   </div>
 )
