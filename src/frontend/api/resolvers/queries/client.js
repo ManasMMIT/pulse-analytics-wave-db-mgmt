c6f1053fbfed0c90385dfd6c146f172d4a9af724
Matt Van Dyke <matthewlvandyke@gmail.com>
2019-10-07 14:08:49 -0400

    Wean frontend selectedClient resolver off root

--- src/frontend/api/resolvers/queries/client.js
+++ src/frontend/api/resolvers/queries/client.js
@@ -2,21 +2,27 @@ import _ from 'lodash'
 
 import {
   GET_CLIENTS,
+  GET_SELECTED_CLIENT,
 } from '../../queries'
 
 const clientQueryResolvers = {
   // This resolver has ONE JOB: to set a default client.
   // The mutation resolver `selectClient` handles cache updates.
-  selectedClient: async (root, args, { cache }) => {
-    let result = root.selectedClient
+  selectedClient: async (root, args, { client, cache }) => {
+    let selectedClient
+    try {
+      selectedClient = cache.readQuery({ query: GET_SELECTED_CLIENT }).selectedClient
+    } catch(e) {
+      const {
+        data: {
+          clients,
+        }
+      } = await client.query({ query: GET_CLIENTS })
 
-    if (!root.selectedClient) {
-      const { clients } = cache.readQuery({ query: GET_CLIENTS })
-
-      result = clients[0]
+      selectedClient = clients[0]
     }
 
-    return result
+    return selectedClient
   },
 }
 
