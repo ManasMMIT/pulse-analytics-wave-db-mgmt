18f35b2369a58011acbb7f3f0e8e433826470890
Matt Van Dyke <matthewlvandyke@gmail.com>
2019-10-09 16:06:50 -0400

    Minor syntax changes

--- src/frontend/Phoenix/shared/TextForm/TextForm.js
+++ src/frontend/Phoenix/shared/TextForm/TextForm.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { useMutation, useApolloClient } from '@apollo/react-hooks'
 
 import Spinner from '../Spinner'
+
 class TextForm extends Component {
   constructor(props) {
     super(props)
