ed46cc34c6cbed10aabd8b2b1ba7818b81ce7502
Matt Van Dyke <matthewlvandyke@gmail.com>
2019-10-09 16:13:50 -0400

    Unnest users panel from UnderlinedTabs

--- src/frontend/Phoenix/Home/UsersPanel/UsersPanel.js
+++ src/frontend/Phoenix/Home/UsersPanel/UsersPanel.js
@@ -1,22 +1,90 @@
 import React from 'react'
+import { Query, useQuery } from 'react-apollo'
 
-import { UnderlinedTabs } from '../../../components/Tabs'
+import Panel from '../../shared/Panel'
+import CreateButton from './CreateButton'
+import UpdateButton from './UpdateButton'
+import DeleteButton from './DeleteButton'
 
-import UsersTabContent from './UsersTabContent'
+import {
+  SELECT_USER,
+} from '../../../api/mutations'
 
-const TAB_ONE = 'Users'
+import {
+  GET_TEAM_USERS,
+  GET_SELECTED_USER,
+  GET_SELECTED_TEAM,
+} from '../../../api/queries'
 
-const FILTER_TAB_OPTIONS = [
-  TAB_ONE,
-]
-
-const UsersPanel = () => {
-  return (
-    <div>
-      <UnderlinedTabs tabsData={FILTER_TAB_OPTIONS}>
-        <UsersTabContent key="Users" />
-      </UnderlinedTabs>
-    </div>
-  )
+const defaultPanelItemStyle = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  cursor: 'pointer',
+  color: '#838c96',
+  borderLeft: '4px solid transparent',
+  padding: 24,
 }
+
+const activePanelItemStyle = {
+  cursor: 'default',
+  backgroundColor: '#f8fafb',
+  color: '#2a7ad3',
+  borderLeft: '4px solid #1f6cc7',
+}
+
+// const buttonGroupCallback = user => {
+//   if (user.isDefault) return null
+
+//   return (
+//     <>
+//       <UpdateButton user={user} />
+//       <DeleteButton userId={user._id} />
+//     </>
+//   )
+// }
+
+const panelItemConfig = {
+  selectEntityMutationDoc: SELECT_USER,
+  style: defaultPanelItemStyle,
+  activeStyle: activePanelItemStyle,
+  // buttonGroupCallback,
+  label1Callback: ({ username }) => username,
+  // ! Note: inactiveStyle not needed until hover effects differ
+  // ! between active and inactive states
+  // inactiveStyle: inactivePanelItemStyle,
+}
+
+const panelStyle = {
+  backgroundColor: '#f7f9fa',
+  height: '100vh',
+}
+
+// const headerChildren = <CreateButton />
+
+const UsersPanel = () => (
+  // <Query query={GET_SELECTED_TEAM}>
+  //   {
+  //     ({ data, loading }) => {
+  //       if (loading) return null
+
+  //       const { selectedTeam } = data
+
+  //       return (
+          <Panel
+            style={panelStyle}
+            // title={`Users for ${selectedTeam.description}`}
+            // headerChildren={headerChildren}
+            queryDocs={{
+              fetchAllQueryProps: { query: GET_TEAM_USERS },
+              fetchSelectedQueryProps: { query: GET_SELECTED_USER },
+            }}
+            panelItemConfig={panelItemConfig}
+          />
+  //       )
+  //     }
+  //   }
+  // </Query>
+)
+
 export default UsersPanel
