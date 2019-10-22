26f413981723b49326b3bc2bd7b0227bf59e71f1
Jonathan Lin <jon.cp.lin@gmail.com>
2019-10-22 15:25:04 -0400

    Sort embedded regimens in indication objects in indications resolver (#160)

--- src/backend/resolvers/queries/indications.js
+++ src/backend/resolvers/queries/indications.js
@@ -1,8 +1,14 @@
-const indications =  (parent, args, { pulseCoreDb }, info) => {
-  return pulseCoreDb.collection('indications')
+const indications =  async (parent, args, { pulseCoreDb }, info) => {
+  const indications = await pulseCoreDb.collection('indications')
     .find()
     .sort({ name: 1 })
     .toArray()
+
+  indications.forEach(({ regimens }) => {
+    regimens.sort((a, b) => a.name.localeCompare(b.name))
+  })
+
+  return indications
 }
 
 module.exports = indications
