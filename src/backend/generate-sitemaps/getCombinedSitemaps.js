b1f17ac42aba7e1708e3150d8d1ea1d8c14e06e1
Matt Van Dyke <matthewlvandyke@gmail.com>
2019-12-31 17:48:33 -0500

    Chore/clean up old reg targeting files/plrs 126 (#266)
    * Remove resources logic from getCombinedSitemaps
    
    * Remove getCombinedResources w/ test + mocks
    
    * Remove old psql files from generate-sitemaps dir
    
    * Update changelog

--- src/backend/generate-sitemaps/getCombinedSitemaps.js
+++ src/backend/generate-sitemaps/getCombinedSitemaps.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-loop-func */
 const _ = require('lodash')
-const getCombinedResources = require('./getCombinedResources')
 
 module.exports = sitemaps => {
   const combinedSitemaps = {}
@@ -12,22 +11,6 @@ module.exports = sitemaps => {
         combinedSitemaps[nodeType] = nodes
       } else {
         combinedSitemaps[nodeType] = combinedSitemaps[nodeType].concat(nodes)
-
-        combinedSitemaps[nodeType].forEach(node => {
-          if (node.resources) {
-            const duplicateNodes = combinedSitemaps[nodeType]
-              .filter(targetNode => targetNode._id === node._id)
-            
-            const duplicateNodesResources = duplicateNodes
-              .map(({ resources }) => resources)
-
-            const combinedResources = getCombinedResources(duplicateNodesResources)
-
-            duplicateNodes.forEach(dupe => {
-              dupe.resources = combinedResources
-            })
-          }
-        })
 
         combinedSitemaps[nodeType] = _.uniqBy(
           combinedSitemaps[nodeType],
