0aca2d6dfafaa4aea3d0803321dfc6de705df92c
Jonathan Lin <jon.cp.lin@gmail.com>
2020-01-07 12:28:53 -0500

    Chore/detransactionalize push sitemaps to dev emergency button (#278)
    * Fix emergency push sitemaps to dev generateUsersPermissions util to not dupe insertion
    
    * Detransactionalize generateUsersPermissions util
    
    * Detransactionalize generateSitemaps util, reuse upsertUsersSitemaps util

--- src/backend/generate-sitemaps/index.js
+++ src/backend/generate-sitemaps/index.js
@@ -1,96 +1,24 @@
 /* eslint-disable no-loop-func */
-// const _ = require('lodash')
-const getCombinedSitemaps = require('./getCombinedSitemaps')
+const upsertUsersSitemaps = require('../resolvers/mutations/sitemap/upsertUsersSitemaps')
 
 const generateSitemaps = async ({
-  mongoClient,
   pulseCoreDb,
   pulseDevDb,
 }) => {
-  const DevUsers = pulseDevDb.collection('users.sitemaps')
-  const CoreRoles = pulseCoreDb.collection('roles')
   const CoreUsers = pulseCoreDb.collection('users')
-  const session = mongoClient.startSession()
+  const users = await CoreUsers.find().toArray()
 
-  await session.withTransaction(async () => {
-    const users = await CoreUsers.find().toArray()
+  await pulseDevDb.collection('users.sitemaps').deleteMany()
 
-    const promiseArray = []
-    for (const user of users) {
-      const usersSitemapUser = await DevUsers.findOne({ _id: user._id })
+  console.log('users.sitemaps collection dropped')
 
-      let promise
-      if (usersSitemapUser) {
-        promise = CoreRoles
-          .find({
-            'users._id': user._id },
-            { session },
-          ).toArray()
-          .then(userRoles => {
-            const userRolesSitemap = userRoles.map(({ sitemap }) => sitemap)
-
-            const combinedSitemap = getCombinedSitemaps(userRolesSitemap)
-
-            const client = userRoles.length ? userRoles[0].client : null
-
-            const teams = userRoles.length
-              ? userRoles.map(({ _id, name }) => ({ _id, name }))
-              : []
-
-            return DevUsers.updateOne(
-              { _id: user._id },
-              {
-                $set: {
-                  sitemap: combinedSitemap,
-                  teams,
-                  client,
-                  schemaVersion: 'v1.2.0',
-                  updatedAt: new Date()
-                }
-              },
-              { session },
-            )
-          })
-      } else {
-        promise = CoreRoles
-          .find(
-            { 'users._id': user._id },
-            { session },
-          ).toArray()
-          .then(userRoles => {
-            const userRolesSitemap = userRoles.map(({ sitemap }) => sitemap)
-
-            const combinedSitemap = getCombinedSitemaps(userRolesSitemap)
-
-            const client = userRoles.length ? userRoles[0].client : null
-
-            const teams = userRoles.length
-              ? userRoles.map(({ _id, name }) => ({ _id, name }))
-              : []
-
-            return DevUsers.insertOne(
-              {
-                _id: user._id,
-                username: user.username,
-                client,
-                teams,
-                sitemap: combinedSitemap,
-                schemaVersion: 'v1.2.0',
-                updatedAt: new Date(),
-                createdAt: new Date()
-              },
-              { session },
-            )
-          })
-      }
-
-      promiseArray.push(promise)
-    }
-
-    await Promise.all(promiseArray)
+  await upsertUsersSitemaps({
+    users,
+    pulseCoreDb,
+    pulseDevDb,
   })
 
-  console.log('Sitemap slice in pulse-dev users.sitemaps collection updated.')
+  console.log('users.sitemaps collection updated')
 
   return true
 }
--- src/backend/generate-users-permissions/index.js
+++ src/backend/generate-users-permissions/index.js
@@ -1,52 +1,26 @@
 /* eslint-disable no-loop-func */
-const _ = require('lodash')
-const upsertUserPerms = require('./upsertUserPerms')
+const upsertUsersPermissions = require('./upsertUsersPermissions')
 
 const generateUsersPermissions = async({
-  mongoClient,
   pulseCoreDb,
   pulseDevDb,
 }) => {
   const coreUsers = pulseCoreDb.collection('users')
 
-  const session = mongoClient.startSession()
+  await pulseDevDb.collection('users.nodes.resources')
+    .deleteMany({})
 
-  await session.withTransaction(async () => {
-    await pulseDevDb.collection('users.nodes.resources')
-      .deleteMany({}, { session })
+  console.log('users.nodes.resources successfully dropped')
 
-    console.log('users.nodes.resources successfully dropped')
+  const users = await coreUsers.find().toArray()
 
-    const users = await coreUsers.find().toArray()
-    const masterLists = await Promise.all(
-      ['organizations', 'indications', 'regimens'].map(collectionName => {
-        return pulseCoreDb.collection(collectionName).find().toArray()
-      })
-    )
-
-    const masterListItemsById = _.keyBy(
-      _.flatten(masterLists),
-      '_id',
-    )
-
-    const usersNodesResourcesPromises = users.map(user => (
-      upsertUserPerms({
-        userId: user._id,
-        pulseDevDb,
-        pulseCoreDb,
-        masterListItemsById,
-        session,
-      })
-    ))
-
-    const docs = await Promise.all(usersNodesResourcesPromises)
-
-    await pulseDevDb
-      .collection('users.nodes.resources')
-      .insertMany(docs, { session })
-
-    console.log('users.nodes.resources successfully rebuilt')
+  await upsertUsersPermissions({ 
+    users, 
+    pulseCoreDb, 
+    pulseDevDb, 
   })
+  
+  console.log('users.nodes.resources successfully rebuilt')
 }
 
 module.exports = generateUsersPermissions
