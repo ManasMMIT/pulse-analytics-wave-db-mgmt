6f2ed6b9b6ce7d18251aa78b6995c689a8fe05cd
Matt Van Dyke <matthewlvandyke@gmail.com>
2020-01-22 16:03:34 -0500

    Add loader to Export Custom Data script button (#300)

--- src/frontend/Orion/DataManagement/ExportCustomData.js
+++ src/frontend/Orion/DataManagement/ExportCustomData.js
@@ -1,23 +1,30 @@
 import React from 'react'
 import { useMutation } from '@apollo/react-hooks'
+import Spinner from './../../Phoenix/shared/Spinner'
 
 import {
   RUN_PIPE_DELIMITED_SCRIPT,
 } from './../../api/mutations'
 
 const ExportCustomData = () => {
-  const [runPipeDelimitedScript] = useMutation(RUN_PIPE_DELIMITED_SCRIPT)
+  const [runPipeDelimitedScript, { loading }] = useMutation(RUN_PIPE_DELIMITED_SCRIPT)
 
   return (
     <div style={{ padding: 24 }}>
       <h1>Export Custom Data</h1>
       <div>
         <p>Pressing this button will save files to the server, which can then be grabbed.</p>
-        <button
-          onClick={runPipeDelimitedScript}
-        >
-          Pipe Delimited Script
-        </button>
+        {
+          loading
+            ? <Spinner />
+            : (
+              <button
+                onClick={runPipeDelimitedScript}
+              >
+                Pipe Delimited Script
+              </button>
+            )
+        }
       </div>
     </div>
   )
