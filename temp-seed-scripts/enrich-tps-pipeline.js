1422c9f03799af6ecc134af0ab73a6ac63d9b856
Matt Van Dyke <matthewlvandyke@gmail.com>
2020-03-04 12:19:14 -0500

    Move enrich tps pipeline to own file

---
+++ temp-seed-scripts/enrich-tps-pipeline.js
@@ -0,0 +1,87 @@
+module.exports = [
+  {
+    '$lookup': {
+      'from': 'indicationsFromHistoricalData',
+      'localField': 'indication',
+      'foreignField': '_id',
+      'as': 'indication'
+    }
+  }, {
+    '$lookup': {
+      'from': 'regimensFromHistoricalData',
+      'localField': 'regimen',
+      'foreignField': '_id',
+      'as': 'regimen'
+    }
+  }, {
+    '$lookup': {
+      'from': 'lines-2',
+      'localField': 'line',
+      'foreignField': '_id',
+      'as': 'line'
+    }
+  }, {
+    '$lookup': {
+      'from': 'populations-2',
+      'localField': 'population',
+      'foreignField': '_id',
+      'as': 'population'
+    }
+  }, {
+    '$lookup': {
+      'from': 'books-2',
+      'localField': 'book',
+      'foreignField': '_id',
+      'as': 'book'
+    }
+  }, {
+    '$lookup': {
+      'from': 'coverages-2',
+      'localField': 'coverage',
+      'foreignField': '_id',
+      'as': 'coverage'
+    }
+  }, {
+    '$project': {
+      'indication': {
+        '$arrayElemAt': [
+          '$indication', 0
+        ]
+      },
+      'regimen': {
+        '$arrayElemAt': [
+          '$regimen', 0
+        ]
+      },
+      'population': {
+        '$arrayElemAt': [
+          '$population', 0
+        ]
+      },
+      'line': {
+        '$arrayElemAt': [
+          '$line', 0
+        ]
+      },
+      'book': {
+        '$arrayElemAt': [
+          '$book', 0
+        ]
+      },
+      'coverage': {
+        '$arrayElemAt': [
+          '$coverage', 0
+        ]
+      }
+    }
+  }, {
+    '$project': {
+      'indication': '$indication.name',
+      'regimen': '$regimen.name',
+      'population': '$population.name',
+      'line': '$line.name',
+      'book': '$book.name',
+      'coverage': '$coverage.name'
+    }
+  }
+]
--- temp-seed-scripts/seedOrganizationsTreatmentPlans.js
+++ temp-seed-scripts/seedOrganizationsTreatmentPlans.js
@@ -1,5 +1,7 @@
 const _ = require('lodash')
 
+const ENRICH_TP_FIELDS_PIPELINE = require('./enrich-tps-pipeline')
+
 module.exports = async ({
   pulseCore,
   payerHistoricalQualityAccess,
@@ -60,7 +62,7 @@ module.exports = async ({
     })
 
   const organizationTreatmentPlanDocs = await Promise.all(ops)
-debugger
+
   await pulseCore.collection('organizations.treatmentPlans-2')
     .insertMany(organizationTreatmentPlanDocs)
 
@@ -75,92 +77,3 @@ debugger
 
   console.log('`organizations.treatmentPlans` seeded');
 }
-
-
-const ENRICH_TP_FIELDS_PIPELINE = [
-  {
-    '$lookup': {
-      'from': 'indicationsFromHistoricalData',
-      'localField': 'indication',
-      'foreignField': '_id',
-      'as': 'indication'
-    }
-  }, {
-    '$lookup': {
-      'from': 'regimensFromHistoricalData',
-      'localField': 'regimen',
-      'foreignField': '_id',
-      'as': 'regimen'
-    }
-  }, {
-    '$lookup': {
-      'from': 'lines-2',
-      'localField': 'line',
-      'foreignField': '_id',
-      'as': 'line'
-    }
-  }, {
-    '$lookup': {
-      'from': 'populations-2',
-      'localField': 'population',
-      'foreignField': '_id',
-      'as': 'population'
-    }
-  }, {
-    '$lookup': {
-      'from': 'books-2',
-      'localField': 'book',
-      'foreignField': '_id',
-      'as': 'book'
-    }
-  }, {
-    '$lookup': {
-      'from': 'coverages-2',
-      'localField': 'coverage',
-      'foreignField': '_id',
-      'as': 'coverage'
-    }
-  }, {
-    '$project': {
-      'indication': {
-        '$arrayElemAt': [
-          '$indication', 0
-        ]
-      },
-      'regimen': {
-        '$arrayElemAt': [
-          '$regimen', 0
-        ]
-      },
-      'population': {
-        '$arrayElemAt': [
-          '$population', 0
-        ]
-      },
-      'line': {
-        '$arrayElemAt': [
-          '$line', 0
-        ]
-      },
-      'book': {
-        '$arrayElemAt': [
-          '$book', 0
-        ]
-      },
-      'coverage': {
-        '$arrayElemAt': [
-          '$coverage', 0
-        ]
-      }
-    }
-  }, {
-    '$project': {
-      'indication': '$indication.name',
-      'regimen': '$regimen.name',
-      'population': '$population.name',
-      'line': '$line.name',
-      'book': '$book.name',
-      'coverage': '$coverage.name'
-    }
-  }
-]
