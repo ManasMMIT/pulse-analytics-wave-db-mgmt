4f1ac5e22c500f8611a9ec697d001fb11e4be353
Matt Van Dyke <matthewlvandyke@gmail.com>
2020-03-04 11:10:36 -0500

    Add newReg, newInd and treatmentPlans seeds

---
+++ temp-seed-scripts/seedTreatmentPlans.js
@@ -0,0 +1,98 @@
+const _ = require('lodash')
+
+module.exports = async ({
+  pulseCore,
+  payerHistoricalQualityAccess,
+  payerHistoricalAdditionalCriteria,
+  payerHistoricalPolicyLinks,
+}) => {
+  await pulseCore.collection('treatmentPlans-2').deleteMany()
+
+  // setup hashes
+  const indications = await pulseCore.collection('indicationsFromHistoricalData').find({}).toArray()
+
+  const indicationsIdMap = indications.reduce((acc, { name, _id }) => {
+    acc[name] = _id
+
+    return acc
+  }, {})
+
+  const regimens = await pulseCore.collection('regimensFromHistoricalData').find({}).toArray()
+
+  const regimensIdMap = regimens.reduce((acc, { name, _id }) => {
+    acc[name] = _id
+
+    return acc
+  }, {})
+
+  const lines = await pulseCore.collection('lines-2').find({}).toArray()
+
+  const linesIdMap = lines.reduce((acc, { name, _id }) => {
+    acc[name] = _id
+
+    return acc
+  }, {})
+
+  const populations = await pulseCore.collection('populations-2').find({}).toArray()
+
+  const populationsIdMap = populations.reduce((acc, { name, _id }) => {
+    acc[name] = _id
+
+    return acc
+  }, {})
+
+  const books = await pulseCore.collection('books-2').find({}).toArray()
+
+  const booksIdMap = books.reduce((acc, { name, _id }) => {
+    acc[name] = _id
+
+    return acc
+  }, {})
+
+  const coverages = await pulseCore.collection('coverages-2').find({}).toArray()
+
+  const coveragesIdMap = coverages.reduce((acc, { name, _id }) => {
+    acc[name] = _id
+
+    return acc
+  }, {})
+
+  // reference hashes for all historical data, inserting tp combos
+  const allTheThings = [
+    ...payerHistoricalQualityAccess,
+    ...payerHistoricalAdditionalCriteria,
+    ...payerHistoricalPolicyLinks,
+  ]
+
+  const onlyTreatmentPlanDocs = allTheThings.filter(thing => (
+    thing.indication
+    && thing.regimen
+    && thing.line
+    && thing.population
+    && thing.book
+    && thing.coverage
+  ))
+
+  const uniqTpsDocs = _.uniqBy(
+    onlyTreatmentPlanDocs,
+    thing => thing.indication + thing.regimen + thing.line + thing.population + thing.book + thing.coverage
+  )
+
+  const ops = uniqTpsDocs
+    .map(async ({ indication, regimen, population, line, book, coverage }) => (
+      pulseCore
+        .collection('treatmentPlans-2')
+        .insertOne({
+          indication: indicationsIdMap[indication] || indication,
+          regimen: regimensIdMap[regimen] || regimen,
+          population: populationsIdMap[population] || population,
+          line: linesIdMap[line] || line,
+          book: booksIdMap[book] || book,
+          coverage: coveragesIdMap[coverage] || coverage,
+        })
+    ))
+
+  await Promise.all(ops)
+
+  console.log('`treatmentPlan combos seeded`');
+};
