18c50c3db585a20f11b332f4b59304731626aa2b
Jonathan Lin <jon.cp.lin@gmail.com>
2020-03-06 13:10:40 -0500

    Get workbook CRUD working on frontend

---
+++ src/frontend/Orion/DataManagement/SheetManagement/WorkbooksPanel/ModalButtonWithForm/Form.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import { useMutation } from '@apollo/react-hooks'
+
+import { GET_WORKBOOKS } from '../../../../../api/queries'
+
+const Form = ({
+  data,
+  mutationDoc,
+  afterMutationHook,
+  closeModal,
+}) => {
+  const [stagedWorkbookStr, setWorkbookStr] = useState(data.name)
+
+  const [saveWorkbook] = useMutation(mutationDoc, {
+    variables: {
+      input: { _id: data._id, name: stagedWorkbookStr }
+    },
+    refetchQueries: [{ query: GET_WORKBOOKS }],
+    onCompleted: result => {
+      const newWorkbook = result.createWorkbook
+      closeModal()
+      afterMutationHook(newWorkbook)
+    },
+    awaitRefetchQueries: true,
+  })
+  
+  const handleChange = e => {
+    e.persist()
+    const value = e.currentTarget && e.currentTarget.value
+    setWorkbookStr(value)
+  }
+
+  return (
+    <div style={{ display: 'flex' }}>
+      <label>Workbook Name</label>
+      <input
+        type="text"
+        value={stagedWorkbookStr}
+        onChange={handleChange}
+      />
+
+      <button onClick={saveWorkbook}>submit</button>
+    </div>
+  )
+}
+
+Form.propTypes = {
+  data: PropTypes.object,
+  mutationDoc: PropTypes.object,
+  afterMutationHook: PropTypes.func,
+}
+
+Form.defaultProps = {
+  data: {},
+  mutationDoc: null,
+  afterMutationHook: () => {},
+}
+
+export default Form
