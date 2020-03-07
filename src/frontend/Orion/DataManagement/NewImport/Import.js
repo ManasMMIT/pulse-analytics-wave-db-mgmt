27b73914bbffb1c908a36e32c76e136c4f68d606
Jonathan Lin <jon.cp.lin@gmail.com>
2020-03-07 15:13:22 -0500

    Convert import component to functional component with useRef hook

--- src/frontend/Orion/DataManagement/NewImport/Import.js
+++ src/frontend/Orion/DataManagement/NewImport/Import.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useRef } from "react"
 import XLSX from 'xlsx'
 import _ from 'lodash'
 
@@ -7,125 +7,84 @@ import sheetToJson from './sheetToJson'
 import Select from 'react-select'
 import Spinner from '../../../Phoenix/shared/Spinner'
 
-class Import extends React.Component {
-  constructor(props) {
-    super(props)
+const Import = () => {
+  const fileInputRef = useRef(null);
+  const [sheetNames, setSheetNames] = useState([])
+  const [selectedSheet, selectSheet] = useState(null)
+  const [workbook, setWorkbook] = useState(null)
+  const [loading, setLoading] = useState(false)
 
-    this.fileInputRef = React.createRef()
+  const onFileAdded = () => {
+    setLoading(true)
 
-    this.workbook = null
-    this.jsonForImport = []
+    const file = fileInputRef.current.files[0]
 
-    this.state = {
-      sheetNames: [],
-      isLoading: false,
-      selectedSheet: null,
+    const reader = new FileReader()
+
+    reader.onload = e => {
+      const data = new Uint8Array(e.target.result)
+      const nextWorkbook = XLSX.read(data, { type: 'array' })
+
+      const nextSheetNames = nextWorkbook.SheetNames
+      const nextSelectedSheet = { value: nextSheetNames[0], label: nextSheetNames[0] }
+
+      setWorkbook(nextWorkbook)
+      setSheetNames(nextSheetNames)
+      selectSheet(nextSelectedSheet)
+      setLoading(false)
     }
+
+    reader.readAsArrayBuffer(file)
   }
 
-  handleSheetSelection = selectedSheet => {
-    this.setState({ selectedSheet })
+  const handleSubmit = () => {
+    setLoading(true)
+
+    const selectedSheetObj = workbook.Sheets[selectedSheet.value]
+    const { json, numExcludedRows } = sheetToJson(selectedSheetObj)
+
+    setLoading(false)
+    alert(`${json.length} rows would be imported; ${numExcludedRows} rows excluded`)
   }
 
-  onFilesAdded = () => {
-    this.setState({ isLoading: true }, () => {
-      const file = this.fileInputRef.current.files[0]
+  return (
+    <div style={{ padding: 24 }}>
+      <div>
+        <p>Pick an Excel file:</p>
+        <input
+          ref={fileInputRef}
+          type="file"
+          multiple
+          onChange={onFileAdded}
+        />
+      </div>
 
-      const reader = new FileReader()
-
-      reader.onload = e => {
-        const data = new Uint8Array(e.target.result)
-        const workbook = XLSX.read(data, { type: 'array' })
-        this.workbook = workbook
-
-        const sheetNames = workbook.SheetNames
-        const selectedSheet = { value: sheetNames[0], label: sheetNames[0] }
-
-        this.setState({
-          sheetNames,
-          isLoading: false,
-          selectedSheet,
-        })
+      {
+        _.isEmpty(sheetNames) || (
+          <div style={{ marginTop: 24, width: 500 }}>
+            <p>Sheets to Upload:</p>
+            {
+              <Select
+                value={selectedSheet}
+                onChange={selectSheet}
+                options={sheetNames.map(n => ({ value: n, label: n }))}
+              />
+            }
+          </div>
+        )
       }
 
-      reader.readAsArrayBuffer(file)
-    })
-  }
+      {
+        selectedSheet && (
+          <button onClick={handleSubmit}>
+            Import
+          </button>
+        )
+      }
 
-  submitHandler = () => {
-    const { selectedSheet } = this.state
-
-    this.setState({ isLoading: true }, () => {
-      const selectedSheetObj = this.workbook.Sheets[selectedSheet.value]
-      const { json, numExcludedRows } = sheetToJson(selectedSheetObj)
-
-      debugger
-
-      // fetch('upload', {
-      //   method: "POST",
-      //   headers: { "Content-Type": "application/json" },
-      //   body: JSON.stringify({ data: json, collectionName: selectedCollection.value }),
-      // }).then(res => res.json())
-      //   .then(persistedData => {
-      //     const numberOfRows = persistedData.length
-
-      //     this.setState({ isLoading: false }, () => {
-      //       alert(`${numberOfRows} rows uploaded; ${numExcludedRows} rows excluded`)
-      //     })
-      //   })
-
-      this.setState({ isLoading: false }, () => {
-        alert(`${json.length} rows would be imported; ${numExcludedRows} rows excluded`)
-      })
-    })
-  }
-
-  render() {
-    const {
-      sheetNames,
-      selectedSheet,
-      isLoading,
-    } = this.state
-
-    return (
-      <div style={{ padding: 24 }}>
-        <div>
-          <p>Pick an Excel file:</p>
-          <input
-            ref={this.fileInputRef}
-            type="file"
-            multiple
-            onChange={this.onFilesAdded}
-          />
-        </div>
-
-        {
-          _.isEmpty(sheetNames) || (
-            <div style={{ marginTop: 24, width: 500 }}>
-              <p>Sheets to Upload:</p>
-              {
-                <Select
-                  value={selectedSheet}
-                  onChange={this.handleSheetSelection}
-                  options={this.state.sheetNames.map(n => ({ value: n, label: n }))}
-                />
-              }
-            </div>
-          )
-        }
-
-        {
-          selectedSheet && (
-            <button onClick={this.submitHandler}>
-              Import
-            </button>
-          )
-        }
-
-        {isLoading && <Spinner />}
-      </div>
-    )
-  }
+      { loading && <Spinner /> }
+    </div>
+  )
 }
 
 export default Import
