872fc10d97997d8847284cccae1f41253b0f85ce
Jonathan Lin <jon.cp.lin@gmail.com>
2020-03-19 17:46:53 -0400

    Get minimally working react-select dropdown

--- src/frontend/Orion/DataManagement/AddSourceNode/AddSourceNode.js
+++ src/frontend/Orion/DataManagement/AddSourceNode/AddSourceNode.js
@@ -1,17 +1,26 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { GET_PATH_NODES } from '../../../api/queries'
 import { useQuery, useMutation } from '@apollo/react-hooks'
 import Select from 'react-select'
 
 const AddSourceNode = () => {
+  const [selectedNode, selectNode] = useState({})
   const { data, loading } = useQuery(GET_PATH_NODES)
+
+  useEffect(() => {
+    if (!loading) selectNode(data.pathNodes[0])
+  }, [loading])
 
   if (loading) return 'Loading...'
 
-  console.log(data)
-
   return (
-    <div>hello</div>
+    <div style={{ width: 800 }}>
+      <Select
+        value={{ value: selectedNode._id, label: selectedNode.path }}
+        onChange={({ value, label }) => selectNode({ _id: value, path: label })}
+        options={data.pathNodes.map(({ _id, path }) => ({ value: _id, label: path }))}
+      />
+    </div>
   )
 }
 
