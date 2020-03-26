16e30076fc682c35661829efb4c4d0126b74df43
Matt Van Dyke <matthewlvandyke@gmail.com>
2020-03-26 12:15:25 -0400

    Switch cell values to floats instead of formatted strings

--- src/frontend/Orion/DataManagement/ExportCustomData/ExportCombinedStateLivesButton/getCombinedStateLivesExportData.js
+++ src/frontend/Orion/DataManagement/ExportCustomData/ExportCombinedStateLivesButton/getCombinedStateLivesExportData.js
@@ -27,7 +27,7 @@ export default (combinedStateLivesDoc, source, territoryType) => {
   }) => {
     const accessBucketsObj = accessBuckets
       .reduce((acc, { access, livesPercent }) => {
-        acc[access] = percentFormatterTo1DecimalPlace(livesPercent)
+        acc[access] = livesPercent
 
         return acc
       }, {})
@@ -35,26 +35,13 @@ export default (combinedStateLivesDoc, source, territoryType) => {
     allAccessValuesUniq.forEach(accessValue => {
       let datumAccessValue = accessBucketsObj[accessValue]
 
-      if (!datumAccessValue) accessBucketsObj[accessValue] = '0.0%'
+      if (!datumAccessValue) accessBucketsObj[accessValue] = 0
     })
 
     return {
       state,
       ...accessBucketsObj,
-      'Not Audited': percentFormatterTo1DecimalPlace(1 - auditedLivesPercent),
+      'Not Audited': 1 - auditedLivesPercent,
     }
   })
 }
-
-// ! Stolen from wave-app; probably should be moved to util/ directory
-const percentageFormatter = (value, decimals = 0) => (
-  // #toFixed may result in imperfect rounding,
-  // example: 859.385 doesn't round correctly for two decimal places
-  [undefined, null].includes(value) ? null : `${(value * 100).toFixed(decimals)}%`
-)
-
-const percentageFormatterToNDecimalPlaces = place => value => (
-  percentageFormatter(value, place)
-)
-
-const percentFormatterTo1DecimalPlace = percentageFormatterToNDecimalPlaces(1)
