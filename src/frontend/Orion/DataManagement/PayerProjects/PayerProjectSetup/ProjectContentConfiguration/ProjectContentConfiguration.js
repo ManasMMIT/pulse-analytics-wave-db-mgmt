60ad20a5e042ffaffa13873120e51f441ffa5f28
John Kim <jkim21@pm.me>
2020-04-02 17:54:56 -0400

    Add ProjectContentConfigurationButton to ProjectContentConfiguration

--- src/frontend/Orion/DataManagement/PayerProjects/PayerProjectSetup/ProjectContentConfiguration/ProjectContentConfiguration.js
+++ src/frontend/Orion/DataManagement/PayerProjects/PayerProjectSetup/ProjectContentConfiguration/ProjectContentConfiguration.js
@@ -1,20 +1,15 @@
 import React from 'react'
 
 import ProjectDetails from './ProjectDetails'
+import ProjectContentConfigurationButton from './ProjectContentConfigurationButton'
 import SectionCard from '../../SectionCard'
-import Button from '../../../../../components/Button'
 
 const CARD_TITLE = "Project Content Configuration"
 const CARD_SUBTITLE = "Select your payers, indications and regimens for the project. Choosing an indication and regimen will create a Payer Treatment Plan. \nPayer Treatment Plans can be edited by clicking the Configure Project Content button."
 
 const ProjectContentConfiguration = ({ projectId }) => {
   const leftHeaderContent = (
-    <Button
-      type="secondary"
-      onClick={() => {}}
-    >
-      Configure Project Content
-    </Button>
+    <ProjectContentConfigurationButton projectId={projectId} />
   )
 
   return (
