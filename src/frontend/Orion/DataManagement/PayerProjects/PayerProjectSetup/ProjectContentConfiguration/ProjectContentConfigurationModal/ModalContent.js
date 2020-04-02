5d4a12821fb0373f333197f7bf1aeecb09f5226a
John Kim <jkim21@pm.me>
2020-04-02 19:08:40 -0400

    Remove data fetching from ModalContent

--- src/frontend/Orion/DataManagement/PayerProjects/PayerProjectSetup/ProjectContentConfiguration/ProjectContentConfigurationModal/ModalContent.js
+++ src/frontend/Orion/DataManagement/PayerProjects/PayerProjectSetup/ProjectContentConfiguration/ProjectContentConfigurationModal/ModalContent.js
@@ -7,16 +7,6 @@ import { GET_PROJECT_PTPS } from '../../../../../../api/queries'
 import Spinner from '../../../../../../Phoenix/shared/Spinner'
 
 const ModalContent = ({ projectId }) => {
-  const { data, loading } = useQuery(
-    GET_PROJECT_PTPS,
-    {
-      variables: { 
-        input: { projectId } 
-      },
-    })
-
-  if (loading) return <Spinner />
-
   return (
     <div>Content</div>
   )
