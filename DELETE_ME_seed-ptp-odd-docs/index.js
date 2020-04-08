e94493bd55d2c5e216ab762d280ad8790bedf5e2
Matt Van Dyke <matthewlvandyke@gmail.com>
2020-04-08 10:02:35 -0400

    Enrich treatmentPlans on query

--- DELETE_ME_seed-ptp-odd-docs/index.js
+++ DELETE_ME_seed-ptp-odd-docs/index.js
@@ -36,7 +36,93 @@ const runDiffer = async () => {
     pulseCoreStaging.collection('populations').find().toArray(),
     pulseCoreStaging.collection('books').find().toArray(),
     pulseCoreStaging.collection('coverages').find().toArray(),
-    pulseCoreStaging.collection('treatmentPlans').find().toArray(),
+    pulseCoreStaging.collection('treatmentPlans').aggregate([
+      {
+        '$lookup': {
+          'from': 'indications',
+          'localField': 'indication',
+          'foreignField': '_id',
+          'as': 'indication'
+        }
+      }, {
+        '$lookup': {
+          'from': 'regimens',
+          'localField': 'regimen',
+          'foreignField': '_id',
+          'as': 'regimen'
+        }
+      }, {
+        '$lookup': {
+          'from': 'lines',
+          'localField': 'line',
+          'foreignField': '_id',
+          'as': 'line'
+        }
+      }, {
+        '$lookup': {
+          'from': 'populations',
+          'localField': 'population',
+          'foreignField': '_id',
+          'as': 'population'
+        }
+      }, {
+        '$lookup': {
+          'from': 'books',
+          'localField': 'book',
+          'foreignField': '_id',
+          'as': 'book'
+        }
+      }, {
+        '$lookup': {
+          'from': 'coverages',
+          'localField': 'coverage',
+          'foreignField': '_id',
+          'as': 'coverage'
+        }
+      }, {
+        '$project': {
+          'indication': {
+            '$arrayElemAt': [
+              '$indication', 0
+            ]
+          },
+          'regimen': {
+            '$arrayElemAt': [
+              '$regimen', 0
+            ]
+          },
+          'population': {
+            '$arrayElemAt': [
+              '$population', 0
+            ]
+          },
+          'line': {
+            '$arrayElemAt': [
+              '$line', 0
+            ]
+          },
+          'book': {
+            '$arrayElemAt': [
+              '$book', 0
+            ]
+          },
+          'coverage': {
+            '$arrayElemAt': [
+              '$coverage', 0
+            ]
+          }
+        }
+      }, {
+        '$project': {
+          'indication': '$indication.name',
+          'regimen': '$regimen.name',
+          'population': '$population.name',
+          'line': '$line.name',
+          'book': '$book.name',
+          'coverage': '$coverage.name'
+        }
+      }
+    ]).toArray(),
   ])
 
   const validSlugs = _.keyBy(organizations, 'slug')
