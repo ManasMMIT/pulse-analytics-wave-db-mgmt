581f5c4e92db85ada092c18033f4d34b79d603e7
John Kim <jkim21@pm.me>
2020-05-04 09:41:58 -0400

    Style ProjectPlacard

--- src/frontend/Orion/DataManagement/PayerProjects/PayerProjectsList/ProjectPlacard.js
+++ src/frontend/Orion/DataManagement/PayerProjects/PayerProjectsList/ProjectPlacard.js
@@ -3,25 +3,69 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import styled from '@emotion/styled'
 
-import { Colors, Spacing } from '../../../../utils/pulseStyles'
+import Icon from '../../../../components/Icon'
+
+import Color from '../../../../utils/color'
+import Spacing from '../../../../utils/spacing'
+import FontSpace from '../../../../utils/fontspace'
+import { formatDateMonthYearLong } from '../../../../utils/formatDate'
 
 const Placard = styled.div({
-  border: `1px solid ${ Colors.BLACK }`,
-  padding: Spacing.NORMAL,
-  margin: Spacing.NORMAL,
-  width: '20%',
+  margin: Spacing.S4,
+  padding: Spacing.S4,
+  width: 340,
+  background: Color.LIGHT_BLUE_GRAY_2,
+  borderRadius: 4,
+  fontWeight: 500,
+})
+
+const ProjectName = styled.div({
+  ...FontSpace.FS3,
+})
+
+const LastImport = styled.div({
+  opacity: 0.6,
+  ...FontSpace.FS2,
+})
+
+const ViewContainer = styled.div({
+  display: 'flex',
+  justifyContent: 'flex-end',
+  alignItems: 'center',
+  color: Color.BLUE,
+  textAlign: 'right',
+  ...FontSpace.FS2,
 })
 
 const ProjectPlacard = ({
   projectName,
   projectId,
+  timestamps,
 }) => {
+  const sortedTimestamps = timestamps.sort().reverse()
+  const latestTimestamp = formatDateMonthYearLong(sortedTimestamps[0])
+
   return (
     <Placard>
       <Link
         to={`/orion/data-management/payer-projects/${ projectId }`}
       >
-      { projectName }
+        <ProjectName>
+          { projectName }
+        </ProjectName>
+        <LastImport>
+          { `Last Data Import: ${latestTimestamp}` }
+        </LastImport>
+        <ViewContainer>
+          View { 
+            <Icon
+              iconName="arrow-drop-right"
+              height={16}
+              width={16}
+              color1={Color.BLUE}
+            /> 
+          }
+        </ViewContainer>
       </Link>
     </Placard>
   )
