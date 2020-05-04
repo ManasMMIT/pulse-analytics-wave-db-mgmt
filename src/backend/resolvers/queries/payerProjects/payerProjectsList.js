8f4ad92bb6314fa29da69543ba74a65345ffb560
John Kim <jkim21@pm.me>
2020-05-04 09:40:46 -0400

    Add agg pipeline for timestamps to payerProjectsList query

--- src/backend/resolvers/queries/payerProjects/payerProjectsList.js
+++ src/backend/resolvers/queries/payerProjects/payerProjectsList.js
@@ -1,7 +1,41 @@
 const payerProjectsList = async (parent, args, { pulseCoreDb }) => {
   const projectList = await pulseCoreDb
     .collection('tdgProjects')
-    .find({}, { _id: 1, name: 1 })
+    .aggregate([
+      {
+        $unwind: '$orgTpIds'
+      },
+      {
+        $lookup: {
+          from: 'organizations.treatmentPlans.history',
+          localField: 'orgTpIds',
+          foreignField: 'orgTpId',
+          as: 'orgTpIds'
+        }
+      },
+      {
+        $project: {
+          name: 1,
+          timestamps: '$orgTpIds.timestamp'
+        }
+      },
+      { $unwind: '$timestamps' },
+      {
+        $group: {
+          _id: { _id: '$_id', name: '$name' },
+          timestamps: {
+            $addToSet: '$timestamps'
+          }
+        }
+      },
+      {
+        $project: {
+          _id: '$_id._id',
+          name: '$_id.name',
+          timestamps: 1
+        }
+      }
+    ])
     .toArray()
 
   return projectList
