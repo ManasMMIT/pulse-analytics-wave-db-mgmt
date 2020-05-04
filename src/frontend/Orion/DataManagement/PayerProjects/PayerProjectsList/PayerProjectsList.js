75e31a9b63cc4a20bdaba57771e21d64673feb69
John Kim <jkim21@pm.me>
2020-05-04 09:42:56 -0400

    Style PayerProjectsList and add timestamps

--- src/frontend/Orion/DataManagement/PayerProjects/PayerProjectsList/PayerProjectsList.js
+++ src/frontend/Orion/DataManagement/PayerProjects/PayerProjectsList/PayerProjectsList.js
@@ -1,37 +1,43 @@
 import React from 'react'
 import { useQuery } from '@apollo/react-hooks'
 import styled from '@emotion/styled'
+import _ from 'lodash'
 
-import { GET_PAYER_PROJECTS_LIST } from '../../../../api/queries'
-import Spinner from '../../../../Phoenix/shared/Spinner'
-import { Colors, Spacing } from '../../../../utils/pulseStyles'
 import ProjectPlacard from './ProjectPlacard'
 import CreateProjectButton from './CreateProjectButton'
+import Title from '../../../../components/Title'
+import Spinner from '../../../../Phoenix/shared/Spinner'
+
+import { GET_PAYER_PROJECTS_LIST } from '../../../../api/queries'
+import Color from '../../../../utils/color'
+import Spacing from '../../../../utils/spacing'
 
 const PlacardContainer = styled.div({
   display: 'flex',
   flexWrap: 'wrap', 
-  backgroundColor: Colors.WHITE,
+  backgroundColor: Color.WHITE,
   borderRadius: 4,
+  padding: Spacing.S4,
 })
 
 const Wrapper = styled.div({
-  margin: Spacing.NORMAL,
+  margin: Spacing.S4,
 })
 
 const Header = styled.div({
-  margin: Spacing.NORMAL,
+  margin: Spacing.S4,
   display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'center',
 })
 
-const generatePlacards = ({ _id, name, }) => {
+const generatePlacards = ({ _id, name, timestamps }) => {
   return (
     <ProjectPlacard
       key={_id}
       projectId={_id}
       projectName={name}
+      timestamps={timestamps}
     />
   )
 }
@@ -40,16 +46,15 @@ const PayerProjectsList = props => {
   const { data, loading } = useQuery(GET_PAYER_PROJECTS_LIST)
   if (loading) return <Spinner />
 
+  const sortedProjects = _.sortBy(data.payerProjectsList, ['name'])
   return (
     <Wrapper>
       <Header>
-        <div>
-          Payer Projects
-        </div>
+        <Title title="Payer Projects"/>
         <CreateProjectButton/>
       </Header>
       <PlacardContainer>
-        { data.payerProjectsList.map(generatePlacards)}
+        { sortedProjects.map(generatePlacards)}
       </PlacardContainer>
     </Wrapper>
   )
