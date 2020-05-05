df544bc8058791c976e1975099b7d5085a13fe01
fhcwongaa <fhcw31@gmail.com>
2020-05-05 11:16:20 -0400

    scaffold test and mocks

---
+++ src/backend/resolvers/mutations/collection/uploadSheet/importPayerHistoricalAccessData/SheetToCore/Validator/Validator.test.js
@@ -0,0 +1,21 @@
+const Validator = require('./Validator')
+
+const validatorConfig = {
+  sheetData: [],
+  projectId: '',
+
+}
+
+describe('Validator', () => {
+  test('validates Quality of Access sheet', () => {
+    // const sheetValidator = new Validator(validatorConfig)
+  })
+
+  test('validates Additional Criteria sheet', () => {
+
+  })
+
+  test('validates Policy Links sheet', () => {
+
+  })
+})
