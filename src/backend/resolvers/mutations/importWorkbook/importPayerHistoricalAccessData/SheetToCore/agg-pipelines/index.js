2cd917ff9abe61147be0f54ede79d50490fa3184
Jonathan Lin <jon.cp.lin@gmail.com>
2020-05-13 12:06:36 -0400

    Delete deprecated enrich-org-tp-pipeline, now that only project-specific PTPs are used

--- src/backend/resolvers/mutations/importWorkbook/importPayerHistoricalAccessData/SheetToCore/agg-pipelines/index.js
+++ src/backend/resolvers/mutations/importWorkbook/importPayerHistoricalAccessData/SheetToCore/agg-pipelines/index.js
@@ -1,7 +1,5 @@
-const ENRICH_ORG_TP_PIPELINE = require('./enrich-org-tp-pipeline')
 const getProjectOrgTpsEnrichedPipeline = require('./getProjectOrgTpsEnrichedPipeline')
 
 module.exports = {
-  ENRICH_ORG_TP_PIPELINE,
   getProjectOrgTpsEnrichedPipeline,
 }
