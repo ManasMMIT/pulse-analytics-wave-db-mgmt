c672d3eafa22b7418d95f465652f8b860c2124b3
Jonathan Lin <jon.cp.lin@gmail.com>
2020-06-09 20:18:39 -0400

    Chore/hide admin settings from consultants/plrs 729 (#594)
    * Create super-users hash
    
    * Stop consultants from being able to go to admin settings url
    
    * Hide admin settings in Orion sidebar from consultants

--- src/frontend/Orion/Configuration/Configuration.js
+++ src/frontend/Orion/Configuration/Configuration.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 
+import { useAuth0 } from '../../../react-auth0-spa'
+import superUsersById from '../../utils/super-users'
 import AdminSettings from './AdminSettings'
 import ProductsIndications from './ProductsIndications'
 
-const Configuration = () => (
-  <Switch>
-    <Route path={'/orion/configuration/products-indications'} component={ProductsIndications} />
-    <Route path={'/orion/configuration/admin-settings'} component={AdminSettings} />
-    <Redirect to="/orion/configuration/products-indications" />
-  </Switch>
-)
+const Configuration = () => {
+  const { user } = useAuth0()
+  const isSuperUser = user.sub in superUsersById
+
+  return (
+    <Switch>
+      <Route path={'/orion/configuration/products-indications'} component={ProductsIndications} />
+      { isSuperUser && <Route path={'/orion/configuration/admin-settings'} component={AdminSettings} /> }
+      <Redirect to="/orion/configuration/products-indications" />
+    </Switch>
+  )
+}
 
 export default Configuration
---
+++ src/frontend/utils/super-users.js
@@ -0,0 +1,11 @@
+const superUsersById = {
+  'auth0|5e3b3ed1ae39650da7fc2dbc': 'jonathan.lin',
+  'auth0|5e5fd908b752c70d52f75faf': 'clairekim',
+  'auth0|5e3b36aab661bc0e5912d831': 'chuck.flores',
+  'auth0|5e601df196c4280d4a9ae4f3': 'johnkim',
+  'auth0|5e2a03a47397b20e870ad385': 'admin',
+  'auth0|5e406107521e060e7f178e3a': 'mattvanlaw',
+  'auth0|5e4fe2a8facf340d44ae322a': 'maya',
+}
+
+export default superUsersById
