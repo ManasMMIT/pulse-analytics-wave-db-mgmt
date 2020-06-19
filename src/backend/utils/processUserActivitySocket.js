13c0d553b2446f5ffc4dd353bf6ee72b2b3e68d3
Matt Van Dyke <matthewlvandyke@gmail.com>
2020-06-19 12:31:40 -0400

    Add logout handler in user activty socket, server-side

--- src/backend/utils/processUserActivitySocket.js
+++ src/backend/utils/processUserActivitySocket.js
@@ -1,6 +1,8 @@
 const LISTEN_USER_TRACKER = 'USER_TRACKER'
 const EMIT_USER_TRACKER = 'USER_TRACKER_DATA'
 
+const LOGOUT_USER_TRACKER = 'LOGOUT_USER_TRACKER'
+
 module.exports = io => {
   const userActivity = {}
 
@@ -18,6 +20,11 @@ module.exports = io => {
         userActivity[user.sub].timestamp = new Date()
       }
 
+      socket.emit(EMIT_USER_TRACKER, userActivity)
+    })
+
+    socket.on(LOGOUT_USER_TRACKER, userId => {
+      delete userActivity[userId]
       socket.emit(EMIT_USER_TRACKER, userActivity)
     })
   })
