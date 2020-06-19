b3f9d094009449680a1456a9227299c195545baf
Matt Van Dyke <matthewlvandyke@gmail.com>
2020-06-19 16:06:01 -0400

    Reduce user information moving across socket; leave note on crazy cookies

--- src/frontend/UserTracker/useTrackerSocket.js
+++ src/frontend/UserTracker/useTrackerSocket.js
@@ -9,10 +9,18 @@ const EMIT_USER_TRACKER = 'USER_TRACKER'
 const LISTEN_USER_TRACKER = 'USER_TRACKER_DATA'
 const LOGIN_USER_TRACKER = 'LOGIN_USER_TRACKER'
 
+// ! HACK DAY NOTE: Headers eventually get too large and fail! Had to clear cookies to fix this. Why are the cookies growing??
+
 const useTrackerSocket = () => {
   const [trackedUserData, setTrackedUserData] = useState({})
   const location = useLocation()
-  const { user } = useAuth0()
+  const { user: entireAuth0User } = useAuth0()
+
+  const user = {
+    sub: entireAuth0User.sub,
+    picture: entireAuth0User.picture,
+    name: entireAuth0User.name,
+  }
 
   useEffect(() => {
     socket.emit(LOGIN_USER_TRACKER, {
@@ -37,11 +45,25 @@ const useTrackerSocket = () => {
       setTrackedUserData(data)
   })
 
-  const otherUsersOnPage = Object.values(trackedUserData)
-    .filter(({ user: localUser, pathname: userPathname }) => (userPathname === location.pathname
-      && user.sub !== localUser.sub))
+  const otherUsers = Object.values(trackedUserData).filter(({ user: localUser}) => user.sub !== localUser.sub)
 
-  return { otherUsersOnPage }
+  const otherUsersOnPage = otherUsers
+    .filter(({ pathname: userPathname }) => userPathname === location.pathname)
+
+  const otherUsersOnTool = getOtherUsersOnTool(otherUsers, location.pathname)
+
+  return {
+    otherUsersOnPage,
+    otherUsersOnTool,
+  }
 }
 
 export default useTrackerSocket
+
+const TOOL_PATHS = ['/orion', '/phoenix', '/delphi', '/payer-projects']
+
+const getOtherUsersOnTool = (otherUsersTrackedData, currentPathname) => {
+  const currentToolPath = TOOL_PATHS.find(toolPath => currentPathname.includes(toolPath))
+
+  return otherUsersTrackedData.filter(({ pathname }) => pathname.includes(currentToolPath))
+}
