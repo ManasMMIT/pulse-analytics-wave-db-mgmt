212555429ec3dfd033ab935a57a82702047dcd9e
Matt Van Dyke <matthewlvandyke@gmail.com>
2020-06-19 12:33:33 -0400

    Refactor client socket code to listen closer to connection + flip bubble opacity

--- src/frontend/SocketListener.js
+++ src/frontend/SocketListener.js
@@ -1,28 +1,57 @@
-import { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { useLocation } from 'react-router-dom'
+import _ from 'lodash'
 
 import socket from './api/socket'
 import { useAuth0 } from '../react-auth0-spa'
+
+import UserProfile from './UserProfile'
+
 const EMIT_USER_TRACKER = 'USER_TRACKER'
 const LISTEN_USER_TRACKER = 'USER_TRACKER_DATA'
+const LOGOUT_USER_TRACKER = 'LOGOUT_USER_TRACKER'
+
+const wrapperStyle = {
+  position: 'absolute',
+  bottom: 12,
+  right: 12,
+}
 
 export default () => {
-  const [trackedUserData, setTrackedUserData] = useState([])
+  const [trackedUserData, setTrackedUserData] = useState({})
   const location = useLocation()
   const { user } = useAuth0()
 
-  useEffect(() => {
+  if (!trackedUserData || !trackedUserData[user.sub] || trackedUserData[user.sub].location.pathname !== location.pathname) {
     socket.emit(
       EMIT_USER_TRACKER,
       {
         user,
-        accountIcon: null,
         location,
       }
     )
-  }, [location])
+  }
 
-  socket.on(LISTEN_USER_TRACKER, setTrackedUserData)
-console.log(trackedUserData)
-  return null
+  socket.on(LISTEN_USER_TRACKER, data => {
+    if (!_.isEqual(data, trackedUserData)) setTrackedUserData(data)
+  })
+
+  console.log(trackedUserData)
+  // TODO: Hide own facebub
+  return (
+    <div style={wrapperStyle}>
+      {
+        Object.values(trackedUserData).map(({ user, location: userLocation }) => {
+          let profileStyle = { opacity: 0.2 }
+          if (userLocation.pathname === location.pathname) profileStyle = { opacity: 1 }
+
+          return (
+              <UserProfile style={profileStyle} key={user.sub} user={user} />
+          )
+        })
+      }
+    </div>
+  )
 }
+
+export const clearUserProfileTracker = (userId) => socket.emit(LOGOUT_USER_TRACKER, userId)
