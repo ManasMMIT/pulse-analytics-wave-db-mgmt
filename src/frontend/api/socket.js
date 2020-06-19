bbd4f35de2ecdfde15f10ff03e4c86965d8a94bf
Matt Van Dyke <matthewlvandyke@gmail.com>
2020-06-19 12:32:53 -0400

    Lazy load socket in socket.js
    This change might not be production ready

--- src/frontend/api/socket.js
+++ src/frontend/api/socket.js
@@ -1,11 +1,11 @@
 import socketIoClient from 'socket.io-client'
 
-let socket 
+let socket
 
 if (process.env.NODE_ENV === 'production') {
-  socket = socketIoClient('https://www.polaris.pulse-tools.com')
+  socket = socket || socketIoClient('https://www.polaris.pulse-tools.com')
 } else {
-  socket = socketIoClient('http://localhost:1337')
+  socket = socket || socketIoClient('http://localhost:1337')
 }
 
 export default socket
