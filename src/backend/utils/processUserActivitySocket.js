e9bac4699096afeb87422c896ca95a846f457b73
Matt Van Dyke <matthewlvandyke@gmail.com>
2020-06-19 15:14:42 -0400

    Add LOGIN logic to socket processing in server and refactor code

--- src/backend/utils/processUserActivitySocket.js
+++ src/backend/utils/processUserActivitySocket.js
@@ -1,31 +1,42 @@
 const LISTEN_USER_TRACKER = 'USER_TRACKER'
 const EMIT_USER_TRACKER = 'USER_TRACKER_DATA'
 
+const LOGIN_USER_TRACKER = 'LOGIN_USER_TRACKER'
 const LOGOUT_USER_TRACKER = 'LOGOUT_USER_TRACKER'
 
 module.exports = io => {
   const userActivity = {}
 
   io.on('connection', socket => {
-    socket.on(LISTEN_USER_TRACKER, ({ user, location }) => {
-      if (!userActivity[user.sub]) {
-        userActivity[user.sub] = {
-          timestamp: new Date(),
-          user,
-          location
-        }
-      }
-      else {
-        userActivity[user.sub].location = location
-        userActivity[user.sub].timestamp = new Date()
-      }
+    socket.on(LISTEN_USER_TRACKER, ({ user, pathname }) => {
+      updateUserActivity(userActivity, user, pathname)
 
       socket.emit(EMIT_USER_TRACKER, userActivity)
+    })
+
+    socket.on(LOGIN_USER_TRACKER, ({ user, pathname }) => {
+      updateUserActivity(userActivity, user, pathname)
+
+      socket.broadcast.emit(EMIT_USER_TRACKER, userActivity)
     })
 
     socket.on(LOGOUT_USER_TRACKER, userId => {
       delete userActivity[userId]
-      socket.emit(EMIT_USER_TRACKER, userActivity)
+      socket.broadcast.emit(EMIT_USER_TRACKER, userActivity)
     })
   })
+}
+
+function updateUserActivity(userActivity, user, pathname) {
+  if (!userActivity[user.sub]) {
+    userActivity[user.sub] = {
+      timestamp: new Date(),
+      user,
+      pathname
+    }
+  }
+  else {
+    userActivity[user.sub].pathname = pathname
+    userActivity[user.sub].timestamp = new Date()
+  }
 }
