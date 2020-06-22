eec22a7765ea9b7f47f328bff658b855b431446f
Felix Wong <fhcw31@gmail.com>
2020-06-22 12:36:47 -0400

    Chore/static analysis tools/plrs 669 (#616)
    * remove await and add console timers
    
    * fix console timers
    
    * remove materialization console log
    
    * tweak console timers
    
    * add unique Id to timer
    
    * add alert title and description to alertStatus PLRS-790
    
    * change payer projects icon
    
    * change default rows to 100
    
    * uniq by timestamp long
    
    * fix long scroll issue
    
    * tweak error messages in import
    
    * add sheet name to error msg
    
    * add better error formatting
    
    * add error pound system
    
    * add id to css
    
    * make tests pass
    
    * remove comment
    
    * add reload warning
    
    * add description to changelog
    
    * move changelog
    
    * install husky, lint staged and prettier
    
    * add lint-staged config
    
    * testing commit
    
    * testing commit
    
    * testing commit
    
    * testing changes 2
    
    * more changes
    
    * add test to pre-commit
    
    * comment out lint staged
    
    * add lint staged to pre-commit
    
    * removes redundant chanelog statement
    
    * move deps to dev dependencies
    
    * update changelog
    
    * test prettier
    
    * ignore deleteTreatmentPlans on pre-commit
    
    * update changelog
    
    * change labeling in changelog

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import styled from '@emotion/styled'
-import { useAuth0 } from "../react-auth0-spa";
+import { useAuth0 } from '../react-auth0-spa'
 import { transparentize } from 'polished'
 import { ApolloProvider } from '@apollo/react-hooks'
 import ApolloClient from 'apollo-boost'
 import UserProfile from './UserProfile'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faSignOutAlt, faQuestionCircle } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faSignOutAlt,
+  faQuestionCircle,
+} from '@fortawesome/free-solid-svg-icons'
 
 import {
   BrowserRouter as Router,
@@ -29,10 +32,14 @@ import PayerProject from './PayerProjects/PayerProject'
 import { Colors, Spacing } from './utils/pulseStyles'
 
 const IconSource = {
-  PHOENIX: 'https://res.cloudinary.com/pulsedatatools/image/upload/v1573136582/polaris/icons/phoenix-1-white.svg',
-  ORION: 'https://res.cloudinary.com/pulsedatatools/image/upload/v1573136582/polaris/icons/orion-1-white.svg',
-  DELPHI: 'https://res.cloudinary.com/pulsedatatools/image/upload/v1573136582/polaris/icons/mercury-1-white.svg',
-  PAYER_PROJECTS: 'https://res.cloudinary.com/pulsedatatools/image/upload/v1591273667/polaris/icons/payer-1-white.svg',
+  PHOENIX:
+    'https://res.cloudinary.com/pulsedatatools/image/upload/v1573136582/polaris/icons/phoenix-1-white.svg',
+  ORION:
+    'https://res.cloudinary.com/pulsedatatools/image/upload/v1573136582/polaris/icons/orion-1-white.svg',
+  DELPHI:
+    'https://res.cloudinary.com/pulsedatatools/image/upload/v1573136582/polaris/icons/mercury-1-white.svg',
+  PAYER_PROJECTS:
+    'https://res.cloudinary.com/pulsedatatools/image/upload/v1591273667/polaris/icons/payer-1-white.svg',
 }
 
 const PolarisSidebar = styled.div({
@@ -43,11 +50,11 @@ const PolarisSidebar = styled.div({
 })
 
 const iconStyle = {
- width: 20,
- height: 20,
+  width: 20,
+  height: 20,
 }
 
-const activeLinkStyle = activeColor => ({
+const activeLinkStyle = (activeColor) => ({
   background: transparentize(0.8, activeColor),
   opacity: 1,
 })
@@ -61,14 +68,14 @@ const StyledNavLink = styled(NavLink)({
   opacity: 0.6,
   ':hover': {
     background: transparentize(0.92, Colors.WHITE),
-  }
+  },
 })
 
 const sidebarBottomSectionStyle = {
   marginTop: 'auto',
   display: 'flex',
   flexDirection: 'column',
-  alignItems: 'center'
+  alignItems: 'center',
 }
 
 const logoutButtonStyle = {
@@ -83,7 +90,7 @@ const LogoutContainer = styled.div({
   ':hover': {
     background: transparentize(0.92, Colors.WHITE),
     opacity: 1,
-  }
+  },
 })
 
 const SupportDocumentationContainer = styled.a({
@@ -93,20 +100,29 @@ const SupportDocumentationContainer = styled.a({
   ':hover': {
     background: transparentize(0.92, Colors.WHITE),
     opacity: 1,
-  }
+  },
 })
 
 const App = () => {
-  const { loading, isAuthenticated, loginWithRedirect, logout, accessToken } = useAuth0()
+  const {
+    loading,
+    isAuthenticated,
+    loginWithRedirect,
+    logout,
+    accessToken,
+  } = useAuth0()
 
-  const logoutWithRedirect = () => logout({
-    returnTo: window.location.origin
-  })
+  const logoutWithRedirect = () =>
+    logout({
+      returnTo: window.location.origin,
+    })
 
   if (loading) return null
 
   if (!isAuthenticated) {
-    const { location: { pathname, search } } = window
+    const {
+      location: { pathname, search },
+    } = window
     loginWithRedirect({ appState: { targetUrl: pathname + search } })
     return
   }
@@ -117,95 +133,94 @@ const App = () => {
       resolvers,
       typeDefs,
     },
-    request: operation => {
-      operation.setContext(context => ({
+    request: (operation) => {
+      operation.setContext((context) => ({
         headers: {
           ...context.headers,
           Authorization: `Bearer ${accessToken}` || null,
         },
-      })
-      );
+      }))
     },
-    onError: errors => {
+    onError: (errors) => {
       const { networkError } = errors
 
       if (
-        networkError
-        && networkError.result
-        && networkError.result.message === 'jwt expired'
+        networkError &&
+        networkError.result &&
+        networkError.result.message === 'jwt expired'
       ) {
         localStorage.clear()
         client.clearStore().then(logoutWithRedirect)
       }
-    }
+    },
   })
 
   return (
     <ApolloProvider client={client}>
-        <Router>
-          <div style={{ display: 'flex', height: '100vh' }}>
-            <PolarisSidebar>
-              <StyledNavLink
-                to="/phoenix"
-                activeStyle={activeLinkStyle(Colors.PHOENIX)}
-              >
-                <img style={iconStyle} src={IconSource.PHOENIX} />
-              </StyledNavLink>
-              <StyledNavLink
-                to="/orion"
-                activeStyle={activeLinkStyle(Colors.ORION)}
-              >
-                <img style={iconStyle} src={IconSource.ORION} />
-              </StyledNavLink>
-              <StyledNavLink
-                to="/delphi"
-                activeStyle={activeLinkStyle(Colors.DELPHI)}
-              >
-                <img style={iconStyle} src={IconSource.DELPHI} />
-              </StyledNavLink>
-              <StyledNavLink
-                to="/payer-projects"
-                activeStyle={activeLinkStyle(Colors.DELPHI)}
-              >
-                <img style={iconStyle} src={IconSource.PAYER_PROJECTS} />
-              </StyledNavLink>
+      <Router>
+        <div style={{ display: 'flex', height: '100vh' }}>
+          <PolarisSidebar>
+            <StyledNavLink
+              to="/phoenix"
+              activeStyle={activeLinkStyle(Colors.PHOENIX)}
+            >
+              <img style={iconStyle} src={IconSource.PHOENIX} />
+            </StyledNavLink>
+            <StyledNavLink
+              to="/orion"
+              activeStyle={activeLinkStyle(Colors.ORION)}
+            >
+              <img style={iconStyle} src={IconSource.ORION} />
+            </StyledNavLink>
+            <StyledNavLink
+              to="/delphi"
+              activeStyle={activeLinkStyle(Colors.DELPHI)}
+            >
+              <img style={iconStyle} src={IconSource.DELPHI} />
+            </StyledNavLink>
+            <StyledNavLink
+              to="/payer-projects"
+              activeStyle={activeLinkStyle(Colors.DELPHI)}
+            >
+              <img style={iconStyle} src={IconSource.PAYER_PROJECTS} />
+            </StyledNavLink>
 
-              <div style={sidebarBottomSectionStyle}>
-                <UserProfile />
-                <LogoutContainer title="Log Out">
-                  <FontAwesomeIcon
-                    style={logoutButtonStyle}
-                    onClick={() => {
-                      localStorage.clear()
-                      client.clearStore().then(logoutWithRedirect)
-                    }}
-                    icon={faSignOutAlt}
-                    color={Colors.WHITE}
-                  />
-                </LogoutContainer>
-                <SupportDocumentationContainer
-                  href="https://dedhamgroup.atlassian.net/servicedesk/customer/portal/2/topic/6163d9cf-df29-498b-9c5a-40300462eb76"
-                  target="_blank"
-                  title="Polaris Support Documentation for TDG"
-                >
-                  <FontAwesomeIcon
-                    style={logoutButtonStyle}
-                    icon={faQuestionCircle}
-                    color={Colors.WHITE}
-                  />
-                </SupportDocumentationContainer>
-              </div>
-            </PolarisSidebar>
-            <Switch>
-              <Route path="/phoenix" component={Phoenix} />
-              <Route path="/orion" component={Orion} />
-              <Route path="/delphi" component={Delphi} />
-              <Route exact path="/payer-projects" component={PayerProjectsList} />
-              <Route path="/payer-projects/:projectId" component={PayerProject} />
-              <Redirect to="/phoenix" />
-            </Switch>
-          </div>
-        </Router>
+            <div style={sidebarBottomSectionStyle}>
+              <UserProfile />
+              <LogoutContainer title="Log Out">
+                <FontAwesomeIcon
+                  style={logoutButtonStyle}
+                  onClick={() => {
+                    localStorage.clear()
+                    client.clearStore().then(logoutWithRedirect)
+                  }}
+                  icon={faSignOutAlt}
+                  color={Colors.WHITE}
+                />
+              </LogoutContainer>
+              <SupportDocumentationContainer
+                href="https://dedhamgroup.atlassian.net/servicedesk/customer/portal/2/topic/6163d9cf-df29-498b-9c5a-40300462eb76"
+                target="_blank"
+                title="Polaris Support Documentation for TDG"
+              >
+                <FontAwesomeIcon
+                  style={logoutButtonStyle}
+                  icon={faQuestionCircle}
+                  color={Colors.WHITE}
+                />
+              </SupportDocumentationContainer>
+            </div>
+          </PolarisSidebar>
+          <Switch>
+            <Route path="/phoenix" component={Phoenix} />
+            <Route path="/orion" component={Orion} />
+            <Route path="/delphi" component={Delphi} />
+            <Route exact path="/payer-projects" component={PayerProjectsList} />
+            <Route path="/payer-projects/:projectId" component={PayerProject} />
+            <Redirect to="/phoenix" />
+          </Switch>
+        </div>
+      </Router>
     </ApolloProvider>
   )
 }
