e38c9a5e34341b822a9bf6267cd68820e159f84d
Jonathan Lin <jon.cp.lin@gmail.com>
2020-06-26 14:39:42 -0400

    Chore/cascade delete obm payer connections/plrs 844 (#635)
    * Copy global deleteOrganization util into payer delete to make it specifically additionally delete obm_payer entries
    
    * Add GET_OBM_PAYER_PARTNERSHIPS to delete payer button refetchQueries
    
    * Add David Anderson to super-users list

--- src/backend/resolvers/mutations/organization/payer/delete.js
+++ src/backend/resolvers/mutations/organization/payer/delete.js
@@ -1,3 +1,86 @@
-const deleteOrganization = require('../delete')
+const { ObjectId } = require('mongodb')
 
-module.exports = deleteOrganization
+const deleteTreatmentPlanConnectionsAndHistory = require('../utils/deleteTreatmentPlanConnectionsAndHistory')
+
+const deletePayerOrganization = async (
+  parent,
+  { input: { _id: stringId } },
+  { pulseDevDb, pulseCoreDb, coreRoles, mongoClient },
+  info
+) => {
+  const _id = ObjectId(stringId)
+
+  const session = mongoClient.startSession()
+
+  let deletedOrg
+
+  await session.withTransaction(async () => {
+    // STEP 0: Delete all affiliated org-treatmentPlans and trash historical docs
+    await deleteTreatmentPlanConnectionsAndHistory({
+      db: pulseCoreDb,
+      session,
+      organizationId: _id,
+    })
+
+    // STEP 1: delete the org from organizations collection
+    const { value } = await pulseCoreDb
+      .collection('organizations')
+      .findOneAndDelete({ _id }, { session })
+
+    deletedOrg = value
+
+    // STEP 2: Delete the org across all teams' resources' accounts
+    await coreRoles.updateMany(
+      {
+        'resources.accounts._id': _id,
+      },
+      {
+        $pull: {
+          'resources.$[].accounts': { _id },
+        },
+      },
+      { session }
+    )
+
+    // STEP 3: Delete the org across all pulse-dev.users.nodes.resources
+    await pulseDevDb.collection('users.nodes.resources').updateMany(
+      {
+        'resources.accounts._id': _id,
+      },
+      {
+        $pull: {
+          'resources.$[].accounts': { _id },
+        },
+      },
+      { session }
+    )
+
+    // STEP 4: Remove the org from pulse-core connections and pulse-dev providers collection
+    const connectionDocs = await pulseCoreDb
+      .collection('organizations.connections')
+      .find({ orgs: _id }, { session })
+      .toArray()
+
+    const connectionIdsToRemove = connectionDocs.map(({ _id }) => _id)
+
+    // if one or more connections targeted for deletion are between a payer and a VBM,
+    // that's fine because those deletion ops will merely do nothing to the
+    // providers collection in dev (since that only has provider/VBM connections)
+    await pulseDevDb
+      .collection('newProviders')
+      .deleteMany({ _id: { $in: connectionIdsToRemove } }, { session })
+
+    await pulseCoreDb
+      .collection('organizations.connections')
+      .deleteMany({ orgs: _id }, { session })
+
+    // STEP 5: Delete all obm-payer connections
+    await pulseCoreDb
+      .collection('obm_payers')
+      .deleteMany({ payerId: _id }, { session })
+  })
+
+  return deletedOrg
+}
+
+module.exports = deletePayerOrganization
--- src/frontend/utils/super-users.js
+++ src/frontend/utils/super-users.js
@@ -6,6 +6,7 @@ const superUsersById = {
   'auth0|5e2a03a47397b20e870ad385': 'admin',
   'auth0|5e406107521e060e7f178e3a': 'mattvanlaw',
   'auth0|5e4fe2a8facf340d44ae322a': 'maya',
+  'auth0|5ef5f9cf59a29e0019c5d4cd': 'david',
 }
 
 export default superUsersById
