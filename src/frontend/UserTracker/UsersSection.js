67646aa081bcb4367833fa6ce21d49bb21a66102
Matt Van Dyke <matthewlvandyke@gmail.com>
2020-06-25 22:18:12 -0400

    Add useChatSocket and chat modal to UserTracker

--- src/frontend/UserTracker/UsersSection.js
+++ src/frontend/UserTracker/UsersSection.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+import Modal from '../components/Modal'
 import UserProfile from '../UserProfile'
+import useChatSocket from './useChatSocket'
 
 const profileStyle = {
   clipPath: `circle(15px at center)`,
@@ -15,10 +17,52 @@ const UsersSection = ({ title, users }) => {
     </div>
     <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
       {users.map(({ user }) => {
-        return (<UserProfile style={profileStyle} key={user.sub} user={user} />)
+        return <User user={user} />
       })}
     </div>
   </div>
 }
 
 export default UsersSection
+
+const inputStyle = {
+  padding: '12px 24px',
+  background: 'white',
+  width: '90%',
+  margin: '12px',
+}
+
+const User = ({ user }) => {
+  const [isOpen, setIsOpen] = useState(false)
+  const [message, setMessage] = useState('')
+  const { emitMessage, chatLog } = useChatSocket(user.sub)
+
+  return (
+    <div onClick={() => setIsOpen(!isOpen)} key={user.sub}>
+      <UserProfile style={profileStyle} user={user} />
+      <Modal
+        handleClose={() => setIsOpen(false)}
+        show={isOpen}
+        title={`Chatting with... ${user.name}`}
+      >
+        <form
+          style={{ opacity: 1 }}
+          onSubmit={e => {
+            e.preventDefault()
+            emitMessage(message)
+            setMessage('')
+          }}
+        >
+          <div>
+            {chatLog}
+          </div>
+          <input
+            style={inputStyle}
+            value={message}
+            onChange={e => setMessage(e.target.value)}
+          />
+        </form>
+      </Modal>
+    </div>
+  )
+}
