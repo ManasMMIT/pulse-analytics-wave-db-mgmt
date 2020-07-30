a5bd8ba4fe5dd493ab52c36d43f8bbea75af74e7
Matt Van Dyke <matthewlvandyke@gmail.com>
2020-07-30 15:49:02 -0400

    Remove useEffect in CreatePersonComponent

--- src/frontend/cache-tests/createPerson.cache.test.js/components/CreatePersonComponent.js
+++ src/frontend/cache-tests/createPerson.cache.test.js/components/CreatePersonComponent.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { useMutation } from '@apollo/client/react/hooks'
 
 import createPersonDoc from '../gql-tags/createPersonGraphQLTag'
