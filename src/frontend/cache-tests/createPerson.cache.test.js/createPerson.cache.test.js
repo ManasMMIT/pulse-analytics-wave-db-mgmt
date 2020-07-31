dbaf3a2b81d9ad78dd1ab30df4d7d41f06e6a025
Matt Van Dyke <matthewlvandyke@gmail.com>
2020-07-31 09:55:38 -0400

    Cleanup and improve test
    Reuse app's graphql tags; await mutation and trigger directly

--- src/frontend/cache-tests/createPerson.cache.test.js/createPerson.cache.test.js
+++ src/frontend/cache-tests/createPerson.cache.test.js/createPerson.cache.test.js
@@ -1,43 +1,24 @@
 import React from 'react'
-import { ObjectId } from 'mongodb'
-
 import { InMemoryCache } from '@apollo/client'
 import { MockedProvider } from '@apollo/client/testing'
-import { render, fireEvent } from '@testing-library/react'
+import { render } from '@testing-library/react'
 
 import initialCache from './initialCache.json'
 import initialPeople from './initialPeople'
 
-import getPeopleDoc from './gql-tags/getPeopleGraphQLTag'
-import createPersonDoc from './gql-tags/createPersonGraphQLTag'
-
 import GetPeopleComponent from './components/GetPeopleComponent'
 import CreatePersonComponent from './components/CreatePersonComponent'
 
-const waitForResponse = () => new Promise((res) => setTimeout(res, 0))
+import { GET_PEOPLE } from 'frontend/api/queries'
+
+import { CREATE_PERSON } from 'frontend/api/mutations'
 
 describe('Create Person', () => {
+  jest.setTimeout(500000)
   it('should update the cache correctly when triggered with refetch', async () => {
     const cache = new InMemoryCache().restore(initialCache)
 
-    const queryMocks1 = {
-      request: {
-        query: getPeopleDoc,
-      },
-      result: {
-        data: {
-          people: initialPeople,
-        },
-      },
-    }
-
-    render(
-      <MockedProvider cache={cache} mocks={[queryMocks1]}>
-        <GetPeopleComponent />
-      </MockedProvider>
-    )
-
-    const newObjId = new ObjectId()
+    const newObjId = '123'
 
     const newPerson = {
       _id: newObjId, // NOT ACTUALLY DONE IN THE APP
@@ -45,26 +26,32 @@ describe('Create Person', () => {
       lastName: 'Test',
     }
     const newCachedPerson = {
-      _id: newObjId.toString(),
+      _id: newObjId,
       firstName: newPerson.firstName,
       __typename: 'Person',
+    }
+
+    const createPersonPayload = {
+      _id: newObjId,
+      firstName: newPerson.firstName,
+      __typename: 'CreatePersonPayload',
     }
 
     const mutationMocks = [
       {
         request: {
-          query: createPersonDoc,
+          query: CREATE_PERSON,
           variables: { input: newPerson },
         },
         result: {
           data: {
-            createPerson: newCachedPerson,
+            createPerson: createPersonPayload,
           },
         },
       },
       {
         request: {
-          query: getPeopleDoc,
+          query: GET_PEOPLE,
         },
         result: {
           data: {
@@ -74,18 +61,17 @@ describe('Create Person', () => {
       },
     ]
 
-    const { getByTestId } = render(
+    let myMutation = {}
+
+    render(
       <MockedProvider cache={cache} mocks={mutationMocks}>
-        <CreatePersonComponent person={newPerson} />
+        <CreatePersonComponent person={newPerson} myMutation={myMutation} />
       </MockedProvider>
     )
 
-    const button = getByTestId('button-mutate')
-    fireEvent.click(button)
-    await waitForResponse()
-
+    await myMutation.createPerson()
     const updatedCache = cache.extract()
 
-    expect(!!updatedCache[`Person:${newObjId.toString()}`]._id).toBe(true)
+    expect(!!updatedCache[`Person:${newObjId}`]._id).toBe(true)
   })
 })
