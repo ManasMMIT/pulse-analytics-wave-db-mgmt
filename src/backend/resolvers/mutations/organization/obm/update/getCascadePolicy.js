5187e4ca5dee64cdd3d6f55edada8fcb0832069e
Matt Van Dyke <matthewlvandyke@gmail.com>
2020-09-03 12:04:50 -0400

    Task/sync obm update to dev colls/plrs 1109 (#748)
    * Refactor + extend obm update resolver cascade logic, using a config policy approach
    
    * Rename cascade policy util
    
    * Use _.get for string fieldmaps
    
    * Fix prettier styling
    
    * Remove findObj from users.nodes.res cascade policy

---
+++ src/backend/resolvers/mutations/organization/obm/update/getCascadePolicy.js
@@ -0,0 +1,109 @@
+const getCascadePolicy = ({ input, pulseDevDb, session }) => [
+  {
+    input,
+    into: {
+      db: pulseDevDb,
+      coll: 'users.nodes.resources',
+      options: {
+        session,
+        arrayFilters: [
+          { 'resource.accounts': { $exists: true } },
+          { 'el._id': input._id },
+        ],
+      },
+    },
+    fieldMap: [
+      {
+        foreignField: 'resources.$[resource].accounts.$[el].slug',
+        localField: 'slug',
+      },
+    ],
+  },
+  {
+    input,
+    session,
+    into: {
+      db: pulseDevDb,
+      coll: 'obms',
+      findObj: { _id: input._id },
+    },
+    fieldMap: [
+      'businessModel',
+      'organization',
+      'organizationTiny',
+      'slug',
+      'start',
+    ],
+  },
+  {
+    input,
+    session,
+    into: {
+      db: pulseDevDb,
+      coll: 'obmsServices',
+      findObj: { 'obm._id': input._id },
+    },
+    fieldMap: [
+      {
+        foreignField: 'obm.slug',
+        localField: 'slug',
+      },
+      {
+        foreignField: 'obm.organization',
+        localField: 'organization',
+      },
+      {
+        foreignField: 'obm.organizationTiny',
+        localField: 'organizationTiny',
+      },
+    ],
+  },
+  {
+    input,
+    session,
+    into: {
+      db: pulseDevDb,
+      coll: 'obmsPayers',
+      findObj: { 'obm._id': input._id },
+    },
+    fieldMap: [
+      {
+        foreignField: 'obm.slug',
+        localField: 'slug',
+      },
+      {
+        foreignField: 'obm.organization',
+        localField: 'organization',
+      },
+      {
+        foreignField: 'obm.organizationTiny',
+        localField: 'organizationTiny',
+      },
+    ],
+  },
+  {
+    input,
+    session,
+    into: {
+      db: pulseDevDb,
+      coll: 'obmsInfluencers',
+      findObj: { 'obm._id': input._id },
+    },
+    fieldMap: [
+      {
+        foreignField: 'obm.slug',
+        localField: 'slug',
+      },
+      {
+        foreignField: 'obm.organization',
+        localField: 'organization',
+      },
+      {
+        foreignField: 'obm.organizationTiny',
+        localField: 'organizationTiny',
+      },
+    ],
+  },
+]
+
+module.exports = getCascadePolicy
