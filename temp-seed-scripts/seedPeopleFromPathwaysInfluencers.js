34eef9a07636608240fb52614f75aa4224818f4e
fhcwongaa <fhcw31@gmail.com>
2020-10-26 14:14:42 -0400

    clean connection logic

--- temp-seed-scripts/seedPeopleFromPathwaysInfluencers.js
+++ temp-seed-scripts/seedPeopleFromPathwaysInfluencers.js
@@ -20,27 +20,7 @@ const EXISTING_PEOPLE_COLLECTION = 'EXISTING_pathways_people'
 const EVENTS_COLLECTIONS = 'events'
 const ALL_EXCLUDED_ROWS_COLL = 'EXCLUDED_pathwaysInfluencers'
 
-const seedPeopleFromPathwaysInfluencers = async () => {
-  const dbs = await connectToMongoDb()
-
-  const pulseCoreDb = dbs.db('pulse-core')
-  const pulseDevDb = dbs.db('pulse-dev')
-
-  // Find all influencers that needs to be udpated
-  const pathwaysInfluencers = await pulseDevDb.collection('RAW_pathwaysInfluencers')
-    .find({
-      $and: [
-        { type: 'Pathways' },
-        {
-          $or: [
-            { npiNumber: { $nin: ['N/A', null] } },
-            { personId: { $ne: null } }
-          ]
-        }
-      ]
-    })
-    .toArray()
-
+const cleanCollections = async ({ pulseCoreDb, pulseDevDb }) => {
   console.log('Remove All JOIN_pathways_people documents...')
   await pulseCoreDb.collection(JOIN_PEOPLE_COLLECTION)
     .deleteMany()
@@ -65,180 +45,182 @@ const seedPeopleFromPathwaysInfluencers = async () => {
   console.log('Reset global excluded rows collection')
   await pulseDevDb.collection(ALL_EXCLUDED_ROWS_COLL)
     .deleteMany()
+}
+
+const materializeDevInfluencers = ({ pulseCoreDb, pulseDevDb, joinDocId }) => async () => {
+  const materializedDoc = await pulseCoreDb
+    .collection(JOIN_PEOPLE_COLLECTION)
+    .aggregate(
+      getMaterializationAggPipeline({
+        $match: { _id: joinDocId },
+      })
+    )
+    .next()
+
+  await pulseDevDb
+    .collection(TEMP_PEOPLE_COLLECTION)
+    .insertOne(materializedDoc)
+
+  console.log(`Materialized Doc for ${joinDocId}`)
+}
+
+const getSeedOps = ({ pulseCoreDb, pulseDevDb, keyedIndicationsByName }) => async ({
+  personId,
+  npiNumber,
+  firstName,
+  middleName,
+  lastName,
+  affiliation,
+  affiliationPosition,
+  primaryState,
+  managementType,
+  influencerType,
+  slug,
+  title,
+  indication,
+  indicationCategory,
+  priority,
+  chairIndications,
+  startDate,
+  startQuarter,
+  endQuarter,
+  outdated,
+  exclusionSettings,
+  alertDate,
+  alertDescription,
+  disclosureTotal,
+  disclosureDate1,
+  disclosureDate2,
+  disclosureDate3,
+  disclosureDate4,
+  internalNote,
+  source,
+  contact,
+  internalRole,
+}) => {
+  let joinPersonId = ObjectId(personId)
+
+  // See if person with personId already exists (anyone with a personId will most likely already be in the DB)
+  const personWithId = await pulseCoreDb.collection(PEOPLE_COLLECTION).findOne({ _id: joinPersonId })
+  const personWithNpi = await pulseCoreDb.collection(PEOPLE_COLLECTION).findOne({ nationalProviderIdentifier: Number(npiNumber) })
+  const personWithName = await pulseCoreDb.collection(PEOPLE_COLLECTION).findOne({
+    firstName,
+    middleName,
+    lastName
+  })
+
+  const pathwaysOrg = await pulseCoreDb.collection('organizations')
+    .findOne({ type: 'Pathways', slug })
+
+  const indicationIds = []
+
+  indication.forEach(indicationName => {
+    const indicationIdObj = keyedIndicationsByName[indicationName]
+    if (indicationIdObj) indicationIds.push(indicationIdObj._id)
+  })
+
+  if (personWithNpi) joinPersonId = personWithNpi._id
+  if (personWithId) joinPersonId = personWithId._id
+
+  // Create the person if it doesn't already exist in the people collection
+  if (personWithId === null && personWithNpi === null) {
+    joinPersonId = await pulseCoreDb.collection(PEOPLE_COLLECTION)
+      .insertOne({
+        _id: ObjectId(),
+        firstName,
+        lastName,
+        middleName,
+        affiliation,
+        affiliationPosition,
+        primaryState,
+        nationalProviderIdentifier: npiNumber ? Number(npiNumber) : null,
+        createdOn: new Date(),
+        updatedOn: new Date(),
+        isPathwaysPeople: true // flag to differentiate the injected people
+      })
+
+    console.log(`Inserted Person of ${ firstName } ${ middleName } ${ lastName } into people collection`)
+  }
+
+  const joinDocId = ObjectId()
+  const joinPathwaysPeopleDoc = {
+    _id: joinDocId,
+    personId: joinPersonId,
+    pathwaysId: pathwaysOrg._id,
+    indicationIds,
+    pathwaysInfluencerTypes: influencerType,
+    tumorTypeSpecialty: indicationCategory,
+    position: title,
+    priority,
+    startDate,
+    startQuarter,
+    endDate: outdated,
+    endQuarter,
+    source,
+    contact,
+    internalRole,
+    internalFields: {
+      internalNotes: internalNote,
+      pathwaysManagementTypes: managementType,
+      valueChairsIndications: chairIndications,
+      totalDisclosures: disclosureTotal,
+      dateDisclosure1: disclosureDate1,
+      dateDisclosure2: disclosureDate2,
+      dateDisclosure3: disclosureDate3,
+      dateDisclosure4: disclosureDate4,
+    },
+    alert: {
+      date: alertDate,
+      type: 'Influencer',
+      description: alertDescription,
+    },
+    exclusionSettings: {
+      isExcluded: Boolean(exclusionSettings),   
+      reason: exclusionSettings,
+    },
+  }
+
+  await pulseCoreDb.collection(JOIN_PEOPLE_COLLECTION)
+    .insertOne(joinPathwaysPeopleDoc)
+
+  console.log(`Inserted Join Doc for ${ joinDocId }`)
+
+  const shouldSkipMaterialization = joinPathwaysPeopleDoc.exclusionSettings.isExcluded
+
+  // Materialize Temp Pathways Inflluencers on pulse-dev unless
+  // isExcluded is truthy
+  if (shouldSkipMaterialization) {
+    console.log(`Doc for ${joinDocId} skipped because isExcluded truthy`) 
+  } else {
+    await materializeDevInfluencers({ pulseCoreDb, pulseDevDb, joinDocId })
+  }
+}
+
+const seedPeopleFromPathwaysInfluencers = async () => {
+  const dbs = await connectToMongoDb()
+
+  const pulseCoreDb = dbs.db('pulse-core')
+  const pulseDevDb = dbs.db('pulse-dev')
+
+  // 1. Clean/Reset all collections
+  cleanCollections()
+
+  // Find all influencers that needs to be udpated
+  const pathwaysInfluencers = await pulseDevDb.collection('RAW_pathwaysInfluencers')
+    .find({ type: 'Pathways' })
+    .toArray()
 
   const indications = await pulseCoreDb.collection('indications')
     .find()
     .toArray()
 
-  const initiallyExcludedRows = await pulseDevDb.collection('RAW_pathwaysInfluencers')
-    .find({ 
-      $or: [
-          { type: { $ne: 'Pathways' } }, 
-          { npiNumber: { $in: ['N/A', null] }, personId: null }
-        ] 
-      })
-    .toArray()
-
-  await pulseDevDb.collection(ALL_EXCLUDED_ROWS_COLL)
-    .insertMany(initiallyExcludedRows)
-    
   const keyedIndicationsByName = _.keyBy(indications, 'name')
 
-  const ops = pathwaysInfluencers.map(async datum => {
-    const {
-      personId,
-      npiNumber,
-      firstName,
-      middleName,
-      lastName,
-      affiliation,
-      affiliationPosition,
-      primaryState,
-      managementType,
-      influencerType,
-      slug,
-      title,
-      indication,
-      indicationCategory,
-      priority,
-      chairIndications,
-      startDate,
-      startQuarter,
-      endQuarter,
-      outdated,
-      exclusionSettings,
-      alertDate,
-      alertDescription,
-      disclosureTotal,
-      disclosureDate1,
-      disclosureDate2,
-      disclosureDate3,
-      disclosureDate4,
-      internalNote,
-      source,
-      contact,
-      internalRole,
-    } = datum
-
-    // See if person with personId already exists (anyone with a personId will most likely already be in the DB)
-    const personWithId = await pulseCoreDb.collection(PEOPLE_COLLECTION).findOne({ _id: ObjectId(personId) })
-    const personWithNpi = await pulseCoreDb.collection(PEOPLE_COLLECTION).findOne({ nationalProviderIdentifier: Number(npiNumber) })
-    const personWithName = await pulseCoreDb.collection(PEOPLE_COLLECTION).findOne({
-      firstName,
-      middleName,
-      lastName
-    })
-
-    // if person in pathwaysInfluencers does not already exist
-    if (personWithId === null && personWithNpi === null) {
-      // Insert the person only if it doesn't have a corresponding name
-      if (personWithName === null) {
-        const { insertedId } = await pulseCoreDb.collection(PEOPLE_COLLECTION)
-          .insertOne({
-            _id: ObjectId(),
-            firstName,
-            lastName,
-            middleName,
-            affiliation,
-            affiliationPosition,
-            primaryState,
-            nationalProviderIdentifier: npiNumber ? Number(npiNumber) : null,
-            createdOn: new Date(),
-            updatedOn: new Date(),
-            isPathwaysPeople: true // flag to differentiate the injected people
-          })
-          console.log(`Inserted Person of ${ firstName } ${ middleName } ${ lastName } into people collection`)
-        
-        const pathwaysOrg = await pulseCoreDb.collection('organizations')
-          .findOne({ type: 'Pathways', slug })
-  
-        const indicationIds = []
-  
-        indication.forEach(indicationName => {
-          const indicationIdObj = keyedIndicationsByName[indicationName]
-          if (indicationIdObj) indicationIds.push(indicationIdObj._id)
-        })
-    
-        const joinDocId = ObjectId()
-        const joinPathwaysPeopleDoc = {
-          _id: joinDocId,
-          personId: insertedId,
-          pathwaysId: pathwaysOrg._id,
-          indicationIds,
-          pathwaysInfluencerTypes: influencerType,
-          tumorTypeSpecialty: indicationCategory,
-          position: title,
-          priority,
-          startDate,
-          startQuarter,
-          endDate: outdated,
-          endQuarter,
-          source,
-          contact,
-          internalRole,
-          internalFields: {
-            internalNotes: internalNote,
-            pathwaysManagementTypes: managementType,
-            valueChairsIndications: chairIndications,
-            totalDisclosures: disclosureTotal,
-            dateDisclosure1: disclosureDate1,
-            dateDisclosure2: disclosureDate2,
-            dateDisclosure3: disclosureDate3,
-            dateDisclosure4: disclosureDate4,
-          },
-          alert: {
-            date: alertDate,
-            type: 'Influencer',
-            description: alertDescription,
-          },
-          exclusionSettings: {
-            isExcluded: Boolean(exclusionSettings),   
-            reason: exclusionSettings,
-          },
-        }
-  
-        await pulseCoreDb.collection(JOIN_PEOPLE_COLLECTION)
-          .insertOne(joinPathwaysPeopleDoc)
-        console.log(`Inserted Join Doc for ${ joinDocId }`)
-
-        // Materialize Temp Pathways Inflluencers on pulse-dev unless
-        // isExcluded is truthy
-        if (joinPathwaysPeopleDoc.exclusionSettings.isExcluded) {
-          console.log(`Doc for ${joinDocId} skipped because isExcluded truthy`)
-        } else {
-          const materializedDoc = await pulseCoreDb
-            .collection(JOIN_PEOPLE_COLLECTION)
-            .aggregate(
-              getMaterializationAggPipeline({
-                $match: { _id: joinDocId },
-              })
-            )
-            .next()
-
-          await pulseDevDb
-            .collection(TEMP_PEOPLE_COLLECTION)
-            .insertOne(materializedDoc)
-
-          console.log(`Materialized Doc for ${joinDocId}`)
-        }
-      } else {
-        // Generates list of people with conflicting names
-        await pulseCoreDb.collection(EXISTING_PEOPLE_COLLECTION)
-          .insertOne(datum)
-
-        console.log(`Inserting person: ${ firstName} ${ middleName } ${ lastName } into EXISTING_pathways_people`)
-      }
-    } else {
-      await pulseDevDb
-        .collection(ALL_EXCLUDED_ROWS_COLL)
-        .insertOne(datum)
-
-      console.log(`Excluding person and inserting into excluded collection`)
-    }
-  })
+  const seedOpsCallback = getSeedOps({ pulseCoreDb, pulseDevDb, keyedIndicationsByName })
+  const ops = pathwaysInfluencers.map(seedOpsCallback)
 
   await Promise.all(ops)
-
+  
   await dbs.close()
 }
 
