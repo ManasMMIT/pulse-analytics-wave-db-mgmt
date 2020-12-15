38b73c1fa2d2551f5905434f146d905d723e4fc6
Jonathan Lin <jon.cp.lin@gmail.com>
2020-12-15 13:12:27 -0500

    Adjust obmOrganizations to only fetch obms instead of all mbms

--- src/backend/resolvers/queries/organizations/mbm/obm/obm.js
+++ src/backend/resolvers/queries/organizations/mbm/obm/obm.js
@@ -3,7 +3,7 @@ const { MBM_TOOL_ID } = require('../../../../../global-tool-ids')
 const obmOrganizations = async (parent, args, { pulseCoreDb }) =>
   pulseCoreDb
     .collection('organizations')
-    .find({ toolIds: MBM_TOOL_ID })
+    .find({ toolIds: MBM_TOOL_ID, type: 'Oncology Benefit Manager' })
     .toArray()
 
 module.exports = obmOrganizations
