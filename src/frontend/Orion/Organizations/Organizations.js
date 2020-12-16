a808679b0419be10fe405fd8def480eb5e939d73
Jonathan Lin <jon.cp.lin@gmail.com>
2020-12-16 10:19:37 -0500

    Adjust routing in Organizations to include /lbm route and Lbm component

--- src/frontend/Orion/Organizations/Organizations.js
+++ src/frontend/Orion/Organizations/Organizations.js
@@ -6,29 +6,16 @@ import Payer from './Payer'
 import Provider from './Provider'
 import Apm from './Apm'
 import Obm from './Obm'
+import Lbm from './Lbm'
 
-const Organizations = props => (
+const Organizations = (props) => (
   <Switch>
-    <Route
-      path={'/orion/organizations/pathways'}
-      component={Pathways}
-    />
-    <Route
-      path={'/orion/organizations/payer'}
-      component={Payer}
-    />
-    <Route
-      path={'/orion/organizations/provider'}
-      component={Provider}
-    />
-    <Route
-      path={'/orion/organizations/apm'}
-      component={Apm}
-    />
-    <Route
-      path={'/orion/organizations/obm'}
-      component={Obm}
-    />
+    <Route path={'/orion/organizations/pathways'} component={Pathways} />
+    <Route path={'/orion/organizations/payer'} component={Payer} />
+    <Route path={'/orion/organizations/provider'} component={Provider} />
+    <Route path={'/orion/organizations/apm'} component={Apm} />
+    <Route path={'/orion/organizations/obm'} component={Obm} />
+    <Route path={'/orion/organizations/lbm'} component={Lbm} />
     <Redirect to={'/orion/organizations/pathways'} />
   </Switch>
 )
