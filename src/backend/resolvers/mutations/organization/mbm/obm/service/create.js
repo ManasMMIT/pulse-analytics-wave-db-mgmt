03d36db799c967e18a4a1372cff6df832f805419
Matt Van Dyke <matthewlvandyke@gmail.com>
2021-01-27 11:26:49 -0500

    Simplify obm service create resolver logic

--- src/backend/resolvers/mutations/organization/mbm/obm/service/create.js
+++ src/backend/resolvers/mutations/organization/mbm/obm/service/create.js
@@ -1,11 +1,9 @@
 const axios = require('axios')
 
 const createObmService = async (parent, { input }, { pulseCoreDb }, info) => {
-  const { data } = await axios
-    .post('obm-services/', { ...input, id: input._id })
-    .catch((e) => {
-      throw new Error(JSON.stringify(e.response.data))
-    })
+  const { data } = await axios.post('obm-services/', input).catch((e) => {
+    throw new Error(JSON.stringify(e.response.data))
+  })
 
   return data
 }
