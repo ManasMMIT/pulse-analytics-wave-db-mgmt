391f312bc7a70dce30b15619decf3e6aee87fb3a
Matt Van Dyke <matthewlvandyke@gmail.com>
2021-03-09 16:57:15 -0500

    Bugfix/delphi table breaking in prod/plrs 1487 (#933)
    * Replace delphi UsersTable table component
    
    * Tweak SendEmailPanel width style
    
    * Remove react-tabulator css stylesheet
    
    * Uninstall react-tabulator library
    
    * Update changelog

--- src/frontend/Delphi/SendEmailPanel/PathwaysEmailCard/UsersTable.js
+++ src/frontend/Delphi/SendEmailPanel/PathwaysEmailCard/UsersTable.js
@@ -1,124 +1,99 @@
-import React, { useRef } from 'react'
-import styled from '@emotion/styled'
-import { ReactTabulator } from 'react-tabulator'
+import React from 'react'
 import { useQuery } from '@apollo/react-hooks'
-import { transparentize } from 'polished'
 
 import Spinner from 'frontend/components/Spinner'
+import Table from 'frontend/components/Table'
 
 import { pathwaysEmailSubscription } from '../../../utils/email-subscription-options'
 import { GET_USERS } from '../../../api/queries'
 
-import { Colors, Spacing } from '../../../utils/pulseStyles'
-
-import 'react-tabulator/lib/styles.css'
-import 'react-tabulator/lib/css/tabulator.min.css'
-
-import './usersTableStyles.css'
+import { Colors } from '../../../utils/pulseStyles'
 
 const subscriptionId = pathwaysEmailSubscription._id
 
-const DownloadCsvButton = styled.button({
-  background: transparentize(0.85, Colors.PRIMARY),
-  border: 'none',
-  borderRadius: 4,
-  color: Colors.PRIMARY,
-  cursor: 'pointer',
-  fontSize: 12,
-  fontWeight: 700,
-  lineHeight: '22px',
-  padding: `${Spacing.TINY} ${Spacing.SMALL}`,
-  ':hover': {
-    background: transparentize(0.75, Colors.PRIMARY),
+const COLUMNS = [
+  {
+    Header: 'Client',
+    accessor: 'client.description',
+    sortType: 'text',
   },
-  ':focus': {
-    outline: 'none',
+  {
+    Header: 'Username',
+    accessor: 'username',
+    sortType: 'text',
   },
-  ':active': {
-    background: transparentize(0.65, Colors.PRIMARY),
-  }
-})
-
-const TABLE_COLUMNS = [
-  { title: "Client", field: "client.description" },
-  { title: "Username", field: "username" },
-  { title: "Email", field: "email" },
+  {
+    Header: 'Email',
+    accessor: 'email',
+    sortType: 'text',
+  },
 ]
 
-const TABLE_OPTIONS = {
-  downloadDataFormatter: (data) => data,
-  downloadReady: (fileContents, blob) => blob,
-}
-
 const UsersTable = () => {
-  const tableRef = useRef(null)
+  const now = new Date()
+  const filename = `user-email-list-${now.toISOString()}`
 
-  const { data, loading, error } = useQuery(
-    GET_USERS,
-    {
-      variables: { subscriptionId },
-      fetchPolicy: 'network-only', // prevent caching to always get latest
-    },
-  )
+  const { data, loading, error } = useQuery(GET_USERS, {
+    variables: { subscriptionId },
+    fetchPolicy: 'network-only', // prevent caching to always get latest
+  })
 
-  if (loading) return (
-    <div
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '100%',
-        textAlign: 'center',
-      }}
-    >
-      <div>
-        <p style={{ color: Colors.PRIMARY, fontSize: 12, fontWeight: 600,}}>
-          Loading Client Emails Table
-        </p>
-        <Spinner size="32" />
+  if (loading)
+    return (
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '100%',
+          textAlign: 'center',
+        }}
+      >
+        <div>
+          <p style={{ color: Colors.PRIMARY, fontSize: 12, fontWeight: 600 }}>
+            Loading Client Emails Table
+          </p>
+          <Spinner size="32" />
+        </div>
       </div>
-    </div>
-  )
-  if (error) return (
-    <div
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '100%',
-        textAlign: 'center',
-      }}
-    >
-      <p style={{ color: Colors.RED, fontSize: 12, fontWeight: 600, lineHeight: '22px' }}>
-        There was an error loading the Client Emails Table.
-        <br />
-        Please reload the page before sending client emails for verification. ⟲
-      </p>
-    </div>
-  )
+    )
+  if (error)
+    return (
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '100%',
+          textAlign: 'center',
+        }}
+      >
+        <p
+          style={{
+            color: Colors.RED,
+            fontSize: 12,
+            fontWeight: 600,
+            lineHeight: '22px',
+          }}
+        >
+          There was an error loading the Client Emails Table.
+          <br />
+          Please reload the page before sending client emails for verification.
+          ⟲
+        </p>
+      </div>
+    )
 
   const { users } = data
 
   return (
-    <>
-      <div style={{ display: 'flex', justifyContent: 'flex-end', marginBottom: 12 }}>
-        <DownloadCsvButton
-          onClick={() => tableRef.current.table.download("csv", "usersToEmail.csv")}
-        >
-          Download Table as CSV
-        </DownloadCsvButton>
-      </div>
-
-      <ReactTabulator
-        ref={tableRef}
-        data={users}
-        columns={TABLE_COLUMNS}
-        tooltips={true}
-        layout="fitData"
-        options={TABLE_OPTIONS}
-        height="100%"
-      />
-    </>
+    <Table
+      width={'calc(100vw - 500px)'}
+      data={users}
+      columns={COLUMNS}
+      exportStyle={{ margin: 24 }}
+      exportProps={{ filename }}
+    />
   )
 }
 
--- src/frontend/Delphi/SendEmailPanel/SendEmailPanel.js
+++ src/frontend/Delphi/SendEmailPanel/SendEmailPanel.js
@@ -8,8 +8,8 @@ const SendEmailPanel = () => (
   <div
     style={{
       background: lighten(0.05, Colors.LIGHT_GRAY_1),
-      flex: '1 0 auto',
-      padding: 24
+      width: '100%',
+      padding: 24,
     }}
   >
     <PathwaysEmailCard />
