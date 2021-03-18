ca5539dcbc54680b8efd06e50e0035509de9d7c4
Matt Van Dyke <matthewlvandyke@gmail.com>
2021-03-18 17:20:22 -0400

    Add associated products and uniq set of regimens to  mbDetail page

--- src/frontend/Orion/Configuration/MarketBaskets/MarketBasketDetail/MarketBasketDetail.js
+++ src/frontend/Orion/Configuration/MarketBaskets/MarketBasketDetail/MarketBasketDetail.js
@@ -1,4 +1,5 @@
 import Spinner from 'frontend/components/Spinner'
+import _ from 'lodash'
 import React from 'react'
 import { Link, useParams } from 'react-router-dom'
 
@@ -8,13 +9,29 @@ const MarketBasketDetail = () => {
   const { marketBasketId } = useParams()
   const { marketBaskets, loading } = useMarketBasketData()
   if (loading) return <Spinner />
-
-  const marketBasket = marketBaskets.find(({ id }) => id === marketBasketId)
+  const { name, products } = marketBaskets.find(({ id }) => id === marketBasketId)
+  const uniqRegs = _.uniqBy(products.reduce((acc, { regimens }) => [...acc, ...regimens], []), 'id')
 
   return (
     <div>
       <Link to="/orion/configuration/market-baskets/">Back</Link>
-      <h1>{marketBasket.name}</h1>
+      <h1>{name}</h1>
+      <h2>Products</h2>
+      <ul>
+        {
+          products.map(({ id, generic_name, brand_name }) => (
+            <li key={id}>{brand_name ? `${generic_name} (${brand_name})` : generic_name}</li>
+          ))
+        }
+      </ul>
+      <h2>Products' Unique Regimens</h2>
+      <ul>
+        {
+          uniqRegs.map(({ id, name }) => (
+            <li key={id}>{name}</li>
+          ))
+        }
+      </ul>
     </div>
   )
 }
