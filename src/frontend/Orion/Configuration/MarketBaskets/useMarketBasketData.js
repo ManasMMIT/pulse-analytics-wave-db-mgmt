4ca50f31854c049958c7728975de652872c0ba37
Matt Van Dyke <matthewlvandyke@gmail.com>
2021-03-18 17:03:58 -0400

    DRY up useMarketBasketData hook & use vega prod/reg data

--- src/frontend/Orion/Configuration/MarketBaskets/useMarketBasketData.js
+++ src/frontend/Orion/Configuration/MarketBaskets/useMarketBasketData.js
@@ -2,55 +2,72 @@ import { useState, useEffect } from 'react'
 import { useQuery } from '@apollo/react-hooks'
 import _ from 'lodash'
 
+// ? Might want to fetch heavy vega collections when user first navigates to orion
 import {
   GET_SOURCE_INDICATIONS,
-  GET_SOURCE_PRODUCTS,
+  GET_VEGA_PRODUCTS,
+  GET_VEGA_REGIMENS,
   GET_MARKET_BASKETS,
 } from 'frontend/api/queries'
 
 const useMarketBasketData = () => {
   const { data: mbData, loading: mbLoading } = useQuery(GET_MARKET_BASKETS)
   const { data: indData, loading: indLoading } = useQuery(GET_SOURCE_INDICATIONS)
+  const { data: vegaProdData, loading: vegaProdLoading } = useQuery(GET_VEGA_PRODUCTS)
+  const { data: vegaRegimenData, loading: vegaRegimenLoading } = useQuery(GET_VEGA_REGIMENS)
+
   const [indMap, setIndMap] = useState({})
-
-  useEffect(() => {
-    if (!mbLoading && !indLoading) {
-      const indsByUuidMap = _.keyBy(indData.indications, 'uuid')
-
-      setIndMap(indsByUuidMap)
-    }
-  }, [mbLoading, indLoading])
-
-  const { data: productData, loading: productLoading } = useQuery(GET_SOURCE_PRODUCTS)
   const [productMap, setProductMap] = useState({})
-
-  useEffect(() => {
-    if (!mbLoading && !productLoading) {
-      const productsByUuidMap = _.keyBy(productData.products, 'uuid')
-
-      setProductMap(productsByUuidMap)
-    }
-  }, [mbLoading, productLoading])
+  const [regimenMap, setRegimenMap] = useState({})
+  const [hydratedMbs, setHydratedMbData] = useState([])
 
   const areAnyMapsLoadingOrEmpty = [
     indLoading || _.isEmpty(indMap),
-    productLoading || _.isEmpty(productMap),
+    vegaProdLoading || _.isEmpty(productMap),
+    vegaRegimenLoading || _.isEmpty(regimenMap),
   ].some(bool => bool)
 
-  const [hydratedMbs, setHydratedMbData] = useState([])
+  useEffect(
+    getMapSetterCallback({
+      mbLoading,
+      mapDataLoading: indLoading,
+      setMapData: setIndMap,
+      data: indData,
+      dataKey: 'indications',
+    }),
+    [mbLoading, indLoading]
+  )
+
+  useEffect(
+    getMapSetterCallback({
+      mbLoading,
+      mapDataLoading: vegaProdLoading,
+      setMapData: setProductMap,
+      data: vegaProdData,
+      dataKey: 'vegaProducts',
+    }),
+    [mbLoading, vegaProdLoading]
+  )
+
+  useEffect(
+    getMapSetterCallback({
+      mbLoading,
+      mapDataLoading: vegaRegimenLoading,
+      setMapData: setRegimenMap,
+      data: vegaRegimenData,
+      dataKey: 'vegaRegimens',
+    }),
+    [mbLoading, vegaRegimenLoading]
+  )
 
   useEffect(() => {
     if (mbData && !areAnyMapsLoadingOrEmpty) {
-
-      const hydratedMbs = mbData.marketBaskets.map(({
-        indication,
-        products,
-        ...rest
-      }) => ({
-        indication: indMap[indication],
-        products: products.map(uuid => productMap[uuid]),
-        ...rest
-      }))
+      const hydratedMbs = getHydratedMbs({
+        mbData,
+        indMap,
+        productMap,
+        regimenMap,
+      })
 
       setHydratedMbData(hydratedMbs)
     }
@@ -58,10 +75,36 @@ const useMarketBasketData = () => {
 
   return {
     marketBaskets: hydratedMbs,
-    // ? not sure about load state yet
-    // * thinking if maps aren't ready or hydratedMbs haven't been set at least once to have data
     loading: areAnyMapsLoadingOrEmpty || _.isEmpty(hydratedMbs),
   }
+}
+
+const getMapSetterCallback = ({ mbLoading, mapDataLoading, setMapData, data, dataKey }) => () => {
+  if (!mbLoading && !mapDataLoading) {
+    const id = data[dataKey][0].uuid ? 'uuid' : 'id'
+    const map = _.keyBy(data[dataKey], id)
+
+    setMapData(map)
+  }
+}
+
+const getHydratedMbs = ({ mbData, indMap, productMap, regimenMap }) => {
+  return mbData.marketBaskets.map(({
+    indication,
+    products,
+    ...rest
+  }) => ({
+    indication: indMap[indication],
+    products: products.map(id => {
+      const product = productMap[id]
+
+      return {
+        ...product,
+        regimens: product.regimens.map(id => regimenMap[id]),
+      }
+    }),
+    ...rest
+  }))
 }
 
 export default useMarketBasketData
