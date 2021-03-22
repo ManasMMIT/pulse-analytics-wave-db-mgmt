3faabb0cb16de3ddc687e7dc1575963e0b63813c
Matt Van Dyke <matthewlvandyke@gmail.com>
2021-03-22 09:40:17 -0400

    Add TeamSection to Detail page

--- src/frontend/Orion/Configuration/MarketBaskets/MarketBasketDetail/MarketBasketDetail.js
+++ src/frontend/Orion/Configuration/MarketBaskets/MarketBasketDetail/MarketBasketDetail.js
@@ -1,3 +1,6 @@
+import { nest } from 'd3-collection'
+import { useQuery } from '@apollo/react-hooks'
+import { GET_TEAMS } from 'frontend/api/queries'
 import Spinner from 'frontend/components/Spinner'
 import _ from 'lodash'
 import React from 'react'
@@ -5,11 +8,52 @@ import { Link, useParams } from 'react-router-dom'
 
 import useMarketBasketData from './../useMarketBasketData'
 
+const TeamSection = ({ teamSubscriptions }) => {
+  const { data, loading } = useQuery(GET_TEAMS)
+
+  if (loading) return <Spinner />
+  const { teams } = data
+  const teamsByUuid = _.keyBy(teams, 'uuid')
+  const teamSubsHydrated = teamSubscriptions.map(({ team, ...rest }) => ({
+    team: teamsByUuid[team],
+    client: teamsByUuid[team].client,
+    ...rest,
+  }))
+
+  const subsByClientThenTeam = nest()
+    .key(row => row.client.description)
+    .key(row => row.team.description)
+    .rollup(row => row[0])
+    .object(teamSubsHydrated)
+
+  const clientTeamNodes = Object.entries(subsByClientThenTeam).map(([client, teamsObj]) => {
+    return (
+      <div key={client}>
+        <div>{client}</div>
+        <ul>
+          {
+            Object.entries(teamsObj).map(([teamName, sub]) => {
+              return <li key={teamName}>{teamName}</li>
+            })
+          }
+        </ul>
+      </div>
+    )
+  })
+
+  return (
+    <>
+      <h3>Client-Team Subs</h3>
+      { clientTeamNodes}
+    </>
+  )
+}
+
 const MarketBasketDetail = () => {
   const { marketBasketId } = useParams()
   const { marketBaskets, loading } = useMarketBasketData()
   if (loading) return <Spinner />
-  const { name, products } = marketBaskets.find(({ id }) => id === marketBasketId)
+  const { name, products, team_subscriptions: teamSubscriptions } = marketBaskets.find(({ id }) => id === marketBasketId)
   const uniqRegs = _.uniqBy(products.reduce((acc, { regimens }) => [...acc, ...regimens], []), 'id')
 
   return (
@@ -32,6 +76,7 @@ const MarketBasketDetail = () => {
           ))
         }
       </ul>
+      <TeamSection teamSubscriptions={teamSubscriptions} />
     </div>
   )
 }
