63343fc14053e9fe98a34e0954b58c350f05cac1
Matt Van Dyke <matthewlvandyke@gmail.com>
2021-03-22 11:42:40 -0400

    Split useMbListData into data and mutation hooks

--- src/frontend/Orion/Configuration/MarketBaskets/MarketBasketList/useMarketBasketListData.jsx
+++ src/frontend/Orion/Configuration/MarketBaskets/MarketBasketList/useMarketBasketListData.jsx
@@ -1,11 +1,36 @@
+/* 
+  ? Premise-lvl ideas:
+    * These view hooks will output all data
+    * needed for a page to completely render.
+    * 
+    * These view hooks can safely be used for
+    * any children components in a view without making
+    * additional network requests.
+    * 
+    * Mutations are tied to a view's data -- the idea being
+    * the page will need to either refetch/re-hydrate its data
+    * post-op or will need data to populate forms with the data.
+*/
+
 import { useState, useEffect } from 'react'
-import { useQuery } from '@apollo/react-hooks'
+import { useQuery, useMutation } from '@apollo/react-hooks'
 import _ from 'lodash'
 
 import { GET_MARKET_BASKETS } from 'frontend/api/queries'
+import { CREATE_MARKET_BASKET, UPDATE_MARKET_BASKET } from 'frontend/api/mutations'
 import { useIndicationsMap } from '../data-hooks'
 
-const useMarketBasketListData = () => {
+const getHydratedMbs = (data, { indicationsMap }) => {
+  return data.marketBaskets.map(({
+    indication,
+    ...rest
+  }) => ({
+    indication: indicationsMap[indication],
+    ...rest
+  }))
+}
+
+const useData = () => {
   const marketBasketQuery = useQuery(GET_MARKET_BASKETS)
 
   const [hydratedMbs, setHydratedMbData] = useState([])
@@ -36,32 +61,34 @@ const useMarketBasketListData = () => {
     marketBasketsData = marketBasketQuery.data.marketBaskets
   }
 
-  const data = {
+  return {
     marketBaskets: {
       data: marketBasketsData,
       loading: marketBasketQuery.loading,
       isHydrating,
     }
   }
+}
 
-  const mutations = {
+const useMutations = () => {
+  const [save] = useMutation(CREATE_MARKET_BASKET, {
+    refetchQueries: [{ query: GET_MARKET_BASKETS }],
+    onError: alert,
+  })
 
+  const [update] = useMutation(UPDATE_MARKET_BASKET, {
+    refetchQueries: [{ query: GET_MARKET_BASKETS }],
+    onError: alert,
+  })
+
+  return {
+    marketBasket: {
+      save,
+      update,
+    }
   }
-
-  return [
-    data,
-    mutations,
-  ]
 }
 
-const getHydratedMbs = (data, { indicationsMap }) => {
-  return data.marketBaskets.map(({
-    indication,
-    ...rest
-  }) => ({
-    indication: indicationsMap[indication],
-    ...rest
-  }))
-}
+const useMarketBasketListData = () => [useData(), useMutations()]
 
 export default useMarketBasketListData
