054d2cd5702862bbe9b6e306f85f0542ec190639
Matt Van Dyke <matthewlvandyke@gmail.com>
2021-03-30 16:35:50 -0400

    Add src node form functionality to edit role-node form (#955)

--- src/frontend/Orion/Configuration/AdminSettings/EditRoleNodeView/EditRoleNodeForm.js
+++ src/frontend/Orion/Configuration/AdminSettings/EditRoleNodeView/EditRoleNodeForm.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { useMutation } from '@apollo/react-hooks'
+import _ from 'lodash'
+
+import Color from 'frontend/utils/color'
 
 import {
   UPDATE_TEAM_NODE
@@ -12,8 +15,6 @@ import {
   InputLabel,
   StyledButton,
 } from './styledComponents'
-
-import { Colors } from '../../../../utils/pulseStyles'
 
 const EditRoleNodeForm = ({
   node,
@@ -43,28 +44,70 @@ const EditRoleNodeForm = ({
     awaitRefetchQueries: true,
   })
 
-  const handleInputChange = e => {
+  const [newTextField, setNewTextField] = useState(null)
+
+  const handleInputChange = (e) => {
     let value = e.target.value
     const key = e.target.name
 
-    let newTextObj = {}
-    if (['subtitle', 'caption', 'title'].includes(key)) {
-      newTextObj = { text: { ...nodeData.text, [key]: value } }
+    if (e.target.type === 'number') value = Number(value)
 
-      setNodeData(state => ({ ...state, ...newTextObj }))
-
-      return
-    }
-
-    if (key === 'order') value = Number(value)
-
-    setNodeData(state => ({ ...state, [key]: value }))
+    setNodeData((state) => ({ ...state, [key]: value }))
   }
 
-  return (
-    <div style={{ padding: 24, background: Colors.LIGHT_BLUE_GRAY_2 }}>
-      <FormLabel>Update Team Node</FormLabel>
+  const handleTextInputChange = (e) => {
+    let value = e.target.value
+    const key = e.target.name
 
+    const newTextObj = { text: { ...nodeData.text, [key]: value } }
+
+    setNodeData((state) => ({ ...state, ...newTextObj }))
+  }
+
+  const handleAddTextField = () => {
+    const camelCasedNewTextField = _.camelCase(newTextField)
+    if (camelCasedNewTextField === 'tdgTimestamp') {
+      alert('tdgTimestamp can not be set in this modal')
+      setNewTextField('')
+      return null
+    }
+    if (
+      !newTextField ||
+      nodeData.text[camelCasedNewTextField] ||
+      _.isEmpty(newTextField)
+    ) {
+      return null
+    }
+
+    setNodeData((state) => ({
+      ...state,
+      text: { ...state.text, [camelCasedNewTextField]: null },
+    }))
+    setNewTextField('')
+  }
+
+  const handleRemoveTextField = (key) => {
+    setNodeData((state) => {
+      const clonedState = _.cloneDeep(state)
+      const { text, ...restState } = clonedState
+
+      delete text[key]
+
+      return {
+        text,
+        ...restState,
+      }
+    })
+  }
+
+  const textInputs = getTextInputs(
+    nodeData,
+    handleTextInputChange,
+    handleRemoveTextField
+  )
+
+  return (
+    <div style={{ padding: 24, background: Color.LIGHT_BLUE_GRAY_2 }}>
       <InputContainer>
         <InputLabel>Name:</InputLabel>
         <StyledInput
@@ -78,10 +121,11 @@ const EditRoleNodeForm = ({
       <InputContainer>
         <InputLabel>Type:</InputLabel>
         <StyledInput
-          value={nodeData.type || ''}
+          disabled
+          value={nodeData.type}
           name="type"
           type="text"
-          onChange={handleInputChange}
+          onChange={() => alert('Type field should be disabled')}
         />
       </InputContainer>
 
@@ -104,42 +148,83 @@ const EditRoleNodeForm = ({
           onChange={handleInputChange}
         />
       </InputContainer>
-
-      <InputContainer>
-        <InputLabel>Title:</InputLabel>
-        <StyledInput
-          value={nodeData.text.title || ''}
-          name="title"
-          type="text"
-          onChange={handleInputChange}
-        />
-      </InputContainer>
-
-      <InputContainer>
-        <InputLabel>Subtitle:</InputLabel>
-        <StyledInput
-          value={nodeData.text.subtitle || ''}
-          name="subtitle"
-          type="text"
-          onChange={handleInputChange}
-        />
-      </InputContainer>
-
-      <InputContainer>
-        <InputLabel>Caption:</InputLabel>
-        <StyledInput
-          value={nodeData.text.caption || ''}
-          name="caption"
-          type="text"
-          onChange={handleInputChange}
-        />
-      </InputContainer>
-
-      <StyledButton onClick={updateTeamNode}>
-        Update Team Node
-      </StyledButton>
+      <div style={{ padding: 12, border: `1px solid ${Color.MEDIUM_GRAY_1}` }}>
+        <FormLabel>Text Object</FormLabel>
+        {textInputs}
+        <div>
+          <InputContainer>
+            <InputLabel>Add New Text Field</InputLabel>
+            <div>
+              <span style={{ fontWeight: 700 }}>Key Name: </span>
+              <input
+                style={{ background: Color.WHITE, margin: 12, padding: 6 }}
+                value={newTextField}
+                type="text"
+                onChange={(e) => setNewTextField(e.target.value)}
+              />
+              <button
+                style={{
+                  padding: 6,
+                  borderRadius: 4,
+                  cursor: 'pointer',
+                  display: 'inline',
+                  background: Color.PRIMARY,
+                  color: Color.WHITE,
+                  fontWeight: 700,
+                }}
+                onClick={handleAddTextField}
+              >
+                Add
+              </button>
+            </div>
+          </InputContainer>
+        </div>
+      </div>
+      <StyledButton style={{ margin: 12 }} onClick={updateTeamNode}>SAVE</StyledButton>
     </div>
   )
 }
 
 export default EditRoleNodeForm
+
+const getTextInputs = ({ text }, handleInputChange, handleRemoveTextField) => {
+  const orderedKeys = Object.keys(text).sort()
+
+  return orderedKeys.map((key) => {
+    // ! this timestamp is being disabled because it's set elsewhere on Orion
+    const isTdgTimestamp = Boolean(key === 'tdgTimestamp')
+
+    return (
+      <InputContainer key={key}>
+        <InputLabel>{_.startCase(key)}</InputLabel>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <StyledInput
+            disabled={isTdgTimestamp}
+            style={{ flex: 25 }}
+            value={text[key] || ''}
+            name={key}
+            type="text"
+            onChange={handleInputChange}
+          />
+          {!isTdgTimestamp && (
+            <button
+              style={{
+                borderRadius: 4,
+                cursor: 'pointer',
+                flex: 1,
+                background: Color.RED,
+                color: Color.WHITE,
+                fontWeight: 700,
+                padding: 6,
+                margin: 6,
+              }}
+              onClick={() => handleRemoveTextField(key)}
+            >
+              -
+            </button>
+          )}
+        </div>
+      </InputContainer>
+    )
+  })
+}
