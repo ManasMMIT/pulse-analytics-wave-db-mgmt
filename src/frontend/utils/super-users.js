2456216f0fcdbd51cb7e59792083848b5f4d48f7
Matt Van Dyke <matthewlvandyke@gmail.com>
2021-04-29 08:37:25 -0400

    Add and remove polaris super user auth0 ids (#991)
    * Add and remove polaris super user auth0 ids
    
    * Add more devs to super user array

--- src/frontend/utils/super-users.js
+++ src/frontend/utils/super-users.js
@@ -1,12 +1,13 @@
 const superUsersById = {
-  'auth0|5e3b3ed1ae39650da7fc2dbc': 'jonathan.lin',
   'auth0|5e5fd908b752c70d52f75faf': 'clairekim',
   'auth0|5e3b36aab661bc0e5912d831': 'chuck.flores',
   'auth0|5e601df196c4280d4a9ae4f3': 'johnkim',
   'auth0|5e2a03a47397b20e870ad385': 'admin',
   'auth0|5e406107521e060e7f178e3a': 'mattvanlaw',
   'auth0|5e4fe2a8facf340d44ae322a': 'maya',
-  'auth0|5ef5f9cf59a29e0019c5d4cd': 'david',
+  'auth0|6075d94786d33a0073cbb7e9': 'chrisyuan',
+  'auth0|6089c8aa9b4c6c00710eb5b4': 'joseph.triolo',
+  'auth0|6089cad689f2c30070bec97c': 'anh.vuong',
 }
 
 export default superUsersById
