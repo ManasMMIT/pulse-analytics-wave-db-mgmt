ee47f7e8f28c183d6278ce1c631aac3f19e4cab9
Matt Van Dyke <matthewlvandyke@gmail.com>
2021-05-11 20:36:37 -0400

    Test headers axios post fix for vega data (#1009)

--- src/backend/resolvers/mutations/marketBasket/create.ts
+++ src/backend/resolvers/mutations/marketBasket/create.ts
@@ -6,14 +6,18 @@ const createMarketBasket = async (
   context,
   info
 ) => {
-  const vegaInput = {
+  const headers = {
+    'Content-Type': 'application/json'
+  }
+
+  const vegaInput = JSON.stringify({
     ...input,
     team_subscriptions: [],
     categories: [],
     question_rating_range: "{\"bounds\": \"[)\", \"lower\": \"0\", \"upper\": \"5\"}",
-  }
+  })
 
-  const { id } = await axios.post('market-baskets/', vegaInput)
+  const { id } = await axios.post('market-baskets/', vegaInput, { headers })
     .then(({ data }) => data)
     .catch((e) => {
       throw new Error(JSON.stringify(e.response.data))
