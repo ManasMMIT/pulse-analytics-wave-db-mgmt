c3a7194762cfd2b23374bf9e3bfd9b08655eb28b
Matt Van Dyke <matthewlvandyke@gmail.com>
2021-05-17 17:46:57 -0400

    Build out initial materializeData subprocess

--- src/backend/resolvers/mutations/marketBasket/importSurvey/importMarketBasketSurvey.ts
+++ src/backend/resolvers/mutations/marketBasket/importSurvey/importMarketBasketSurvey.ts
@@ -11,7 +11,7 @@ const importMarketBasketSurvey = async (
   input.surveyId = "39e7f556-6d29-4b18-ac19-ab0c77caaff1"
   await validateSurveyData(input.data)
   await upsertRelationalData(input)
-  await materializeData(input.data, pulseDevDb)
+  await materializeData({ ...input, pulseDevDb })
 
   // TODO: decide to return data from replace job or reformatted input?
   return 'done'
--- src/backend/resolvers/mutations/marketBasket/importSurvey/materializeData/materializeData.js
+++ src/backend/resolvers/mutations/marketBasket/importSurvey/materializeData/materializeData.js
@@ -1,1 +1,79 @@
-module.exports = async (data, pulseDevDb) => { }
+const axios = require('axios')
+
+module.exports = async ({ marketBasketId, surveyId, pulseDevDb }) => {
+  const surveyQuestionsAndAnswers = await axios.get(
+    `hydrated-market-basket-surveys-questions/?survey__id__in=${surveyId}`
+  ).then(({ data }) => data)
+
+  const docsToInsert = surveyQuestionsAndAnswers.reduce((acc, {
+    id,
+    survey: {
+      date: surveyDate,
+    },
+    category,
+    characteristic,
+    regimen,
+    product,
+    manufacturer,
+    answers,
+  }) => {
+    let productObj
+    if (product) {
+      productObj = {
+        _id: product.id,
+        genericName: product.generic_name,
+        brandName: product.brand_name,
+      }
+    }
+
+    let regimenObj
+    if (regimen) {
+      regimenObj = {
+        _id: regimen.id,
+        name: regimen.name,
+      }
+    }
+
+    let manufacturerObj
+    if (manufacturer) {
+      manufacturerObj = {
+        _id: manufacturer.id,
+        name: manufacturer.name,
+      }
+    }
+    const flatAnswerDocs = answers.map(({ rating, stakeholder }) => ({
+      _id: id,
+      surveyId,
+      marketBasketId,
+      surveyDate,
+      category: {
+        _id: category.id,
+        name: category.name,
+        prompt: category.prompt,
+        type: category.category_type,
+      },
+      characteristic: {
+        _id: characteristic.id,
+        name: characteristic.name,
+        description: characteristic.description,
+      },
+      regimen: regimenObj,
+      product: productObj,
+      manufacturer: manufacturerObj,
+      rating,
+      stakeholder: {
+        _id: stakeholder.id,
+        firstName: stakeholder.first_name,
+        lastName: stakeholder.last_name,
+      }
+    }))
+
+    return [...acc, ...flatAnswerDocs]
+  }, [])
+
+  if (docsToInsert.length) {
+    const [{ survey: { date: surveyDate } }] = surveyQuestionsAndAnswers
+    await pulseDevDb.collection('marketBasketsSurveyAnswers').deleteMany({ surveyId, surveyDate })
+    await pulseDevDb.collection('marketBasketsSurveyAnswers').insertMany(docsToInsert)
+  }
+}
