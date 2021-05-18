05af8373ff054430aabe3c1eea223657db749ead
Matt Van Dyke <matthewlvandyke@gmail.com>
2021-05-18 14:16:35 -0400

    Add ImportMarketBasketSurveyDataInput to typedef

--- src/backend/typeDefs/mutations/marketBasket/importSurvey.ts
+++ src/backend/typeDefs/mutations/marketBasket/importSurvey.ts
@@ -3,10 +3,30 @@ import { gql } from 'apollo-server-express'
 // TODO: Replace data: JSON with something real
 
 const importMarketBasketSurveyTypeDefs = gql`
+  input ImportMarketBasketSurveyDataInput {
+    personId: ID!
+    categoryId: ID!
+    characteristicId: ID!
+
+    person: String
+    category: String
+    characteristic: String
+    stakeholderRole: String
+    regimen: String
+    product: String
+    manufacturer: String
+    rating: Int
+    regimenId: ID
+    productId: ID
+    manufacturerId: ID
+    questionId: ID
+    answerId: ID
+  }
+
   input ImportMarketBasketSurveyInput {
     marketBasketId: ID!
     surveyId: ID!
-    data: JSON
+    data: [ImportMarketBasketSurveyDataInput!]!
   }
 `
 
