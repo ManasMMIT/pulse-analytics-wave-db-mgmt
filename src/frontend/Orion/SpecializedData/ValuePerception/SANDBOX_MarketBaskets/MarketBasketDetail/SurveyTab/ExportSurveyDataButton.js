3db0662e80ed89667662e441c59358891ccd3920
Matt Van Dyke <matthewlvandyke@gmail.com>
2021-05-18 15:57:22 -0400

    Change ExportSurveyDataButton query to lazy query

--- src/frontend/Orion/SpecializedData/ValuePerception/SANDBOX_MarketBaskets/MarketBasketDetail/SurveyTab/ExportSurveyDataButton.js
+++ src/frontend/Orion/SpecializedData/ValuePerception/SANDBOX_MarketBaskets/MarketBasketDetail/SurveyTab/ExportSurveyDataButton.js
@@ -1,44 +1,52 @@
 import React from 'react'
-import { useQuery } from '@apollo/react-hooks'
+import { useLazyQuery } from '@apollo/react-hooks'
 import { useParams } from 'react-router'
 
 import { GET_MARKET_BASKET_SURVEY_EXPORT_DATA } from 'frontend/api/queries'
 import Spinner from 'frontend/components/Spinner'
 import ExportExcelButton from 'frontend/components/ExportExcelButton'
+import Button from 'frontend/components/Button'
 import Icon from 'frontend/components/Icon'
 import Color from 'frontend/utils/color'
 
 const ExportSurveyDataButton = ({ surveyId }) => {
   const { marketBasketId } = useParams()
 
-  const { data, loading } = useQuery(
+  const [getExportData, { data, loading }] = useLazyQuery(
     GET_MARKET_BASKET_SURVEY_EXPORT_DATA,
     { variables: { marketBasketId, surveyId } }
   )
 
   if (loading) return <Spinner />
 
-  const exportData = Object.values(data)[0].map(({ __typename, ...rest }) => rest)
+  // ! Likely want to hold data in state and disable the button after generating exprt data
+  // * this will force users to export again, if they don't have the sheet handy or changed something.
+  const exportData = data
+    ? Object.values(data)[0].map(({ __typename, ...rest }) => rest)
+    : []
 
   return (
-    <ExportExcelButton
-      filename="market-basket-survey-export" // TODO make more specific
-      data={exportData}
-      buttonStyle={{
-        margin: '0 0 12px',
-        display: 'flex',
-        alignItems: 'center',
-      }}
-    >
-      <Icon
-        iconName="export"
-        color1={Color.PRIMARY}
-        width={16}
-        height={16}
-        style={{ marginRight: 8 }}
-      />
-        Export to Excel
-    </ExportExcelButton>
+    <>
+      <Button onClick={getExportData}>Generate Export Data</Button>
+      <ExportExcelButton
+        filename="market-basket-survey-export" // TODO make more specific
+        data={exportData}
+        buttonStyle={{
+          margin: '0 0 12px',
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <Icon
+          iconName="export"
+          color1={Color.PRIMARY}
+          width={16}
+          height={16}
+          style={{ marginRight: 8 }}
+        />
+        {loading ? <Spinner /> : 'Export to Excel'}
+      </ExportExcelButton>
+    </>
   )
 }
 
