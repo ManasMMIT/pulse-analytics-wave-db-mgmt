48803e9e85cb099c96bfc140f5135f8a8d5c97bb
Joseph Triolo <joseph.triolo2@gmail.com>
2021-05-20 09:01:51 -0400

    Add socket logic to frontend

--- src/frontend/Orion/SpecializedData/ValuePerception/SANDBOX_MarketBaskets/MarketBasketDetail/SurveyTab/ExportSurveyDataButton.js
+++ src/frontend/Orion/SpecializedData/ValuePerception/SANDBOX_MarketBaskets/MarketBasketDetail/SurveyTab/ExportSurveyDataButton.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useLazyQuery } from '@apollo/react-hooks'
 import { useParams } from 'react-router'
+import socket from 'frontend/api/socket'
 
 import { GET_MARKET_BASKET_SURVEY_EXPORT_DATA } from 'frontend/api/queries'
 import Spinner from 'frontend/components/Spinner'
@@ -9,13 +10,28 @@ import Button from 'frontend/components/Button'
 import Icon from 'frontend/components/Icon'
 import Color from 'frontend/utils/color'
 
+const DEFAULT_NOTIFICATION = '✅ Good to generate export'
+
 const ExportSurveyDataButton = ({ surveyId }) => {
+  const [notification, setNotification] = useState(DEFAULT_NOTIFICATION)
+  const [generateExportStatus, setGenerateExportStatus] = useState(null)
   const { marketBasketId } = useParams()
 
   const [getExportData, { data, loading }] = useLazyQuery(
     GET_MARKET_BASKET_SURVEY_EXPORT_DATA,
-    { variables: { marketBasketId, surveyId } }
+    {
+      variables: { marketBasketId, surveyId },
+      fetchPolicy: 'network-only',
+    }
   )
+
+  useEffect(() => {
+    socket.on('GENERATE_SURVEY_DATA_EXPORT', setNotification)
+  }, [])
+
+  useEffect(() => {
+    setGenerateExportStatus(notification)
+  }, [notification])
 
   if (loading) return <Spinner />
 
@@ -44,6 +60,7 @@ const ExportSurveyDataButton = ({ surveyId }) => {
           height={16}
           style={{ marginRight: 8 }}
         />
+        {generateExportStatus}
         {loading ? <Spinner /> : 'Export to Excel'}
       </ExportExcelButton>
     </>
