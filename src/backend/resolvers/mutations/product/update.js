e5356bfdb648247f141324158c886fba9bfa00c1
Joseph Triolo <joseph.triolo2@gmail.com>
2021-06-24 17:37:20 -0400

    Update marketBaskets + marketBasketsSurveyAnswers when updating product

--- src/backend/resolvers/mutations/product/update.js
+++ src/backend/resolvers/mutations/product/update.js
@@ -4,7 +4,7 @@ const axios = require('axios')
 const updateSourceProduct = async (
   parent,
   { input: { _id: productId, ...body } },
-  { mongoClient, pulseCoreDb },
+  { mongoClient, pulseCoreDb, pulseDevDb },
   info
 ) => {
   const _id = ObjectId(productId)
@@ -42,6 +42,31 @@ const updateSourceProduct = async (
       { products: { $elemMatch: { _id } } },
       { $set: { 'products.$': { _id, ...body } } }, // only need to update the 1st match in products array because they're unique
       { session }
+    )
+
+    // update the product for market Baskets
+    await pulseDevDb.collection('marketBaskets').updateMany(
+      { 'productsRegimens.product._id': uuid },
+      {
+        $set: {
+          'productsRegimens.$[productRegimen].product.brandName':
+            result.value.nameBrand,
+          'productsRegimens.$[productRegimen].product.genericName':
+            result.value.nameGeneric,
+        },
+      },
+      { arrayFilters: [{ 'productRegimen.product._id': uuid }] }
+    )
+
+    // update the product for market basket survey answers
+    await pulseDevDb.collection('marketBasketsSurveyAnswers').updateMany(
+      { 'product._id': uuid },
+      {
+        $set: {
+          'product.brandName': result.value.nameBrand,
+          'product.genericName': result.value.nameGeneric,
+        },
+      }
     )
   })
 
