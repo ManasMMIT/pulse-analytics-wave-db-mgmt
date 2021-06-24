0593471bb5408fb3f314dd17c0517534434bf83d
Joseph Triolo <joseph.triolo2@gmail.com>
2021-06-24 17:24:45 -0400

    Update mongo marketBaskets collection when updating vega Product

--- src/backend/resolvers/vega/mutations/product/update.ts
+++ src/backend/resolvers/vega/mutations/product/update.ts
@@ -1,16 +1,29 @@
 import axios from 'axios'
 
-const updateVegaProduct = (
+const updateVegaProduct = async (
   parent,
   { input: { id, ...body } },
-  context,
+  { pulseDevDb },
   info
 ) => {
-  return axios.patch(`products/${id}/`, body)
+  const updatedProduct = await axios.patch(`products/${id}/`, body)
     .then(({ data }) => data)
     .catch((e) => {
       throw new Error(JSON.stringify(e.response.data))
     })
+
+  await pulseDevDb.collection('marketBaskets').updateMany(
+    { 'productsRegimens.product._id': id },
+    {
+      $set: {
+        'productsRegimens.$[productRegimen].product.logoLink': updatedProduct.logo_link,
+        'productsRegimens.$[productRegimen].product.color': updatedProduct.color,
+      }
+    },
+    { arrayFilters: [{ 'productRegimen.product._id': id }] }
+  )
+
+  return updatedProduct
 }
 
 export default updateVegaProduct
